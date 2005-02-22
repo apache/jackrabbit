d6b7055870c52ab359270f21972b3f4d141656d9
Marcel Reutegger <mreutegg@apache.org>
2005-02-22 10:15:06 +0000

    QueryHandler does not release read lock when exception is thrown while creating executable query.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jackrabbit/trunk@154824 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/jackrabbit/core/search/lucene/SearchIndex.java
+++ src/java/org/apache/jackrabbit/core/search/lucene/SearchIndex.java
@@ -235,27 +235,27 @@ public class SearchIndex extends AbstractQueryHandler {
             throw new IOException("Unable to obtain read lock on search index.");
         }
 
-        SortField[] sortFields = new SortField[orderProps.length];
-        for (int i = 0; i < orderProps.length; i++) {
-            String prop = null;
-            if (QueryConstants.JCR_SCORE.equals(orderProps[i])) {
-                // order on jcr:score does not use the natural order as
-                // implemented in lucene. score ascending in lucene means that
-                // higher scores are first. JCR specs that lower score values
-                // are first.
-                sortFields[i] = new SortField(null, SortField.SCORE, orderSpecs[i]);
-            } else {
-                try {
-                    prop = orderProps[i].toJCRName(nsMappings);
-                } catch (NoPrefixDeclaredException e) {
-                    // will never happen
-                }
-                sortFields[i] = new SortField(prop, SortField.STRING, !orderSpecs[i]);
-            }
-        }
-
         Hits hits = null;
         try {
+            SortField[] sortFields = new SortField[orderProps.length];
+            for (int i = 0; i < orderProps.length; i++) {
+                String prop = null;
+                if (QueryConstants.JCR_SCORE.equals(orderProps[i])) {
+                    // order on jcr:score does not use the natural order as
+                    // implemented in lucene. score ascending in lucene means that
+                    // higher scores are first. JCR specs that lower score values
+                    // are first.
+                    sortFields[i] = new SortField(null, SortField.SCORE, orderSpecs[i]);
+                } else {
+                    try {
+                        prop = orderProps[i].toJCRName(nsMappings);
+                    } catch (NoPrefixDeclaredException e) {
+                        // will never happen
+                    }
+                    sortFields[i] = new SortField(prop, SortField.STRING, !orderSpecs[i]);
+                }
+            }
+
             if (sortFields.length > 0) {
                 hits = persistentIndex.getIndexSearcher().search(query,
                         new Sort(sortFields));
