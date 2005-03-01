a3ce6dbd6d892ea8cd5f4e53326f890cba52a4b0
Marcel Reutegger <mreutegg@apache.org>
2005-03-01 10:18:14 +0000

    Added ability to specify the jaas configuration path in the stub properties file. This allows to quickly run test cases from within IntelliJ.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jackrabbit/trunk@155766 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/jackrabbit/test/JackrabbitRepositoryStub.java
+++ src/test/org/apache/jackrabbit/test/JackrabbitRepositoryStub.java
@@ -38,10 +38,24 @@ public class JackrabbitRepositoryStub extends RepositoryStub {
      * Property for the repository configuration file (used for repository instantiation)
      */
     public static final String PROP_REPOSITORY_CONFIG = "org.apache.jackrabbit.repository.config";
+
     /**
      * Property for the repository home directory (used for repository instantiation)
      */
     public static final String PROP_REPOSITORY_HOME = "org.apache.jackrabbit.repository.home";
+
+    /**
+     * Property for the jaas config path. If the system property
+     * <code>java.security.auth.login.config</code> is not set this repository
+     * stub will try to read this property from the environment and use the
+     * value retrieved as the value for the system property.
+     */
+    public static final String PROP_JAAS_CONFIG = "org.apache.jackrabbit.repository.jaas.config";
+
+    /**
+     * The name of the jaas config system property.
+     */
+    private static final String SYS_JAAS_CONFIG = "java.security.auth.login.config";
 
     /**
      * The repository instance
@@ -72,6 +86,13 @@ public class JackrabbitRepositoryStub extends RepositoryStub {
                 String repName = environment.getProperty(PROP_REPOSITORY_NAME, "repo");
                 String repConfig = environment.getProperty(PROP_REPOSITORY_CONFIG);
                 String repHome = environment.getProperty(PROP_REPOSITORY_HOME);
+                String jaasConfig = environment.getProperty(PROP_JAAS_CONFIG);
+
+                // set jaas config from stub properties if system property is
+                // not set.
+                if (System.getProperty(SYS_JAAS_CONFIG) == null && jaasConfig != null) {
+                    System.setProperty(SYS_JAAS_CONFIG, jaasConfig);
+                }
 
                 // register repository instance
                 Hashtable env = new Hashtable();
