6e1e9f432801a9f0031d054d9f89d9441c08b06e
Marcel Reutegger <mreutegg@apache.org>
2005-03-11 23:05:03 +0000

    oops, forgot to add test cases to TestAll.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jackrabbit/trunk@157173 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/jackrabbit/test/api/nodetype/TestAll.java
+++ src/test/org/apache/jackrabbit/test/api/nodetype/TestAll.java
@@ -55,6 +55,9 @@ public class TestAll extends TestCase {
         suite.addTestSuite(CanSetPropertyStringTest.class);
         suite.addTestSuite(CanSetPropertyTest.class);
 
+        suite.addTestSuite(CanAddChildNodeCallWithNodeTypeTest.class);
+        suite.addTestSuite(CanAddChildNodeCallWithoutNodeTypeTest.class);
+
         return suite;
     }
 }
