e54200537ba0216daf8acbaedb480e61c454330b
Stefan Guggisberg <stefan@apache.org>
2005-04-28 16:33:21 +0000

    port to jcr api v0.16.4.1
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jackrabbit/trunk@165161 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/jackrabbit/core/value/ValueFactoryImpl.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2004-2005 The Apache Software Foundation or its licensors,
+ *                     as applicable.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.core.value;
+
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+import javax.jcr.Value;
+import javax.jcr.ValueFactory;
+import javax.jcr.ValueFormatException;
+import java.io.InputStream;
+import java.util.Calendar;
+
+/**
+ * This class implements the  <code>ValueFactory</code> interface.
+ *
+ * @see javax.jcr.Session#getValueFactory()
+ */
+public class ValueFactoryImpl implements ValueFactory {
+
+    /**
+     * Constructs a <code>ValueFactory</code> object.
+     */
+    public ValueFactoryImpl() {
+    }
+
+    //---------------------------------------------------------< ValueFactory >
+    /**
+     * {@inheritDoc}
+     */
+    public Value createValue(boolean value) {
+        return new BooleanValue(value);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Value createValue(Calendar value) {
+        return new DateValue(value);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Value createValue(double value) {
+        return new DoubleValue(value);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Value createValue(InputStream value) {
+        return new BinaryValue(value);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Value createValue(long value) {
+        return new LongValue(value);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Value createValue(Node value) throws RepositoryException {
+        return new ReferenceValue(value);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Value createValue(String value) {
+        return new StringValue(value);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Value createValue(String value, int type)
+            throws ValueFormatException {
+        return ValueHelper.convert(value, type);
+    }
+}
