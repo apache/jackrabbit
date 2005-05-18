ab483d4766f175f709ae7bd30f42d449a49d3833
Jukka Zitting <jukka@apache.org>
2005-05-18 20:05:17 +0000

    JCR-RMI: Applying patch sent by Felix: "I have some more adaptions for the JCR-RMI value implementation:  StatefullValueAdapter needs read/writeObject as does BinaryValue.  On the other hand SerialValue does not need it."
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jackrabbit/trunk@170807 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/jcr-rmi/src/java/org/apache/jackrabbit/value/StatefullValueAdapter.java
+++ contrib/jcr-rmi/src/java/org/apache/jackrabbit/value/StatefullValueAdapter.java
@@ -16,24 +16,47 @@
  */
 package org.apache.jackrabbit.value;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.Calendar;
 
+import javax.jcr.PropertyType;
 import javax.jcr.RepositoryException;
 import javax.jcr.Value;
 import javax.jcr.ValueFormatException;
 
 /**
- * The <code>StatefullValueAdapter</code> class 
- * 
+ * The <code>StatefullValueAdapter</code> class implements the committed value
+ * state for some JCR <code>Value</code> as a part of the State design pattern
+ * (Gof) used by this package.
+ * <p>
+ * This class implements {@link #readObject(ObjectInputStream)} and
+ * {@link #writeObject(ObjectOutputStream)} overwriting the default behaviour.
+ * The reason for this is, that we cannot guarantee delegatee value to be
+ * serializable in which case the {@link #writeObject(ObjectOutputStream)}
+ * must first create a serializable value. The
+ * {@link #readObject(ObjectInputStream)} method is here just to guarantee
+ * symetric implementation.
+ *
  * @version $Revision$, $Date$
- * @author fmeschbe
- * @since 
+ * @author Felix Meschberger
+ * @since 0.16.4.1
+ * 
+ * @see SerialValue
  */
 final class StatefullValueAdapter implements StatefullValue {
 
-    private final Value delegatee;
+    /** The delegatee value. */
+    private Value delegatee;
     
+    /**
+     * Creates an instance adapting the given JCR <code>Value</code> to the
+     * State design pattern.
+     * 
+     * @param delegatee The JCR <code>Value</code> providing the value date.
+     */
     StatefullValueAdapter(Value delegatee) {
         this.delegatee = delegatee;
     }
@@ -71,5 +94,95 @@ final class StatefullValueAdapter implements StatefullValue {
     /** {@inheritDoc} */
     public int getType() {
         return delegatee.getType();
+    }
+
+    /**
+     * Writes the delegate value to the given <code>ObjectOutputStream</code>.
+     * If the delegatee is {@link SerialValue} it is directly written. Otherwise
+     * the {@link SerialValueFactory} is asked to create a {@link StatefullValue}
+     * from the delegatee, which is then written. The newly created
+     * {@link StatefullValue} value also replaces the original delegatee
+     * internally.
+     * 
+     * @param out The destination to write the delegatee to.
+     * 
+     * @throws IOException If an error occurrs writing the value or if an
+     *      error occurrs creating the {@link StatefullValue} from the
+     *      delegatee.
+     */
+    private void writeObject(ObjectOutputStream out) throws IOException {
+        // if the delegatee value is a StatefullValue or SerialValue, serialize it
+        if (delegatee instanceof StatefullValue ||
+                delegatee instanceof SerialValue) {
+            out.writeObject(delegatee);
+            return;
+        }
+        
+        // otherwise create a SerialValue from the delegatee value to send
+        try {
+            SerialValueFactory factory = SerialValueFactory.getInstance();
+            Value toSend;
+            switch (getType()) {
+                case PropertyType.BINARY:
+                     toSend = factory.createBinaryValue(getStream());
+                    break;
+                case PropertyType.BOOLEAN:
+                    toSend = factory.createBooleanValue(getBoolean());
+                    break;
+                case PropertyType.DATE:
+                    toSend = factory.createDateValue(getDate());
+                    break;
+                case PropertyType.DOUBLE:
+                    toSend = factory.createDoubleValue(getDouble());
+                    break;
+                case PropertyType.LONG:
+                    toSend = factory.createLongValue(getLong());
+                    break;
+                case PropertyType.NAME:
+                    toSend = factory.createNameValue(getString());
+                    break;
+                case PropertyType.PATH:
+                    toSend = factory.createPathValue(getString());
+                    break;
+                case PropertyType.REFERENCE:
+                    toSend = factory.createReferenceValue(getString());
+                    break;
+                case PropertyType.STRING:
+                    toSend = factory.createStringValue(getString());
+                    break;
+                default:
+                    throw new IOException("Unknown value type");
+            }
+
+            // replace the delegatee with the new one
+            delegatee = toSend;
+            
+            // and finally send the serial value instance
+            out.writeObject(toSend);
+        } catch (RepositoryException ex) {
+            throw new IOException(ex.getMessage());
+        }
+    }
+    
+    /**
+     * Reads an reconstructs the delegatee from the given
+     * <code>ObjectInputStream</code>. The value read will either be an
+     * instance of {@link SerialValue} or a {@link StatefullValue} depending
+     * on the original delegatee written.
+     * 
+     * @param in The <code>ObjectInputStream</code> from which to read the
+     *      delegatee.
+     * 
+     * @throws IOException If an error occurrs reading from the
+     *      <code>ObjectInputStream</code> or if the runtime class of the
+     *      value to be read cannot be found. 
+     */
+    private void readObject(ObjectInputStream in) throws IOException {
+        try {
+            delegatee = (Value) in.readObject();
+        } catch (ClassNotFoundException cnfe) {
+            throw new IOException("Cannot load value object class: " + 
+                cnfe.getMessage());
+        }
     }
 }
