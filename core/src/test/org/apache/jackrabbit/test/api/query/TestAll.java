17fef4e779e4cf70d45d71b656529456ad31553b
Marcel Reutegger <mreutegg@apache.org>
2005-07-13 13:57:37 +0000

    - Name of new test case clashes with existing test case. Renamed to QueryResultNodeIteratorTest.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jackrabbit/trunk@216155 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/test/org/apache/jackrabbit/test/api/query/TestAll.java
+++ core/src/test/org/apache/jackrabbit/test/api/query/TestAll.java
@@ -58,7 +58,7 @@ public class TestAll extends TestCase {
         suite.addTestSuite(GetStatementTest.class);
         suite.addTestSuite(GetSupportedQueryLanguagesTest.class);
 
-        suite.addTestSuite(NodeIteratorTest.class);
+        suite.addTestSuite(QueryResultNodeIteratorTest.class);
         suite.addTestSuite(GetPropertyNamesTest.class);
         suite.addTestSuite(PredicatesTest.class);
         suite.addTestSuite(SimpleSelectionTest.class);
