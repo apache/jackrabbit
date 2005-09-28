70b66dbc8ccdfc7b0b74789c9aa7fa2ec1d96700
Jukka Zitting <jukka@apache.org>
2005-09-28 20:46:08 +0000

    Added RepositoryConfig test cases and better error reporting when attempting to create a duplicate workspace.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jackrabbit/trunk@292294 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/jackrabbit/core/config/RepositoryConfigTest.java
+++ src/test/org/apache/jackrabbit/core/config/RepositoryConfigTest.java
@@ -28,21 +28,46 @@ import junit.framework.TestCase;
  */
 public class RepositoryConfigTest extends TestCase {
 
-    /**
-     * Test that a standard repository configuration file is
-     * correctly parsed.
-     *
-     * @throws Exception on errors
-     */
-    public void testRepositoryXml() throws Exception {
-        InputStream xml = getClass().getClassLoader().getResourceAsStream(
-                "org/apache/jackrabbit/core/config/repository.xml");
-        RepositoryConfig config =
-            RepositoryConfig.create(new InputSource(xml), "target");
+    private static final String REPOSITORY_XML = "org/apache/jackrabbit/core/config/repository.xml";
 
-        assertEquals("target", config.getHomeDir());
+    private static final String REPOSITORY_HOME = "target/test-repository";
+
+    private RepositoryConfig config;
+
+    private static void deleteAll(File file) {
+        if (file.exists()) {
+            if (file.isDirectory()) {
+                File[] children = file.listFiles();
+                for (int i = 0; i < children.length; i++) {
+                    deleteAll(children[i]);
+                }
+            }
+            file.delete();
+        }
+    }
+
+    /**
+     * Sets up the test case by reading the test repository configuration file.
+     */
+    protected void setUp() throws Exception {
+        ClassLoader loader = getClass().getClassLoader();
+        InputStream xml = loader.getResourceAsStream(REPOSITORY_XML);
+        config = RepositoryConfig.create(new InputSource(xml), REPOSITORY_HOME);
+    }
+
+    protected void tearDown() {
+        deleteAll(new File(REPOSITORY_HOME));
+    }
+
+    /**
+     * Test that the repository configuration file is correctly parsed.
+     */
+    public void testRepositoryConfig() throws Exception {
+        assertEquals(REPOSITORY_HOME, config.getHomeDir());
         assertEquals("default", config.getDefaultWorkspaceName());
-        assertEquals("target/workspaces", config.getWorkspacesConfigRootDir());
+        assertEquals(
+                new File(REPOSITORY_HOME, "workspaces").getPath(),
+                config.getWorkspacesConfigRootDir());
         assertEquals("Jackrabbit", config.getAppName());
 
         AccessManagerConfig amc = config.getAccessManagerConfig();
@@ -52,11 +77,35 @@ public class RepositoryConfigTest extends TestCase {
         assertTrue(amc.getParameters().isEmpty());
 
         VersioningConfig vc = config.getVersioningConfig();
-        assertEquals(new File("target/version"), vc.getHomeDir());
+        assertEquals(new File(REPOSITORY_HOME, "version"), vc.getHomeDir());
         assertEquals(
                 "org.apache.jackrabbit.core.state.obj.ObjectPersistenceManager",
                 vc.getPersistenceManagerConfig().getClassName());
         assertTrue(vc.getPersistenceManagerConfig().getParameters().isEmpty());
+    }
+
+    public void testInit() throws Exception {
+        File workspaces_dir = new File(REPOSITORY_HOME, "workspaces");
+        File workspace_dir = new File(workspaces_dir, "default");
+        File workspace_xml = new File(workspace_dir, "workspace.xml");
+        assertTrue(workspace_xml.exists());
+    }
+
+    public void testCreateWorkspaceConfig() throws Exception {
+        config.createWorkspaceConfig("test-workspace");
+        File workspaces_dir = new File(REPOSITORY_HOME, "workspaces");
+        File workspace_dir = new File(workspaces_dir, "test-workspace");
+        File workspace_xml = new File(workspace_dir, "workspace.xml");
+        assertTrue(workspace_xml.exists());
+    }
+
+    public void testCreateDuplicateWorkspaceConfig() throws Exception {
+        try {
+            config.createWorkspaceConfig("default");
+            fail("No exception thrown when creating a duplicate workspace");
+        } catch (ConfigurationException e) {
+            // test passed
+        }
     }
 
 }
