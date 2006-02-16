93598596845f6427826b313f1c3e8423aa73ba0f
Jukka Zitting <jukka@apache.org>
2006-02-16 10:01:38 +0000

    JCR-309: Added the JackrabbitNodeTypeManager extension interface and a simple implementation in NodeTypeManagerImpl.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jackrabbit/trunk@378211 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit/src/main/java/org/apache/jackrabbit/core/nodetype/NodeTypeManagerImpl.java
+++ jackrabbit/src/main/java/org/apache/jackrabbit/core/nodetype/NodeTypeManagerImpl.java
@@ -22,26 +22,35 @@ import org.apache.jackrabbit.name.NamespaceResolver;
 import org.apache.jackrabbit.name.QName;
 import org.apache.jackrabbit.name.UnknownPrefixException;
 import org.apache.jackrabbit.util.IteratorHelper;
+import org.apache.jackrabbit.api.JackrabbitNodeTypeManager;
+import org.apache.jackrabbit.core.nodetype.xml.NodeTypeReader;
 import org.apache.jackrabbit.core.util.Dumpable;
 import org.apache.log4j.Logger;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 import javax.jcr.RepositoryException;
 import javax.jcr.nodetype.NoSuchNodeTypeException;
 import javax.jcr.nodetype.NodeType;
 import javax.jcr.nodetype.NodeTypeIterator;
-import javax.jcr.nodetype.NodeTypeManager;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * A <code>NodeTypeManagerImpl</code> implements a session dependant
  * NodeTypeManager.
  */
-public class NodeTypeManagerImpl implements NodeTypeManager, Dumpable,
-        NodeTypeRegistryListener {
+public class NodeTypeManagerImpl implements JackrabbitNodeTypeManager,
+        Dumpable, NodeTypeRegistryListener {
 
     /**
      * Logger instance for this class
@@ -286,6 +295,56 @@ public class NodeTypeManagerImpl implements NodeTypeManager, Dumpable,
             throw new NoSuchNodeTypeException(nodeTypeName, upe);
         } catch (IllegalNameException ine) {
             throw new NoSuchNodeTypeException(nodeTypeName, ine);
+        }
+    }
+
+    //--------------------------------------------< JackrabbitNodeTypeManager >
+
+    /**
+     * Registers the node types defined in the given XML stream. This is a
+     * trivial implementation that just invokes the existing
+     * {@link NodeTypeReader} and {@link NodeTypeRegistry} methods and
+     * heuristically creates the returned node type array.
+     *
+     * {@inheritDoc}
+     */
+    public NodeType[] registerNodeTypes(InputSource in)
+            throws SAXException, RepositoryException {
+        try {
+            NodeTypeDef[] defs = NodeTypeReader.read(in.getByteStream());
+            ntReg.registerNodeTypes(Arrays.asList(defs));
+
+            Set types = new HashSet();
+            for (int i = 0; i < defs.length; i++) {
+                try {
+                    types.add(getNodeType(defs[i].getName()));
+                } catch (NoSuchNodeTypeException e) {
+                    // ignore
+                }
+            }
+            return (NodeType[]) types.toArray(new NodeType[types.size()]);
+        } catch (InvalidNodeTypeDefException e) {
+            throw new RepositoryException("Invalid node type definition", e);
+        } catch (IOException e) {
+            throw new SAXException("Error reading node type stream", e);
+        }
+    }
+
+    private static final String APPLICATION_XML = "application/xml";
+
+    /** {@inheritDoc} */
+    public NodeType[] registerNodeTypes(InputStream in, String contentType)
+            throws IOException, RepositoryException {
+        if (contentType.equalsIgnoreCase(JackrabbitNodeTypeManager.TEXT_XML)
+            || contentType.equalsIgnoreCase(APPLICATION_XML)) {
+            try {
+                return registerNodeTypes(new InputSource(in));
+            } catch (SAXException e) {
+                throw new IOException(e.getMessage());
+            }
+        } else {
+            throw new UnsupportedOperationException(
+                    "Unsupported content type: " + contentType);
         }
     }
 
