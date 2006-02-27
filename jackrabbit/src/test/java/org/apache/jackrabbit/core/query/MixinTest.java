e130b0f68c12ea2298ec7a0a02e9fad8911d707c
Marcel Reutegger <mreutegg@apache.org>
2006-02-27 08:58:19 +0000

    JCR-327: Mixins as supertypes do not appear to be queryable - also search for mixins that extend the mixin of the element test
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jackrabbit/trunk@381292 13f79535-47bb-0310-9956-ffa450edef68

---
+++ jackrabbit/src/test/java/org/apache/jackrabbit/core/query/MixinTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2004-2005 The Apache Software Foundation or its licensors,
+ *                     as applicable.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.core.query;
+
+import javax.jcr.RepositoryException;
+import javax.jcr.Node;
+import java.io.ByteArrayInputStream;
+import java.util.Calendar;
+
+/**
+ * Tests if mixin types are queried correctly when using element test: element()
+ */
+public class MixinTest extends AbstractQueryTest {
+
+    public void testBuiltInMixin() throws RepositoryException {
+        // nt:resoure is referenceable by its node type definition
+        Node n1 = testRootNode.addNode("n1", "nt:resource");
+        n1.setProperty("jcr:data", new ByteArrayInputStream("hello world".getBytes()));
+        n1.setProperty("jcr:lastModified", Calendar.getInstance());
+        n1.setProperty("jcr:mimeType", "application/octet-stream");
+
+        // assign mix:referenceable to arbitrary node
+        Node n2 = testRootNode.addNode("n2");
+        n2.addMixin("mix:referenceable");
+
+        // make node referenceable using a mixin that extends from mix:referenceable
+        Node n3 = testRootNode.addNode("n3");
+        n3.addMixin("test:referenceable");
+
+        testRootNode.save();
+
+        String query = testPath + "//element(*, mix:referenceable)";
+        executeXPathQuery(query, new Node[]{n1, n2, n3});
+    }
+
+}
