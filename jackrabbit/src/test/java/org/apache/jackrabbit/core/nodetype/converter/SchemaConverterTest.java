d86909bfb9a3df0ec37d79bbfba9c8dab0d21254
Jukka Zitting <jukka@apache.org>
2006-03-07 12:05:39 +0000

    1.0: Backported the XMLSchema/nodetype converter move to o.a.j.core. (r383847)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jackrabbit/branches/1.0@383859 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit/src/test/java/org/apache/jackrabbit/core/nodetype/converter/SchemaConverterTest.java
+++ jackrabbit/src/test/java/org/apache/jackrabbit/core/nodetype/converter/SchemaConverterTest.java
@@ -21,7 +21,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.jackrabbit.util.nodetype;
+package org.apache.jackrabbit.core.nodetype.converter;
 
 
 import org.apache.jackrabbit.core.nodetype.compact.CompactNodeTypeDefReader;
