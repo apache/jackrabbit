e4582e2c3c844cc1ff9e2dda521df17f015a742d
Jukka Zitting <jukka@apache.org>
2006-03-18 13:15:58 +0000

    JCR-313: Extracted common database file system functionality to an abstract DatabaseFileSystem class. Added a JNDIDatabaseFileSystem class.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jackrabbit/trunk/jackrabbit@386848 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/jackrabbit/core/fs/db/JNDIDatabaseFileSystem.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2004-2005 The Apache Software Foundation or its licensors,
+ *                     as applicable.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.core.fs.db;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+/**
+ * Database file system that uses JNDI to acquire the database connection.
+ * The JNDI location of the {@link DataSource} to be used in given as
+ * the <code>dataSourceLocation</code> configuration property. See the
+ * {@link DbFileSystem} for more configuration details.
+ * <p>
+ * <strong>WARNING:</strong> The acquired database connection is kept
+ * for the entire lifetime of the file system instance. The configured data
+ * source should be prepared for this. 
+ */
+public class JNDIDatabaseFileSystem extends DatabaseFileSystem {
+
+    /**
+     * JNDI location of the data source used to acquire database connections.
+     */
+    private String dataSourceLocation;
+
+    //----------------------------------------------------< setters & getters >
+
+    /**
+     * Returns the JNDI location of the data source.
+     *
+     * @return data source location
+     */
+    public String getDataSourceLocation() {
+        return dataSourceLocation;
+    }
+
+    /**
+     * Sets the JNDI location of the data source.
+     *
+     * @param dataSourceLocation data source location
+     */
+    public void setDataSourceLocation(String dataSourceLocation) {
+        this.dataSourceLocation = dataSourceLocation;
+    }
+
+    //--------------------------------------------------< DatabaseFileSystem >
+
+    /**
+     * Returns a JDBC connection from a {@link DataSource} acquired from JNDI
+     * with the configured data source location.
+     *
+     * @return new database connection
+     * @throws NamingException if the given data source location does not exist
+     * @throws Exception if a database access error occurs
+     * @see DatabaseFileManager#getConnection()
+     */
+    protected Connection getConnection() throws NamingException, SQLException {
+        InitialContext ic = new InitialContext();
+        DataSource dataSource = (DataSource) ic.lookup(dataSourceLocation);
+        return dataSource.getConnection();
+    }
+
+}
