e09609437011939133743d8ca1fcd9afeb77001c
Jukka Zitting <jukka@apache.org>
2006-03-22 22:52:40 +0000

    JCR-325: Applied the xml-refactoring.patch that puts the PropInfo instances in charge of applying the imported property values to content nodes.
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@387961 13f79535-47bb-0310-9956-ffa450edef68

---
+++ jackrabbit/src/main/java/org/apache/jackrabbit/core/xml/NodeInfo.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2004-2005 The Apache Software Foundation or its licensors,
+ *                     as applicable.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.core.xml;
+
+import org.apache.jackrabbit.core.NodeId;
+import org.apache.jackrabbit.name.QName;
+
+public class NodeInfo {
+
+    private final QName name;
+
+    private final QName nodeTypeName;
+
+    private final QName[] mixinNames;
+
+    private final NodeId id;
+
+    public NodeInfo(QName name, QName nodeTypeName, QName[] mixinNames,
+                    NodeId id) {
+        this.name = name;
+        this.nodeTypeName = nodeTypeName;
+        this.mixinNames = mixinNames;
+        this.id = id;
+    }
+
+    public QName getName() {
+        return name;
+    }
+
+    public QName getNodeTypeName() {
+        return nodeTypeName;
+    }
+
+    public QName[] getMixinNames() {
+        return mixinNames;
+    }
+
+    public NodeId getId() {
+        return id;
+    }
+
+}
