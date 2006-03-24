289c7cbec7b2f57cc63db98ba91ee298b4057092
Roy T. Fielding <fielding@apache.org>
2006-03-24 09:17:45 +0000

    JCR-356: Update license header
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/branches/1.0@388471 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit/src/main/java/org/apache/jackrabbit/util/name/NamespaceExtractor.java
+++ jackrabbit/src/main/java/org/apache/jackrabbit/util/name/NamespaceExtractor.java
@@ -1,19 +1,11 @@
 /*
- * $Id$
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  The ASF licenses this file to You
+ * under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Copyright 2002-2004 Day Management AG, Switzerland.
- *
- * Licensed under the Day RI License, Version 2.0 (the "License"),
- * as a reference implementation of the following specification:
- *
- *   Content Repository API for Java Technology, revision 0.13
- *        <http://www.jcp.org/en/jsr/detail?id=170>
- *
- * You may not use this file except in compliance with the License.
- * You may obtain a copy of the License files at
- *
- *     http://www.day.com/content/en/licenses/day-ri-license-2.0
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
