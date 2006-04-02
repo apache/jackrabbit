466573791aec7a66bc823a763bef09340b4d5175
Jukka Zitting <jukka@apache.org>
2006-04-02 10:40:57 +0000

    JCR-325: Added a simple test case for multi-value handling in document view import.
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@390807 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit/src/test/java/org/apache/jackrabbit/core/xml/DocumentViewTest.java
+++ jackrabbit/src/test/java/org/apache/jackrabbit/core/xml/DocumentViewTest.java
@@ -16,12 +16,18 @@
 package org.apache.jackrabbit.core.xml;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 
 import javax.jcr.ImportUUIDBehavior;
+import javax.jcr.Node;
+import javax.jcr.PathNotFoundException;
+import javax.jcr.Property;
 import javax.jcr.Session;
 import javax.jcr.ValueFormatException;
+import javax.jcr.nodetype.NoSuchNodeTypeException;
 
+import org.apache.jackrabbit.api.JackrabbitNodeTypeManager;
 import org.apache.jackrabbit.core.TestRepository;
 
 import junit.framework.TestCase;
@@ -34,6 +40,39 @@ import junit.framework.TestCase;
  */
 public class DocumentViewTest extends TestCase {
 
+    /** Test session. */
+    private Session session;
+
+    /**
+     * Sets up the test fixture.
+     *
+     * @throws Exception if an unexpected error occurs
+     */
+    protected void setUp() throws Exception {
+        session = TestRepository.getInstance().login();
+        JackrabbitNodeTypeManager manager = (JackrabbitNodeTypeManager)
+            session.getWorkspace().getNodeTypeManager();
+        try {
+            manager.getNodeType("DocViewMultiValueTest");
+        } catch (NoSuchNodeTypeException e) {
+            String cnd = "[DocViewMultiValueTest] - test (boolean) multiple";
+            manager.registerNodeTypes(
+                    new ByteArrayInputStream(cnd.getBytes("UTF-8")),
+                    JackrabbitNodeTypeManager.TEXT_X_JCR_CND);
+        }
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * @throws Exception if an unexpected error occurs
+     */
+    protected void tearDown() throws Exception {
+        // TODO: Unregister the MultiValueTestType node type once Jackrabbit
+        // supports node type removal.
+        session.logout();
+    }
+
     /**
      * Test case for
      * <a href="http://issues.apache.org/jira/browse/JCR-369">JCR-369</a>:
@@ -42,7 +81,6 @@ public class DocumentViewTest extends TestCase {
      * @throws Exception if an unexpected error occurs
      */
     public void testTwoMixins() throws Exception {
-        Session session = TestRepository.getInstance().login();
         try {
             String xml = "<two-mixins-test"
                 + " jcr:mixinTypes=\"mix:referenceable mix:lockable\""
@@ -54,8 +92,39 @@ public class DocumentViewTest extends TestCase {
         } catch (ValueFormatException e) {
             fail("JCR-369: IllegalNameException when importing document view"
                     + " with two mixins");
-        } finally {
-            session.logout();
+        }
+    }
+
+    /**
+     * Test case for
+     * <a href="http://issues.apache.org/jira/browse/JCR-325">JCR-325</a>:
+     * docview roundtripping does not work with multivalue non-string properties
+     *
+     * @throws Exception if an unexpected error occurs
+     */
+    public void testMultiValue() throws Exception {
+        String message = "JCR-325: docview roundtripping does not work with"
+            + " multivalue non-string properties";
+
+        Node root = session.getRootNode();
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+
+        Node node = root.addNode("multi-value-test", "DocViewMultiValueTest");
+        node.setProperty("test", new String[] {"true", "false"});
+        session.exportDocumentView("/multi-value-test", buffer, true, true);
+        session.refresh(false); // Discard the transient multi-value-test node
+
+        session.importXML(
+                "/", new ByteArrayInputStream(buffer.toByteArray()),
+                ImportUUIDBehavior.IMPORT_UUID_COLLISION_THROW);
+        try {
+            Property property = root.getProperty("multi-value-test/test");
+            assertTrue(message, property.getDefinition().isMultiple());
+            assertEquals(message, property.getValues().length, 2);
+            assertTrue(message, property.getValues()[0].getBoolean());
+            assertFalse(message, property.getValues()[1].getBoolean());
+        } catch (PathNotFoundException e) {
+            fail(message);
         }
     }
 
