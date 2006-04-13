787fb0b9290328d8642629855ed630c202a0f74b
Angela Schreiber <angela@apache.org>
2006-04-13 06:40:50 +0000

    Remove extra line between license header and package.
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@393734 13f79535-47bb-0310-9956-ffa450edef68

--- jcr-server/server/src/java/org/apache/jackrabbit/webdav/simple/LocatorFactoryImpl.java
+++ jcr-server/server/src/java/org/apache/jackrabbit/webdav/simple/LocatorFactoryImpl.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.jackrabbit.webdav.simple;
 
 import org.apache.jackrabbit.util.Text;
