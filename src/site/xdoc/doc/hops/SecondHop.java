32dc289240371eb865555f067bc00b1afc160406
Jukka Zitting <jukka@apache.org>
2006-04-24 22:13:33 +0000

    First Hops: Rewrite of Hop 2 and dependency links on Hop 0.
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk/jackrabbit@396698 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/site/xdoc/doc/hops/SecondHop.java
@@ -0,0 +1,44 @@
+import javax.jcr.Repository;
+import javax.jcr.Session;
+import javax.jcr.SimpleCredentials;
+import javax.jcr.Node;
+import org.apache.jackrabbit.core.TransientRepository;
+
+/**
+ * Second hop example. Stores, retrieves, and removes example content.
+ */
+public class SecondHop {
+
+    /**
+     * The main entry point of the example application.
+     *
+     * @param args command line arguments (ignored)
+     * @throws Exception if an error occurs
+     */
+    public static void main(String[] args) throws Exception {
+        Repository repository = new TransientRepository();
+        Session session = repository.login(
+                new SimpleCredentials("username", "password".toCharArray()));
+        try {
+            Node root = session.getRootNode();
+
+            // Store content
+            Node hello = root.addNode("hello");
+            Node world = hello.addNode("world");
+            world.setProperty("message", "Hello, World!");
+            session.save();
+
+            // Retrieve content
+            Node node = root.getNode("hello/world");
+            System.out.println(node.getPath());
+            System.out.println(node.getProperty("message").getString());
+
+            // Remove content
+            root.getNode("hello").remove();
+            session.save();
+        } finally {
+            session.logout();
+        }
+    }
+
+}
