1537b534b6ba4e2279f646182e61d390faf2ed64
Jukka Zitting <jukka@apache.org>
2006-08-01 18:29:26 +0000

    contrib/xsd2cnd: Moved the XML Schema converter from jackrabbit-core to contrib/xsd2cnd.
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@427674 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/xsd2cnd/src/main/java/org/apache/jackrabbit/xsd2cnd/SchemaConverter.java
+++ contrib/xsd2cnd/src/main/java/org/apache/jackrabbit/xsd2cnd/SchemaConverter.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.jackrabbit.core.nodetype.converter;
+package org.apache.jackrabbit.xsd2cnd;
 
 import org.apache.jackrabbit.core.nodetype.NodeDef;
 import org.apache.jackrabbit.core.nodetype.NodeDefImpl;
--- contrib/xsd2cnd/src/test/java/org/apache/jackrabbit/xsd2cnd/SchemaConverterTest.java
+++ contrib/xsd2cnd/src/test/java/org/apache/jackrabbit/xsd2cnd/SchemaConverterTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.jackrabbit.core.nodetype.converter;
+package org.apache.jackrabbit.xsd2cnd;
 
 
 import org.apache.jackrabbit.core.nodetype.compact.CompactNodeTypeDefReader;
