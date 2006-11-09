8aa8ad7e7e96e44a5c17d2d9fdf29f2f2063a2f5
Angela Schreiber <angela@apache.org>
2006-11-09 09:13:48 +0000

    work in progress
    - some tests for remove/move
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@472823 13f79535-47bb-0310-9956-ffa450edef68

---
+++ contrib/spi/client/src/test/java/org/apache/jackrabbit/jcr2spi/RemoveNodeTest.java
@@ -0,0 +1,121 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.jcr2spi;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.apache.jackrabbit.test.NotExecutableException;
+
+import javax.jcr.RepositoryException;
+import javax.jcr.Node;
+import javax.jcr.PathNotFoundException;
+import javax.jcr.InvalidItemStateException;
+import javax.jcr.Item;
+
+/**
+ * <code>RemoveNodeTest</code>...
+ */
+public class RemoveNodeTest extends RemoveItemTest {
+
+    private static Logger log = LoggerFactory.getLogger(RemoveNodeTest.class);
+
+    protected Item createRemoveItem() throws NotExecutableException, RepositoryException {
+        if (testRootNode.hasNode(nodeName1)) {
+            throw new NotExecutableException("Parent node must not yet contain a child node '" + nodeName1 + "'.");
+        }
+        Node removeNode = testRootNode.addNode(nodeName1, testNodeType);
+        // make sure the new node is persisted.
+        testRootNode.save();
+
+        return removeNode;
+    }
+
+    /**
+     * Transiently removes a persisted node using {@link javax.jcr.Node#remove()}
+     * and test, whether that node cannot be access from its parent.
+     */
+    public void testRemoveNode() throws RepositoryException {
+        removeItem.remove();
+
+        // check if the node has been properly removed
+        try {
+            testRootNode.getNode(nodeName1);
+            fail("Transiently removed node should no longer be accessible from parent node.");
+        } catch (PathNotFoundException e) {
+            // ok , works as expected
+        }
+    }
+
+    /**
+     * Same as {@link #testRemoveNode()}, but calls save() (persisting the removal)
+     * before executing the test.
+     */
+    public void testRemoveNode2() throws RepositoryException, NotExecutableException {
+        removeItem.remove();
+        testRootNode.save();
+        try {
+            testRootNode.getNode(nodeName1);
+            fail("Persistently removed node should no longer be accessible from parent node.");
+        } catch (PathNotFoundException e) {
+            // ok , works as expected
+        }
+    }
+
+    /**
+     * A removed node must throw InvalidItemStateException upon any call to a
+     * node specific method.
+     */
+    public void testInvalidStateRemovedNode() throws RepositoryException {
+        removeItem.remove();
+        try {
+            ((Node)removeItem).getPrimaryNodeType();
+            fail("Calling getPrimaryNodeType() on a removed node must throw InvalidItemStateException.");
+        } catch (InvalidItemStateException e) {
+            //ok
+        }
+
+        try {
+            ((Node)removeItem).getProperty(jcrPrimaryType);
+            fail("Calling getProperty(String) on a removed node must throw InvalidItemStateException.");
+        } catch (InvalidItemStateException e) {
+            //ok
+        }
+    }
+
+    /**
+     * Same as {@link #testInvalidStateRemovedNode()} but calls save() before
+     * executing the test.
+     */
+    public void testInvalidStateRemovedNode2() throws RepositoryException {
+        removeItem.remove();
+        testRootNode.save();
+
+        try {
+            ((Node)removeItem).getPrimaryNodeType();
+            fail("Calling getPrimaryNodeType() on a removed node must throw InvalidItemStateException.");
+        } catch (InvalidItemStateException e) {
+            //ok
+        }
+
+        try {
+            ((Node)removeItem).getProperty(jcrPrimaryType);
+            fail("Calling getProperty(String) on a removed node must throw InvalidItemStateException.");
+        } catch (InvalidItemStateException e) {
+            //ok
+        }
+    }
+}
