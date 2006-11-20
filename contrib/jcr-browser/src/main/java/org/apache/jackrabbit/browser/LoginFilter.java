5b5b84224d419b87942f1bbf05971fdb582cc682
Edgar Ruben Poce <edgarpoce@apache.org>
2006-11-20 23:07:30 +0000

    adding missing license headers to jcr-browser
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@477390 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/jcr-browser/src/main/java/org/apache/jackrabbit/browser/LoginFilter.java
+++ contrib/jcr-browser/src/main/java/org/apache/jackrabbit/browser/LoginFilter.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.jackrabbit.browser;
 
 import javax.jcr.Session;
@@ -12,6 +28,9 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.apache.jackrabbit.command.CommandHelper;
 
+/**
+ * Filter that binds the jcr session to http session
+ */
 public class LoginFilter implements Filter {
 
 	public void destroy() {
@@ -23,7 +42,6 @@ public class LoginFilter implements Filter {
 
 		chain.doFilter(request, response);
 
-		// bind jcr session to http session on login
 		try {
 			HttpServletRequest httpRequest = (HttpServletRequest) request;
 			if (httpRequest.getSession()
