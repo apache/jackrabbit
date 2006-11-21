4768c7de0faebb102e94810c19bb4c8c32ff50f1
Angela Schreiber <angela@apache.org>
2006-11-21 10:27:12 +0000

    JCR-612 : Restructure the Jackrabbit source tree
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@477608 13f79535-47bb-0310-9956-ffa450edef68

---
+++ webapp/src/main/java/org/apache/jackrabbit/j2ee/RepositoryAccessServlet.java
@@ -0,0 +1,224 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.j2ee;
+
+import org.apache.jackrabbit.rmi.client.ClientRepositoryFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.jcr.Repository;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import java.net.MalformedURLException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.Enumeration;
+import java.util.Properties;
+
+/**
+ * This Class implements a servlet that is used as unified mechanism to retrieve
+ * a jcr repository either through JNID, RMI or JCRWebdavServer.
+ */
+public class RepositoryAccessServlet extends HttpServlet {
+
+    /**
+     * default logger
+     */
+    private static final Logger log = LoggerFactory.getLogger(RepositoryAccessServlet.class);
+
+    /**
+     * the 'repository-name' init parameter
+     */
+    public final static String INIT_PARAM_REPOSITORY_NAME = "repository-name";
+
+    /**
+     * the 'rmi-uri' init parameter
+     */
+    public final static String INIT_PARAM_RMI_URI = "rmi-uri";
+
+    /**
+     * the 'missing-auth-mapping' init parameter
+     */
+    //public final static String INIT_PARAM_MISSING_AUTH_MAPPING = "missing-auth-mapping";
+
+    private static final String CTX_ATTR_REPOSITORY = "jcr.repository";
+
+    private static final String CTX_ATTR_REPOSITORY_NAME = "jcr.repository.name";
+
+    private static final String CTX_ATTR_REPOSITORY_RMI_URI = "jcr.repository.rmiURI";
+
+    private static final String CTX_ATTR_REPOSITORY_JNDI_CONTEXT = "jcr.repository.jndiContext";
+
+    /**
+     * Initializes this servlet
+     *
+     * @throws javax.servlet.ServletException
+     */
+    public void init() throws ServletException {
+        log.info("RepositoryAccessServlet initializing...");
+        // fetching the name
+        String repositoryName = getServletConfig().getInitParameter(INIT_PARAM_REPOSITORY_NAME);
+        if (repositoryName == null) {
+            repositoryName = "default";
+        }
+        getServletContext().setAttribute(CTX_ATTR_REPOSITORY_NAME, repositoryName);
+
+        // fetching the rmiuri
+        getServletContext().setAttribute(CTX_ATTR_REPOSITORY_RMI_URI, getRMIUri());
+
+        // setup initial context
+        getServletContext().setAttribute(CTX_ATTR_REPOSITORY_JNDI_CONTEXT, getInitialContext());
+
+        log.info("RepositoryAccessServlet initialized.");
+    }
+
+    private InitialContext getInitialContext() {
+        // retrieve JNDI Context environment
+        try {
+            Properties env = new Properties();
+            Enumeration names = getServletConfig().getInitParameterNames();
+            while (names.hasMoreElements()) {
+                String name = (String) names.nextElement();
+                if (name.startsWith("java.naming.")) {
+                    String initParam = getServletConfig().getInitParameter(name);
+                    if (initParam.equals("")) {
+                        log.info("  ignoring empty JNDI init param: " + name);
+                    } else {
+                        env.put(name, initParam);
+                        log.info("  adding property to JNDI environment: " + name + "=" + initParam);
+                    }
+                }
+            }
+            return new InitialContext(env);
+        } catch (NamingException e) {
+            log.error("Create initial context: " + e.toString());
+            return null;
+        }
+    }
+
+    private String getRMIUri() {
+        // setup repository name
+        return getServletConfig().getInitParameter(INIT_PARAM_RMI_URI);
+    }
+
+    /**
+     * tries to retrieve the repository using RMI
+     */
+    private static Repository getRepositoryByJNDI(ServletContext ctx) {
+        // acquire via JNDI
+        String repositoryName = (String) ctx.getAttribute(CTX_ATTR_REPOSITORY_NAME);
+        InitialContext jndiContext = (InitialContext) ctx.getAttribute(CTX_ATTR_REPOSITORY_JNDI_CONTEXT);
+        if (jndiContext == null) {
+            return null;
+        }
+        try {
+            Repository r = (Repository) jndiContext.lookup(repositoryName);
+            log.info("Acquired repository via JNDI.");
+            return r;
+        } catch (NamingException e) {
+            log.error("Error while retrieving repository using JNDI (name=" + repositoryName + "): " + e);
+            return null;
+        }
+    }
+
+    /**
+     * tries to retrieve the repository using RMI
+     */
+    private static Repository getRepositoryByRMI(ServletContext ctx) {
+        // acquire via RMI
+        String rmiURI = (String) ctx.getAttribute(CTX_ATTR_REPOSITORY_RMI_URI);
+        if (rmiURI == null) {
+            return null;
+        }
+        log.info("  trying to retrieve repository using rmi. uri=" + rmiURI);
+        ClientFactoryDelegater cfd;
+        try {
+            Class clazz = Class.forName("org.apache.jackrabbit.j2ee.RMIClientFactoryDelegater");
+            cfd = (ClientFactoryDelegater) clazz.newInstance();
+        } catch (NoClassDefFoundError e) {
+            log.error("Unable to locate RMI ClientRepositoryFactory. jcr-rmi.jar missing? " + e.toString());
+            return null;
+        } catch (Exception e) {
+            log.error("Unable to locate RMI ClientRepositoryFactory. jcr-rmi.jar missing?" + e.toString());
+            return null;
+        }
+
+        try {
+            Repository r = cfd.getRepository(rmiURI);
+            log.info("Acquired repository via RMI.");
+            return r;
+        } catch (Exception e) {
+            log.error("Error while retrieving repository using RMI: " + e);
+            return null;
+        }
+    }
+
+    /**
+     * Returns the JSR170 repository
+     *
+     * @return a jsr170 repository
+     * @throws IllegalStateException if the repository is not available in the context.
+     */
+    public static Repository getRepository(ServletContext ctx) {
+        Repository repository = (Repository) ctx.getAttribute(CTX_ATTR_REPOSITORY);
+        if (repository != null) {
+            return repository;
+        } else {
+            repository = getRepositoryByRMI(ctx);
+        }
+        // try to retrieve via jndi
+        if (repository == null) {
+            repository = getRepositoryByJNDI(ctx);
+        }
+        // error
+        if (repository == null) {
+            log.error("The repository is not available. Check config of 'RepositoryAccessServlet'.");
+            throw new IllegalStateException("The repository is not available.");
+        } else {
+            ctx.setAttribute(CTX_ATTR_REPOSITORY, repository);
+            log.info(repository.getDescriptor(Repository.REP_NAME_DESC) + " v" + repository.getDescriptor(Repository.REP_VERSION_DESC));
+            return repository;
+        }
+    }
+}
+
+/**
+ * optional class for RMI, will only be used, if RMI client is present
+ */
+abstract class ClientFactoryDelegater {
+
+    public abstract Repository getRepository(String uri)
+            throws RemoteException, MalformedURLException, NotBoundException;
+}
+
+/**
+ * optional class for RMI, will only be used, if RMI server is present
+ */
+class RMIClientFactoryDelegater extends ClientFactoryDelegater {
+
+    // only used to enforce linking upon Class.forName()
+    static String FactoryClassName = ClientRepositoryFactory.class.getName();
+
+    public Repository getRepository(String uri)
+            throws MalformedURLException, NotBoundException, RemoteException {
+        System.setProperty("java.rmi.server.useCodebaseOnly", "true");
+        return new ClientRepositoryFactory().getRepository(uri);
+    }
+}
---
+++ webapp/src/main/webapp/index.jsp
@@ -0,0 +1,97 @@
+<%@ page import="org.apache.jackrabbit.j2ee.JCRWebdavServerServlet,
+		 org.apache.jackrabbit.j2ee.RepositoryAccessServlet,
+		 org.apache.jackrabbit.j2ee.SimpleWebdavServlet,
+                 javax.jcr.Repository"%><%
+%><html>
+<head>
+<title>Jackrabbit JCR-Server</title>
+</head>
+<body style="font-family:monospace">
+<h1>JCR-Server</h1>
+<h3>JCR-Server provides 2 views</h3><p/>
+<ol>
+    <li>
+        <b>Filebased View</b> (SimpleWebdavServlet)<p/>
+        Filebased ("Simple") WebDAV View to the JSR170 repository.<p/>
+        Enter the following URL to your WebDAV client:<br>
+        http://&lt;<i>host</i>&gt;:&lt;<i>port</i>&gt;<%= request.getContextPath() %><%= SimpleWebdavServlet.getPathPrefix(pageContext.getServletContext()) %>/&lt;<i>workspace name</i>&gt;/
+        <p/>
+        <ul>
+            <li><a href="<%= request.getContextPath() %><%= SimpleWebdavServlet.getPathPrefix(pageContext.getServletContext()) %>/default/">Browser View</a></li>
+            <li>Context Path: <%= request.getContextPath() %></li>
+            <li>Resource Path Prefix: <%= SimpleWebdavServlet.getPathPrefix(pageContext.getServletContext()) %></li>
+            <li>Workspace Name: see /WEB-INF/repository/repository.xml (Default = 'default')</li>
+            <li>Source: /jcr-server/server/webdav/simple</li>
+        </ul>
+        <p/>
+    </li>
+    <li>
+        <b>Item View</b> (JCRServerServlet)<p/>
+        Itembased WebDAV View to the JSR170 repository, mapping the functionality
+        provided by JSR170 to WebDAV, in order to allow remoting of JSR170 via
+        WebDAV. Some more details regarding remoting are available as initial
+        draft "<a href="http://www.day.com/jsr170/server/JCR_Webdav_Protocol.zip">JCR_Webdav_Protocol.zip</a>".
+        In addition the implementation attempts to cover functionality of RFC 2518 and
+        its extensions wherever possible, namely<br>
+        <ul>
+            <li><a href="http://www.ietf.org/rfc/rfc2518.txt">RFC 2518 (WebDAV 1,2)</a></li>
+            <li><a href="http://www.ietf.org/rfc/rfc3253.txt">RFC 3253 (DeltaV)</a></li>
+            <li><a href="http://www.ietf.org/rfc/rfc3648.txt">RFC 3648 (Ordering)</a></li>
+            <li><a href="http://greenbytes.de/tech/webdav/draft-reschke-webdav-search-latest.html">Internet Draft WebDAV Search</a>.</li>
+        </ul>
+        <p/>
+        Enter the following URL to your WebDAV client:<br>
+        http://&lt;<i>host</i>&gt;:&lt;<i>port</i>&gt;<%= request.getContextPath() %><%= JCRWebdavServerServlet.getPathPrefix(pageContext.getServletContext()) %>/
+        <p/>
+        <ul>
+            <li>Browser View: - Not Available - ("<%= request.getContextPath() %><%= JCRWebdavServerServlet.getPathPrefix(pageContext.getServletContext()) %>/")</li>
+            <li>Context Path: <%= request.getContextPath() %></li>
+            <li>Resource Path Prefix: <%= JCRWebdavServerServlet.getPathPrefix(pageContext.getServletContext()) %></li>
+            <li>Workspace Name: - Not required - (available workspaces are mapped as resources)</li>
+            <li>Source: /jcr-server/server/webdav/jcr</li>
+        </ul>
+    </li>
+</ol>
+<p/><p/>
+<h3>Basic overview</h3><p/>
+<ol>
+    <li><b>webdav module</b> (library only)<p/>
+      <ul>
+          <li>aim: JSR170 independent WebDAV library</li>
+          <li>packages: org.apache.jackrabbit.webdav</li>
+      </ul>
+      <p/>
+    </li>
+    <li><b>server module</b><p/>
+      <ul>
+          <li>aim: server and server-side WebDAV implementation</li>
+          <li>packages:
+              <br>- org.apache.jackrabbit.server
+              <br>- org.apache.jackrabbit.server.jcr = jcr-server specific server part
+              <br>- org.apache.jackrabbit.server.io  = import/export
+              <br>- org.apache.jackrabbit.webdav.simple = simple (filebased) webdav implementation
+              <br>- org.apache.jackrabbit.webdav.jcr = jcr-server (itembased) webdav implementation
+          </li>
+      </ul>
+      <p/>
+    </li>
+    <li><b>webapp module</b> (jackrabbit-server)<p/>
+      <ul>
+          <li>aim: contains the webapp</li>
+          <li>packages: org.apache.jackrabbit.j2ee (servlets)</li>
+      </ul>
+      <p/>
+    </li>
+    <li><b>client module</b><p/>
+      <ul>
+          <li>aim: provide jcr-client and WebDAV transport layer for the client</li>
+          <li>packages: - Not Available -</li>
+      </ul>
+    </li>
+</ol>
+<%
+    Repository rep = RepositoryAccessServlet.getRepository(pageContext.getServletContext());
+
+%><hr size="1"><em>Powered by <a href="<%= rep.getDescriptor(Repository.REP_VENDOR_URL_DESC) %>"><%= rep.getDescriptor(Repository.REP_NAME_DESC)%></a> version <%= rep.getDescriptor(Repository.REP_VERSION_DESC) %>.</em>
+</body>
+</html>
