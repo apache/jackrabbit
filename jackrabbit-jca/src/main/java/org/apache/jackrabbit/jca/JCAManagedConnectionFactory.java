878f0e8a8a0053d60a5370468eb484a71a2aa354
Jukka Zitting <jukka@apache.org>
2007-02-22 16:17:55 +0000

    JCR-761: Use only J2EE 1.3 methods
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@510576 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-jca/src/main/java/org/apache/jackrabbit/jca/JCAManagedConnectionFactory.java
+++ jackrabbit-jca/src/main/java/org/apache/jackrabbit/jca/JCAManagedConnectionFactory.java
@@ -290,8 +290,10 @@ public final class JCAManagedConnectionFactory
                 log("Created repository (" + repository + ")");
             } catch (RepositoryException e) {
                 log("Failed to create repository", e);
-                throw new ResourceException(
+                ResourceException exception = new ResourceException(
                         "Failed to create session: " + e.getMessage());
+                exception.setLinkedException(e);
+                throw exception;
             }
         }
     }
