53fe54515d4b4aa5459dff8e67ef411a94e28eea
Marcel Reutegger <mreutegg@apache.org>
2007-02-28 14:56:09 +0000

    Implement Serializable for QValue classes. Added test case for binary QValue.
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@512786 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/spi/commons/src/test/java/org/apache/jackrabbit/value/QValueTest.java
+++ contrib/spi/commons/src/test/java/org/apache/jackrabbit/value/QValueTest.java
@@ -21,9 +21,18 @@ import junit.framework.TestCase;
 import javax.jcr.PropertyType;
 import javax.jcr.RepositoryException;
 import java.util.Calendar;
+import java.util.Arrays;
 import java.io.InputStream;
 import java.io.IOException;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.io.FileInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ObjectInputStream;
+import java.io.BufferedInputStream;
 
 import org.apache.jackrabbit.util.ISO8601;
 import org.apache.jackrabbit.uuid.UUID;
@@ -190,5 +199,52 @@ public class QValueTest extends TestCase {
     public void testBinaryValueType() throws IOException {
         QValue v = factory.create(new byte[] {'a', 'b', 'c'});
         assertTrue(v.getType() == PropertyType.BINARY);
+    }
+
+    public void testBinarySerializable() throws Exception {
+        runBinarySerializableTest(1); // 1k
+        runBinarySerializableTest(10); // 10k
+        runBinarySerializableTest(100); // 100k
+        runBinarySerializableTest(1000); // 1M
+    }
+
+    /**
+     * Runs binary serializable test using a stream with a size of kBytes.
+     * @param size in kBytes.
+     */
+    private void runBinarySerializableTest(int size) throws Exception {
+        File tmp = File.createTempFile("test", "bin");
+        OutputStream out = new FileOutputStream(tmp);
+        byte[] stuff = new byte[1024];
+        Arrays.fill(stuff, (byte) 7);
+        for (int i = 0; i < size; i++) {
+            out.write(stuff);
+        }
+        out.close();
+        InputStream in = new FileInputStream(tmp);
+        QValue v = factory.create(in);
+        in.close();
+        tmp.delete();
+        ByteArrayOutputStream bout = new ByteArrayOutputStream();
+        ObjectOutputStream oout = new ObjectOutputStream(bout);
+        oout.writeObject(v);
+        oout.close();
+        ByteArrayInputStream bin = new ByteArrayInputStream(bout.toByteArray());
+        ObjectInputStream oin = new ObjectInputStream(bin);
+        QValue serValue = (QValue) oin.readObject();
+        try {
+            InputStream in1 = new BufferedInputStream(v.getStream());
+            InputStream in2 = new BufferedInputStream(serValue.getStream());
+            int i;
+            while ((i = in1.read()) > -1) {
+                assertEquals(i, in2.read());
+            }
+            assertEquals(in2.read(), -1);
+            in1.close();
+            in2.close();
+        } finally {
+            v.discard();
+            serValue.discard();
+        }
     }
 }
