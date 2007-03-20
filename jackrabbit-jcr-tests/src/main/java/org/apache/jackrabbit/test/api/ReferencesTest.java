60854e4d83c9c44a482b7968de747ca7d3cd84f9
Julian Reschke <reschke@apache.org>
2007-03-20 14:16:22 +0000

    fix indentation
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@520400 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-jcr-tests/src/main/java/org/apache/jackrabbit/test/api/ReferencesTest.java
+++ jackrabbit-jcr-tests/src/main/java/org/apache/jackrabbit/test/api/ReferencesTest.java
@@ -35,132 +35,132 @@ public class ReferencesTest extends AbstractJCRTest {
      */
     public void testReferences() throws RepositoryException {
         Node n1 = testRootNode.addNode(nodeName1, testNodeType);
-	n1.addMixin(mixReferenceable);
+        n1.addMixin(mixReferenceable);
         // with some impls. the mixin type has only affect upon save
         testRootNode.save();
 
         // create references: n2.p1 -> n1
-	Node n2 = testRootNode.addNode(nodeName2, testNodeType);
-	Property p1 = n2.setProperty(propertyName1, new Value[]{superuser.getValueFactory().createValue(n1)});
-	testRootNode.save();
-	PropertyIterator iter = n1.getReferences();
-	if (iter.hasNext()) {
-	    assertEquals("Wrong referer", iter.nextProperty().getPath(), p1.getPath());
-	} else {
-	    fail("no referer");
-	}
+        Node n2 = testRootNode.addNode(nodeName2, testNodeType);
+        Property p1 = n2.setProperty(propertyName1, new Value[]{superuser.getValueFactory().createValue(n1)});
+        testRootNode.save();
+        PropertyIterator iter = n1.getReferences();
+        if (iter.hasNext()) {
+            assertEquals("Wrong referer", iter.nextProperty().getPath(), p1.getPath());
+        } else {
+            fail("no referer");
+        }
 
-	// create references: n3.p1 -> n1
-	Node n3 = testRootNode.addNode(nodeName3, testNodeType);
-	n3.setProperty(propertyName1, n1);
-	testRootNode.save();
-	iter = n1.getReferences();
-	while (iter.hasNext()) {
-	    Property p = iter.nextProperty();
-	    if (n2 != null && p.getParent().getPath().equals(n2.getPath())) {
-		n2 = null;
-	    } else if (n3 != null && p.getParent().getPath().equals(n3.getPath())) {
-		n3 = null;
-	    } else {
-		fail("too many referers: " + p.getPath());
-	    }
-	}
-	if (n2 != null) {
-	    fail("referer not in references set: " + n2.getPath());
-	}
-	if (n3 != null) {
-	    fail("referer not in references set: " + n3.getPath());
-	}
+        // create references: n3.p1 -> n1
+        Node n3 = testRootNode.addNode(nodeName3, testNodeType);
+        n3.setProperty(propertyName1, n1);
+        testRootNode.save();
+        iter = n1.getReferences();
+        while (iter.hasNext()) {
+            Property p = iter.nextProperty();
+            if (n2 != null && p.getParent().getPath().equals(n2.getPath())) {
+                n2 = null;
+            } else if (n3 != null && p.getParent().getPath().equals(n3.getPath())) {
+                n3 = null;
+            } else {
+                fail("too many referers: " + p.getPath());
+            }
+        }
+        if (n2 != null) {
+            fail("referer not in references set: " + n2.getPath());
+        }
+        if (n3 != null) {
+            fail("referer not in references set: " + n3.getPath());
+        }
 
-	// remove reference n3.p1 -> n1
-	testRootNode.getNode(nodeName3).getProperty(propertyName1).remove();
-	testRootNode.save();
-	iter = n1.getReferences();
-	if (iter.hasNext()) {
-	    assertEquals("Wrong referer", iter.nextProperty().getParent().getPath(), testRootNode.getNode(nodeName2).getPath());
-	} else {
-	    fail("no referer");
-	}
-	if (iter.hasNext()) {
-	    fail("too many referers: " + iter.nextProperty().getPath());
-	}
+        // remove reference n3.p1 -> n1
+        testRootNode.getNode(nodeName3).getProperty(propertyName1).remove();
+        testRootNode.save();
+        iter = n1.getReferences();
+        if (iter.hasNext()) {
+            assertEquals("Wrong referer", iter.nextProperty().getParent().getPath(), testRootNode.getNode(nodeName2).getPath());
+        } else {
+            fail("no referer");
+        }
+        if (iter.hasNext()) {
+            fail("too many referers: " + iter.nextProperty().getPath());
+        }
 
-	// remove reference n2.p1 -> n1
-	testRootNode.getNode(nodeName2).getProperty(propertyName1).setValue(new Value[0]);
-	testRootNode.save();
-	iter = n1.getReferences();
-	if (iter.hasNext()) {
-	    fail("too many referers: " + iter.nextProperty().getPath());
-	}
+        // remove reference n2.p1 -> n1
+        testRootNode.getNode(nodeName2).getProperty(propertyName1).setValue(new Value[0]);
+        testRootNode.save();
+        iter = n1.getReferences();
+        if (iter.hasNext()) {
+            fail("too many referers: " + iter.nextProperty().getPath());
+        }
     }
 
     /**
      * Tests Property.getNode();
      */
     public void testReferenceTarget() throws RepositoryException {
-	Node n1 = testRootNode.addNode(nodeName1, testNodeType);
-	n1.addMixin(mixReferenceable);
+        Node n1 = testRootNode.addNode(nodeName1, testNodeType);
+        n1.addMixin(mixReferenceable);
         // with some impls. the mixin type has only affect upon save
         testRootNode.save();
 
         // create references: n2.p1 -> n1
-	Node n2 = testRootNode.addNode(nodeName2, testNodeType);
-	n2.setProperty(propertyName1, n1);
-	testRootNode.save();
-	assertEquals("Wrong reference target.", n2.getProperty(propertyName1).getNode(), n1);
-	n2.remove();
-	testRootNode.save();
+        Node n2 = testRootNode.addNode(nodeName2, testNodeType);
+        n2.setProperty(propertyName1, n1);
+        testRootNode.save();
+        assertEquals("Wrong reference target.", n2.getProperty(propertyName1).getNode(), n1);
+        n2.remove();
+        testRootNode.save();
     }
 
     /**
      * Tests changing a reference property
      */
     public void testAlterReference() throws RepositoryException {
-	Node n1 = testRootNode.addNode(nodeName1, testNodeType);
-	n1.addMixin(mixReferenceable);
-	Node n2 = testRootNode.addNode(nodeName2, testNodeType);
-	n2.addMixin(mixReferenceable);
+        Node n1 = testRootNode.addNode(nodeName1, testNodeType);
+        n1.addMixin(mixReferenceable);
+        Node n2 = testRootNode.addNode(nodeName2, testNodeType);
+        n2.addMixin(mixReferenceable);
         // with some impls. the mixin type has only affect upon save
         testRootNode.save();
 
         // create references: n3.p1 -> n1
-	Node n3 = testRootNode.addNode(nodeName3, testNodeType);
-	n3.setProperty(propertyName1, n1);
-	testRootNode.save();
-	assertEquals("Wrong reference target.", n3.getProperty(propertyName1).getNode(), n1);
-	PropertyIterator iter = n1.getReferences();
-	if (iter.hasNext()) {
-	    assertEquals("Wrong referer", iter.nextProperty().getParent().getPath(), n3.getPath());
-	} else {
-	    fail("no referer");
-	}
-	if (iter.hasNext()) {
-	    fail("too many referers: " + iter.nextProperty().getPath());
-	}
-	// change reference: n3.p1 -> n2
-	n3.setProperty(propertyName1, n2);
-	n3.save();
-	assertEquals("Wrong reference target.", n3.getProperty(propertyName1).getNode(), n2);
-	iter = n1.getReferences();
-	if (iter.hasNext()) {
-	    fail("too many referers: " + iter.nextProperty().getPath());
-	}
-	iter = n2.getReferences();
-	if (iter.hasNext()) {
-	    assertEquals("Wrong referer", iter.nextProperty().getParent().getPath(), n3.getPath());
-	} else {
-	    fail("no referers");
-	}
-	if (iter.hasNext()) {
-	    fail("too many referers: " + iter.nextProperty().getPath());
-	}
+        Node n3 = testRootNode.addNode(nodeName3, testNodeType);
+        n3.setProperty(propertyName1, n1);
+        testRootNode.save();
+        assertEquals("Wrong reference target.", n3.getProperty(propertyName1).getNode(), n1);
+        PropertyIterator iter = n1.getReferences();
+        if (iter.hasNext()) {
+            assertEquals("Wrong referer", iter.nextProperty().getParent().getPath(), n3.getPath());
+        } else {
+            fail("no referer");
+        }
+        if (iter.hasNext()) {
+            fail("too many referers: " + iter.nextProperty().getPath());
+        }
+        // change reference: n3.p1 -> n2
+        n3.setProperty(propertyName1, n2);
+        n3.save();
+        assertEquals("Wrong reference target.", n3.getProperty(propertyName1).getNode(), n2);
+        iter = n1.getReferences();
+        if (iter.hasNext()) {
+            fail("too many referers: " + iter.nextProperty().getPath());
+        }
+        iter = n2.getReferences();
+        if (iter.hasNext()) {
+            assertEquals("Wrong referer", iter.nextProperty().getParent().getPath(), n3.getPath());
+        } else {
+            fail("no referers");
+        }
+        if (iter.hasNext()) {
+            fail("too many referers: " + iter.nextProperty().getPath());
+        }
 
-	// clear reference by overwriting by other type
-	n3.setProperty(propertyName1, "Hello, world.");
-	n3.save();
-	iter = n2.getReferences();
-	if (iter.hasNext()) {
-	    fail("too many referers: " + iter.nextProperty().getPath());
-	}
+        // clear reference by overwriting by other type
+        n3.setProperty(propertyName1, "Hello, world.");
+        n3.save();
+        iter = n2.getReferences();
+        if (iter.hasNext()) {
+            fail("too many referers: " + iter.nextProperty().getPath());
+        }
     }
 }
