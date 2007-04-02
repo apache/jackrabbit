ab5638f247d11f20a841199b917df80caaa0828d
Julian Reschke <reschke@apache.org>
2007-04-02 12:56:47 +0000

    JCR-830: allow tests to run with repositories that do support binary properties only on automatically created child nodes of type nt:resource.
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@524776 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-jcr-tests/src/main/java/org/apache/jackrabbit/test/api/SetValueBinaryTest.java
+++ jackrabbit-jcr-tests/src/main/java/org/apache/jackrabbit/test/api/SetValueBinaryTest.java
@@ -32,8 +32,11 @@ import java.io.IOException;
  * Configuration requirements:<br/> The node at {@link #testRoot} must allow a
  * child node of type {@link #testNodeType} with name {@link #nodeName1}. The
  * node type {@link #testNodeType} must define a single value binary property
- * with name {@link #propertyName1}.
- *
+ * with name {@link #propertyName1}. <br>As a special case, if the specified node
+ * type automatically adds a jcr:content child node of type nt:resource, and
+ * <code>propertyName1</code> is specified as "jcr:data", that binary property
+ * is used instead.
+ * 
  * @test
  * @sources SetValueBinaryTest.java
  * @executeClass org.apache.jackrabbit.test.api.SetValueBinaryTest
@@ -70,6 +73,14 @@ public class SetValueBinaryTest extends AbstractJCRTest {
 
         // create a new node under the testRootNode
         node = testRootNode.addNode(nodeName1, testNodeType);
+        testRootNode.save();
+
+        // special case for repositories that do allow binary property
+        // values, but only on jcr:content/jcr:data
+        if (propertyName1.equals("jcr:data") && node.hasNode("jcr:content")
+            && node.getNode("jcr:content").isNodeType("nt:resource") && ! node.hasProperty("jcr:data")) {
+            node = node.getNode("jcr:content");
+        }
 
         // create a new single-value property and save it
         property1 = node.setProperty(propertyName1, superuser.getValueFactory().createValue(new ByteArrayInputStream(new byte[0])));
