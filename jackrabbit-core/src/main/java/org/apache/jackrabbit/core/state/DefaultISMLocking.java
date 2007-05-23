908c160149cdaecbd2bfee3d7aec585a5d03faca
Marcel Reutegger <mreutegg@apache.org>
2007-05-23 13:12:26 +0000

    JCR-314: Fine grained locking in SharedItemStateManager
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@540944 13f79535-47bb-0310-9956-ffa450edef68

---
+++ jackrabbit-core/src/main/java/org/apache/jackrabbit/core/state/DefaultISMLocking.java
@@ -0,0 +1,113 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.core.state;
+
+import org.apache.jackrabbit.core.ItemId;
+import EDU.oswego.cs.dl.util.concurrent.ReadWriteLock;
+import EDU.oswego.cs.dl.util.concurrent.ReentrantWriterPreferenceReadWriteLock;
+import EDU.oswego.cs.dl.util.concurrent.Sync;
+
+/**
+ * <code>DefaultISMLocking</code> implements the default locking strategy using
+ * coarse grained locking on an ItemStateManager wide read-write lock. E.g.
+ * while a write lock is held, no read lock can be acquired.
+ */
+public class DefaultISMLocking implements ISMLocking {
+
+    /**
+     * JCR-447: deadlock might occur when this manager is still write-locked and events are dispatched.
+     */
+    private boolean noLockHack = false;
+
+    /**
+     * The internal read-write lock.
+     */
+    private final ReadWriteLock rwLock = new ReentrantWriterPreferenceReadWriteLock() {
+        /**
+         * Allow reader when there is no active writer, or current thread owns
+         * the write lock (reentrant).
+         */
+        protected boolean allowReader() {
+            return activeWriter_ == null
+                    || activeWriter_ == Thread.currentThread()
+                    || noLockHack;
+        }
+    };
+
+    /**
+     * enables or disables the write-lock hack.
+     *
+     * @param noLockHack
+     */
+    public void setNoLockHack(boolean noLockHack) {
+        this.noLockHack = noLockHack;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public ReadLock acquireReadLock(ItemId id)
+            throws InterruptedException {
+        return new ReadLockImpl(rwLock.readLock());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public WriteLock acquireWriteLock(ChangeLog changeLog)
+            throws InterruptedException {
+        return new WriteLock() {
+
+            {
+                rwLock.writeLock().acquire();
+            }
+
+            /**
+             * {@inheritDoc}
+             */
+            public void release() {
+                rwLock.writeLock().release();
+            }
+
+            /**
+             * {@inheritDoc}
+             */
+            public ReadLock downgrade() throws InterruptedException {
+                ReadLock rLock = new ReadLockImpl(rwLock.readLock());
+                release();
+                return rLock;
+            }
+        };
+    }
+
+    private static final class ReadLockImpl implements ReadLock {
+
+        private final Sync readLock;
+
+        private ReadLockImpl(Sync readLock) throws InterruptedException {
+            this.readLock = readLock;
+            this.readLock.acquire();
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        public void release() {
+            readLock.release();
+        }
+    }
+}
