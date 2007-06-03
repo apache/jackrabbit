e4831a13b67bee5bb78e73bcbea45292498983b8
Jukka Zitting <jukka@apache.org>
2007-06-03 00:38:43 +0000

    JCR-956: Repository access and binding servlets for RMI
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@543813 13f79535-47bb-0310-9956-ffa450edef68

---
+++ jackrabbit-jcr-rmi/src/main/java/org/apache/jackrabbit/rmi/client/RMIRepositoryFactory.java
@@ -0,0 +1,69 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.rmi.client;
+
+import java.net.MalformedURLException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+import javax.jcr.Repository;
+import javax.jcr.RepositoryException;
+
+import org.apache.jackrabbit.commons.repository.RepositoryFactory;
+
+/**
+ * Factory that looks up a repository from JNDI.
+ *
+ * @since 1.4
+ */
+public class RMIRepositoryFactory implements RepositoryFactory {
+
+    /**
+     * RMI URL of the repository.
+     */
+    private final String url;
+
+    /**
+     * Creates a factory for looking up a repository from the given RMI URL.
+     *
+     * @param url RMI URL of the repository
+     */
+    public RMIRepositoryFactory(String url) {
+        this.url = url;
+    }
+
+    /**
+     * Looks up and returns a repository from the configured RMI URL.
+     *
+     * @return local adapter for the remote repository
+     * @throws RepositoryException if the repository could not be accessed
+     */
+    public Repository getRepository() throws RepositoryException {
+        try {
+            return new ClientRepositoryFactory().getRepository(url);
+        } catch (MalformedURLException e) {
+            throw new RepositoryException("Invalid repository URL: " + url, e);
+        } catch (NotBoundException e) {
+            throw new RepositoryException("Repository not found: " + url, e);
+        } catch (ClassCastException e) {
+            throw new RepositoryException("Invalid repository: " + url, e);
+        } catch (RemoteException e) {
+            throw new RepositoryException("Repository access error: " + url, e);
+        }
+    }
+
+}
