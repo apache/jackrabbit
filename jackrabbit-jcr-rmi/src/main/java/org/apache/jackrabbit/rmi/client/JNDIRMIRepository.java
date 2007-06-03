822697195c6a57e8b07817e371b6f37d6e7e7e28
Jukka Zitting <jukka@apache.org>
2007-06-03 01:26:33 +0000

    JCR-419: Added support for binding and accessing a remote repository in JNDI
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@543821 13f79535-47bb-0310-9956-ffa450edef68

---
+++ jackrabbit-jcr-rmi/src/main/java/org/apache/jackrabbit/rmi/client/JNDIRMIRepository.java
@@ -0,0 +1,46 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.rmi.client;
+
+import javax.naming.Context;
+
+import org.apache.jackrabbit.commons.repository.ProxyRepository;
+
+/**
+ * Proxy for a remote repository bound in JNDI. The configured repository is
+ * looked up from JNDI lazily during each method call. Thus the JNDI entry
+ * does not need to exist when this class is instantiated. The JNDI entry
+ * can also be replaced with another repository during the lifetime of an
+ * instance of this class.
+ *
+ * @since 1.4
+ */
+public class JNDIRMIRepository extends ProxyRepository {
+
+    /**
+     * Creates a proxy for a remote repository in JNDI.
+     *
+     * @param factory local adapter factory
+     * @param context JNDI context
+     * @param location JNDI location
+     */
+    public JNDIRMIRepository(
+            LocalAdapterFactory factory, Context context, String location) {
+        super(new JNDIRMIRepositoryFactory(factory, context, location));
+    }
+
+}
