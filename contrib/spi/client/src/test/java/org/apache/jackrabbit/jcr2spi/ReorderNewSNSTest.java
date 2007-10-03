4b434b276d83ba044fec6b371ecb424be76d31d1
Angela Schreiber <angela@apache.org>
2007-10-03 15:40:37 +0000

    Tests: stronger tests introduced with rev. 581525 must be followed by correct testing the effect of refresh(false) on NEW nodes. fixing issues introduces with rev. 581525
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@581634 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/spi/client/src/test/java/org/apache/jackrabbit/jcr2spi/ReorderNewSNSTest.java
+++ contrib/spi/client/src/test/java/org/apache/jackrabbit/jcr2spi/ReorderNewSNSTest.java
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory;
 
 import javax.jcr.RepositoryException;
 import javax.jcr.ItemExistsException;
+import javax.jcr.Node;
+import javax.jcr.NodeIterator;
 import javax.jcr.version.VersionException;
 import javax.jcr.nodetype.ConstraintViolationException;
 import javax.jcr.nodetype.NoSuchNodeTypeException;
@@ -22,5 +24,29 @@ public class ReorderNewSNSTest extends ReorderTest {
         child2 = testRootNode.addNode(nodeName2, testNodeType);
         child3 = testRootNode.addNode(nodeName2, testNodeType);
         child4 = testRootNode.addNode(nodeName2, testNodeType);
+    }
+
+    public void testRevertReorder() throws RepositoryException {
+        testRootNode.orderBefore(getRelPath(child4), getRelPath(child2));
+        testOrder(testRootNode, new Node[] { child1, child4, child2, child3});
+
+        // NEW child nodes -> must be removed upon refresh
+        testRootNode.refresh(false);
+        NodeIterator it = testRootNode.getNodes(nodeName2);
+        if (it.hasNext()) {
+            fail("Reverting creation and reordering of new SNSs must remove the children again.");
+        }
+    }
+
+    public void testRevertReorderToEnd() throws RepositoryException {
+        testRootNode.orderBefore(getRelPath(child1), null);
+        testOrder(testRootNode, new Node[] { child2, child3, child4, child1});
+
+        // NEW child nodes -> must be removed upon refresh
+        testRootNode.refresh(false);
+        NodeIterator it = testRootNode.getNodes(nodeName2);
+        if (it.hasNext()) {
+            fail("Reverting creation and reordering of new SNSs must remove the children again.");
+        }
     }
 }
