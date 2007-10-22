44e0b5403d73ecb97b0db62e450d1872012a4c02
Angela Schreiber <angela@apache.org>
2007-10-22 14:13:44 +0000

    JCR-1070 Promotion of SPI from Contrib (copy test cases from contrib/spi/client)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@587104 13f79535-47bb-0310-9956-ffa450edef68

---
+++ jackrabbit-jcr2spi/src/test/java/org/apache/jackrabbit/jcr2spi/HierarchyNodeTest.java
@@ -0,0 +1,116 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.jcr2spi;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.apache.jackrabbit.test.AbstractJCRTest;
+import org.apache.jackrabbit.test.NotExecutableException;
+
+import javax.jcr.Node;
+import javax.jcr.PropertyIterator;
+import javax.jcr.NodeIterator;
+import javax.jcr.RepositoryException;
+import javax.jcr.Property;
+import javax.jcr.Session;
+import java.util.Calendar;
+import java.util.Set;
+import java.util.HashSet;
+
+/**
+ * <code>HierarchyNodeTest</code>...
+ */
+public class HierarchyNodeTest extends AbstractJCRTest {
+
+    private static Logger log = LoggerFactory.getLogger(HierarchyNodeTest.class);
+
+    private Set hierarchyNodeProps = new HashSet();
+    private Set resourceProps = new HashSet();
+
+    private String ntFolder;
+    private String ntFile;
+    private String ntResource;
+
+    private Node fileNode;
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        Session s = testRootNode.getSession();
+        String jcrPrefix = s.getNamespacePrefix(NS_JCR_URI);
+        String ntPrefix = s.getNamespacePrefix(NS_NT_URI);
+
+        ntFolder = ntPrefix + ":folder";
+        ntFile = ntPrefix + ":file";
+        ntResource = ntPrefix + ":resource";
+
+        hierarchyNodeProps.add(jcrPrefix+":primaryType");
+        hierarchyNodeProps.add(jcrPrefix+":created");
+
+        resourceProps.add(jcrPrefix+":primaryType");
+        resourceProps.add(jcrPrefix+":lastModified");
+        resourceProps.add(jcrPrefix+":mimeType");
+        resourceProps.add(jcrPrefix+":data");
+        resourceProps.add(jcrPrefix+":uuid");
+
+        try {
+            Node folder = testRootNode.addNode("folder", ntFolder);
+            fileNode = folder.addNode("file", ntFile);
+
+            Node content = fileNode.addNode(jcrPrefix + ":content", ntResource);
+            content.setProperty(jcrPrefix + ":lastModified", Calendar.getInstance());
+            content.setProperty(jcrPrefix + ":mimeType", "text/plain");
+            content.setProperty(jcrPrefix + ":data", "some plain text");
+
+            testRootNode.save();
+        } catch (RepositoryException e) {
+            throw new NotExecutableException("Cannot create hierarchy nodes.");
+        }
+    }
+
+    public void testGetProperties() throws RepositoryException {
+        Session readSession = helper.getReadOnlySession();
+        dump((Node) readSession.getItem(fileNode.getPath()));
+
+        System.out.println("bla");
+    }
+
+    /** Recursively outputs the contents of the given node. */
+    private void dump(Node node) throws RepositoryException {
+
+        // Then output the properties
+        PropertyIterator properties = node.getProperties();
+        Set set = new HashSet();
+        while (properties.hasNext()) {
+            Property property = properties.nextProperty();
+            set.add(property.getName());
+        }
+
+        if (node.getPrimaryNodeType().getName().equals(ntFolder)) {
+            assertTrue(hierarchyNodeProps.size() == set.size() && hierarchyNodeProps.containsAll(set));
+        } else if (node.getPrimaryNodeType().getName().equals(ntFile)) {
+            assertTrue(hierarchyNodeProps.size() == set.size() && hierarchyNodeProps.containsAll(set));
+        } else if (node.getPrimaryNodeType().getName().equals(ntResource)) {
+            assertTrue(resourceProps.size() == set.size() && resourceProps.containsAll(set));
+        }
+
+        // Finally output all the child nodes recursively
+        NodeIterator nodes = node.getNodes();
+        while (nodes.hasNext()) {
+            dump(nodes.nextNode());
+        }
+    }
+}
---
+++ jackrabbit-jcr2spi/src/test/java/org/apache/jackrabbit/jcr2spi/lock/SessionScopedLockTest.java
@@ -0,0 +1,78 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.jcr2spi.lock;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+import javax.jcr.lock.LockException;
+import javax.jcr.lock.Lock;
+
+/**
+ * <code>SessionScopedLockTest</code>...
+ */
+public class SessionScopedLockTest extends AbstractLockTest {
+
+    private static Logger log = LoggerFactory.getLogger(SessionScopedLockTest.class);
+
+    boolean isSessionScoped() {
+        return true;
+    }
+
+    /**
+     * Test locks are released when session logs out
+     */
+    public void testLockNotLiveAfterLogout() throws RepositoryException {
+        Node testRoot2 = (Node) otherSession.getItem(testRootNode.getPath());
+
+        Node lockedNode2 = testRoot2.addNode(nodeName2, testNodeType);
+        lockedNode2.addMixin(mixLockable);
+        testRoot2.save();
+
+        Lock lock2 = lockedNode2.lock(false, isSessionScoped());
+        otherSession.logout();
+
+        assertFalse(lock2.isLive());
+    }
+
+    /**
+     * Test locks are released when session logs out
+     */
+    public void testNotLockedAfterLogout() throws RepositoryException {
+        Node testRoot2 = (Node) otherSession.getItem(testRootNode.getPath());
+
+        Node lockedNode2 = testRoot2.addNode(nodeName2, testNodeType);
+        lockedNode2.addMixin(mixLockable);
+        testRoot2.save();
+
+        Node n2 = (Node) superuser.getItem(lockedNode2.getPath());
+
+        // remove lock implicit by logout lock-holding session
+        otherSession.logout();
+
+        assertFalse(n2.isLocked());
+        assertFalse(n2.holdsLock());
+        try {
+            n2.getLock();
+            fail("Upon logout of the session a session-scoped lock must be gone.");
+        } catch (LockException e) {
+            // ok
+        }
+    }
+}
