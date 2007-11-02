167435045ac4cd89935d9644ff83652ddff21b45
Jukka Zitting <jukka@apache.org>
2007-11-02 09:25:45 +0000

    JCR-1203: Reorganize core test suite     - Moved initialization code to o.a.j.core.integration.init     - Use the integration test phases for tests
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@591280 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-core/src/test/java/org/apache/jackrabbit/core/integration/init/PropertyTestData.java
+++ jackrabbit-core/src/test/java/org/apache/jackrabbit/core/integration/init/PropertyTestData.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.jackrabbit.init;
+package org.apache.jackrabbit.core.integration.init;
 
 import org.apache.jackrabbit.test.AbstractJCRTest;
 
