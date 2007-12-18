129efdf2b9dc0e9ec3d908ab000482a089b2a315
Marcel Reutegger <mreutegg@apache.org>
2007-12-18 08:35:40 +0000

    JCR-1209: NodeImpl.checkout() calls save() two times
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk/jackrabbit-core@605134 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/jackrabbit/core/NodeImpl.java
+++ src/main/java/org/apache/jackrabbit/core/NodeImpl.java
@@ -3020,13 +3020,26 @@ public class NodeImpl extends ItemImpl implements Node {
         // check lock status
         checkLock();
 
-        Property prop = internalSetProperty(NameConstants.JCR_ISCHECKEDOUT, InternalValue.create(true));
-        prop.save();
-        prop = internalSetProperty(NameConstants.JCR_PREDECESSORS,
+        boolean hasPendingChanges = session.hasPendingChanges();
+
+        Property[] props = new Property[2];
+        props[0] = internalSetProperty(NameConstants.JCR_ISCHECKEDOUT, InternalValue.create(true));
+        props[1] = internalSetProperty(NameConstants.JCR_PREDECESSORS,
                 new InternalValue[]{
                     InternalValue.create(new UUID(getBaseVersion().getUUID()))
                 });
-        prop.save();
+        if (hasPendingChanges) {
+            for (int i = 0; i < props.length; i++) {
+                props[i].save();
+            }
+        } else {
+            try {
+                session.save();
+            } catch (RepositoryException e) {
+                session.refresh(false);
+                throw e;
+            }
+        }
     }
 
     /**
