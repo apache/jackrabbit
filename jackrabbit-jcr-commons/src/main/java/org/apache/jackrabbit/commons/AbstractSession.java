2ab03ace7e96d46b287936250d900d93255f12b7
Jukka Zitting <jukka@apache.org>
2008-01-28 23:30:40 +0000

    JCR-1350: Add a serializing content handler     - Need to unwrap SAXExceptions
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@616104 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-jcr-commons/src/main/java/org/apache/jackrabbit/commons/AbstractSession.java
+++ jackrabbit-jcr-commons/src/main/java/org/apache/jackrabbit/commons/AbstractSession.java
@@ -126,7 +126,14 @@ public abstract class AbstractSession implements Session {
                 getImportContentHandler(parentAbsPath, uuidBehavior);
             new ParsingContentHandler(handler).parse(in);
         } catch (SAXException e) {
-            throw new RepositoryException("XML import failed", e);
+            Throwable exception = e.getException();
+            if (exception instanceof RepositoryException) {
+                throw (RepositoryException) exception;
+            } else if (exception instanceof IOException) {
+                throw (IOException) exception;
+            } else {
+                throw new RepositoryException("XML import failed", e);
+            }
         }
     }
 
