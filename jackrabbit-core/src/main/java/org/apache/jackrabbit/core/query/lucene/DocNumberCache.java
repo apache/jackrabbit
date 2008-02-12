435b13374d38af75496ca3bd0b0e8c82bec86141
Marcel Reutegger <mreutegg@apache.org>
2008-02-12 15:17:27 +0000

    JCR-1377: Reduce memory usage of DocNumberCache
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@620826 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-core/src/main/java/org/apache/jackrabbit/core/query/lucene/DocNumberCache.java
+++ jackrabbit-core/src/main/java/org/apache/jackrabbit/core/query/lucene/DocNumberCache.java
@@ -17,6 +17,7 @@
 package org.apache.jackrabbit.core.query.lucene;
 
 import org.apache.commons.collections.map.LRUMap;
+import org.apache.jackrabbit.uuid.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -93,8 +94,9 @@ final class DocNumberCache {
      */
     void put(String uuid, CachingIndexReader reader, int n) {
         LRUMap cacheSegment = docNumbers[getSegmentIndex(uuid.charAt(0))];
+        UUID key = UUID.fromString(uuid);
         synchronized (cacheSegment) {
-            Entry e = (Entry) cacheSegment.get(uuid);
+            Entry e = (Entry) cacheSegment.get(key);
             if (e != null) {
                 // existing entry
                 // ignore if reader is older than the one in entry
@@ -112,7 +114,7 @@ final class DocNumberCache {
             }
 
             if (e != null) {
-                cacheSegment.put(uuid, e);
+                cacheSegment.put(key, e);
             }
         }
     }
@@ -126,9 +128,10 @@ final class DocNumberCache {
      */
     Entry get(String uuid) {
         LRUMap cacheSegment = docNumbers[getSegmentIndex(uuid.charAt(0))];
+        UUID key = UUID.fromString(uuid);
         Entry entry;
         synchronized (cacheSegment) {
-            entry = (Entry) cacheSegment.get(uuid);
+            entry = (Entry) cacheSegment.get(key);
         }
         if (log.isInfoEnabled()) {
             accesses++;
