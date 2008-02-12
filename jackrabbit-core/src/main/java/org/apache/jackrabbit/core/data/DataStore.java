96966d8aeb4b08dae679b3ec24aa0595639219d8
Thomas Mueller <thomasm@apache.org>
2008-02-12 12:48:15 +0000

    JCR-926 Global data store for binaries: improve documentation
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@620786 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-core/src/main/java/org/apache/jackrabbit/core/data/DataStore.java
+++ jackrabbit-core/src/main/java/org/apache/jackrabbit/core/data/DataStore.java
@@ -111,7 +111,7 @@ public interface DataStore {
      * Get the minimum size of an object that should be stored in this data store.
      * Depending on the overhead and configuration, each store may return a different value.
      *
-     * @return the minimum size
+     * @return the minimum size in bytes
      */
     int getMinRecordLength();
 
