5efc4ef70cb16049e8dc0f186aae6db565f79117
Marcel Reutegger <mreutegg@apache.org>
2008-02-21 17:19:22 +0000

    JCR-1347: Move Jackrabbit Query Parser from core to spi-commons - forgot to adapt package name
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@629877 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-spi-commons/src/main/java/org/apache/jackrabbit/spi/commons/query/qom/LiteralImpl.java
+++ jackrabbit-spi-commons/src/main/java/org/apache/jackrabbit/spi/commons/query/qom/LiteralImpl.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.jackrabbit.core.query.qom;
+package org.apache.jackrabbit.spi.commons.query.qom;
 
 import org.apache.jackrabbit.spi.commons.query.jsr283.qom.Literal;
 import org.apache.jackrabbit.spi.commons.conversion.NamePathResolver;
