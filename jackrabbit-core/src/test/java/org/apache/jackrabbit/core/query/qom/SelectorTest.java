ab2e3e26b91e7b2a3e3016f4ceecb98e0b625022
Marcel Reutegger <mreutegg@apache.org>
2008-03-04 15:54:42 +0000

    JCR-1104: JSR 283 support - selector test cases
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@633523 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-core/src/test/java/org/apache/jackrabbit/core/query/qom/SelectorTest.java
+++ jackrabbit-core/src/test/java/org/apache/jackrabbit/core/query/qom/SelectorTest.java
@@ -18,7 +18,10 @@ package org.apache.jackrabbit.core.query.qom;
 
 import javax.jcr.RepositoryException;
 import javax.jcr.NodeIterator;
+import javax.jcr.nodetype.NodeTypeManager;
+import javax.jcr.nodetype.NoSuchNodeTypeException;
 import javax.jcr.query.Query;
+import javax.jcr.query.InvalidQueryException;
 
 /**
  * <code>SelectorTest</code>...
@@ -34,6 +37,66 @@ public class SelectorTest extends AbstractQOMTest {
         NodeIterator it = q.execute().getNodes();
         while (it.hasNext()) {
             assertTrue("Wrong node type", it.nextNode().isNodeType(testNodeType));
+        }
+    }
+
+    public void testSyntacticallyInvalidName() throws RepositoryException {
+        try {
+            Query q = qomFactory.createQuery(
+                    qomFactory.selector(testNodeType + "["), null, null, null);
+            q.execute();
+            fail("Selector with syntactically invalid name must throw InvalidQueryException");
+        } catch (InvalidQueryException e) {
+            // expected
+        }
+    }
+
+    public void testUnknownNodeType() throws RepositoryException {
+        NodeTypeManager ntMgr = superuser.getWorkspace().getNodeTypeManager();
+        String ntName = testNodeType;
+        for (;;) {
+            try {
+                ntMgr.getNodeType(ntName);
+                ntName += "x";
+            } catch (NoSuchNodeTypeException e) {
+                break;
+            }
+        }
+        Query q = qomFactory.createQuery(qomFactory.selector(ntName),
+                null, null, null);
+        assertFalse("Selector must not select nodes for unknown node type",
+                q.execute().getNodes().hasNext());
+    }
+
+    public void testDuplicateNodeType() throws RepositoryException {
+        try {
+            Query q = qomFactory.createQuery(
+                    qomFactory.join(
+                            qomFactory.selector(testNodeType, "nt"),
+                            qomFactory.selector(testNodeType, "nt"),
+                            JOIN_TYPE_INNER,
+                            qomFactory.descendantNodeJoinCondition("nt", "nt")),
+                    null, null, null);
+            q.execute();
+            fail("Selector with two identical selector names must throw InvalidQueryException");
+        } catch (InvalidQueryException e) {
+            // expected
+        }
+
+        // using one argument selector method
+        try {
+            Query q = qomFactory.createQuery(
+                    qomFactory.join(
+                            qomFactory.selector(testNodeType),
+                            qomFactory.selector(testNodeType),
+                            JOIN_TYPE_INNER,
+                            qomFactory.descendantNodeJoinCondition(
+                                    testNodeType, testNodeType)),
+                    null, null, null);
+            q.execute();
+            fail("Selector with two identical selector names must throw InvalidQueryException");
+        } catch (InvalidQueryException e) {
+            // expected
         }
     }
 }
