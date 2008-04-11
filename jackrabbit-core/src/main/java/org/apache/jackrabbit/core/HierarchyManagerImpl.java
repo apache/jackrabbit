apache/jackrabbit

commit 6c58c33b380624d52d87967f1e5bc7fbafc32e3d

JCR-1104 - JSR 283 support - shareble nodes (work in progress) - improve share-cycle detection
git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@646336 13f79535-47bb-0310-9956-ffa450edef68

jackrabbit-core/src/main/java/org/apache/jackrabbit/core/HierarchyManagerImpl.java
@@ -16,6 +16,11 @@
  */
 package org.apache.jackrabbit.core;
 
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 import org.apache.jackrabbit.core.state.ItemState;
 import org.apache.jackrabbit.core.state.ItemStateException;
 import org.apache.jackrabbit.core.state.ItemStateManager;
@@ -213,6 +218,33 @@ public class HierarchyManagerImpl implements HierarchyManager {
      */
     protected NodeId getParentId(ItemState state) {
         return state.getParentId();
+    }
+
+    /**
+     * Return all parents of a node. A shareable node has possibly more than
+     * one parent.
+     *
+     * @param state item state
+     * @return set of parent <code>NodeId</code>s. If state has no parent,
+     *         array has length <code>0</code>.
+     */
+    protected Set getParentIds(ItemState state) {
+        if (state.isNode()) {
+            // if this is a node, quickly check whether it is shareable and
+            // whether it contains more than one parent
+            NodeState ns = (NodeState) state;
+            Set s = ns.getSharedSet();
+            if (s.size() > 1) {
+                return s;
+            }
+        }
+        NodeId parentId = getParentId(state);
+        if (parentId != null) {
+            LinkedHashSet s = new LinkedHashSet();
+            s.add(parentId);
+            return s;
+        }
+        return Collections.EMPTY_SET;
     }
 
     /**
@@ -453,7 +485,7 @@ public class HierarchyManagerImpl implements HierarchyManager {
             throws ItemNotFoundException, RepositoryException {
 
         NodeState parentState;
-        
+
         try {
             parentState = (NodeState) getItemState(parentId);
         } catch (NoSuchItemStateException nsis) {
@@ -475,7 +507,7 @@ public class HierarchyManagerImpl implements HierarchyManager {
         }
         return entry.getName();
     }
-    
+
     /**
      * {@inheritDoc}
      */
@@ -574,5 +606,93 @@ public class HierarchyManagerImpl implements HierarchyManager {
             throw new RepositoryException(msg, ise);
         }
     }
+
+    /**
+     * {@inheritDoc}
+     */
+    public boolean isShareAncestor(NodeId ancestor, NodeId descendant)
+            throws ItemNotFoundException, RepositoryException {
+        if (ancestor.equals(descendant)) {
+            // can't be ancestor of self
+            return false;
+        }
+        try {
+            ItemState state = getItemState(descendant);
+            Set parentIds = getParentIds(state);
+            while (parentIds.size() > 0) {
+                if (parentIds.contains(ancestor)) {
+                    return true;
+                }
+                Set grandparentIds = new LinkedHashSet();
+                Iterator iter = parentIds.iterator();
+                while (iter.hasNext()) {
+                    NodeId parentId = (NodeId) iter.next();
+                    grandparentIds.addAll(getParentIds(getItemState(parentId)));
+                }
+                parentIds = grandparentIds;
+            }
+            // not an ancestor
+            return false;
+        } catch (NoSuchItemStateException nsise) {
+            String msg = "failed to determine degree of relationship of "
+                    + ancestor + " and " + descendant;
+            log.debug(msg);
+            throw new ItemNotFoundException(msg, nsise);
+        } catch (ItemStateException ise) {
+            String msg = "failed to determine degree of relationship of "
+                    + ancestor + " and " + descendant;
+            log.debug(msg);
+            throw new RepositoryException(msg, ise);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public int getShareRelativeDepth(NodeId ancestor, ItemId descendant)
+            throws ItemNotFoundException, RepositoryException {
+
+        if (ancestor.equals(descendant)) {
+            return 0;
+        }
+        int depth = 1;
+        try {
+            ItemState state = getItemState(descendant);
+            if (state.hasOverlayedState()) {
+                state = state.getOverlayedState();
+            }
+            Set parentIds = getParentIds(state);
+            while (parentIds.size() > 0) {
+                if (parentIds.contains(ancestor)) {
+                    return depth;
+                }
+                depth++;
+                Set grandparentIds = new LinkedHashSet();
+                Iterator iter = parentIds.iterator();
+                while (iter.hasNext()) {
+                    NodeId parentId = (NodeId) iter.next();
+                    state = getItemState(parentId);
+                    if (state.hasOverlayedState()) {
+                        state = state.getOverlayedState();
+                    }
+                    grandparentIds.addAll(getParentIds(state));
+                }
+                parentIds = grandparentIds;
+            }
+            // not an ancestor
+            return -1;
+        } catch (NoSuchItemStateException nsise) {
+            String msg = "failed to determine degree of relationship of "
+                    + ancestor + " and " + descendant;
+            log.debug(msg);
+            throw new ItemNotFoundException(msg, nsise);
+        } catch (ItemStateException ise) {
+            String msg = "failed to determine degree of relationship of "
+                    + ancestor + " and " + descendant;
+            log.debug(msg);
+            throw new RepositoryException(msg, ise);
+        }
+    }
+
 }
 

jackrabbit-core/src/main/java/org/apache/jackrabbit/core/BatchedItemOperations.java
@@ -310,9 +310,8 @@ public class BatchedItemOperations extends ItemValidator {
         // 4. detect share cycle
         NodeId srcId = srcState.getNodeId();
         NodeId destParentId = destParentState.getNodeId();
-        if (destParentId.equals(srcId) ||
-                hierMgr.isAncestor(srcId, destParentId)) {
-            String msg = "This would create a share cycle.";
+        if (destParentId.equals(srcId) || hierMgr.isAncestor(srcId, destParentId)) {
+            String msg = "Share cycle detected.";
             log.debug(msg);
             throw new RepositoryException(msg);
         }
@@ -546,6 +545,14 @@ public class BatchedItemOperations extends ItemValidator {
             // subscript in name element
             String msg = safeGetJCRPath(destPath)
                     + ": invalid destination path (subscript in name element is not allowed)";
+            log.debug(msg);
+            throw new RepositoryException(msg);
+        }
+
+        HierarchyManagerImpl hierMgr = (HierarchyManagerImpl) this.hierMgr;
+        if (hierMgr.isShareAncestor(target.getNodeId(), destParent.getNodeId())) {
+            String msg = safeGetJCRPath(destPath)
+                    + ": invalid destination path (share cycle detected)";
             log.debug(msg);
             throw new RepositoryException(msg);
         }

jackrabbit-core/src/main/java/org/apache/jackrabbit/core/HierarchyManager.java
@@ -93,18 +93,18 @@ public interface HierarchyManager {
      * @throws RepositoryException
      */
     Name getName(ItemId id) throws ItemNotFoundException, RepositoryException;
-    
+
     /**
      * Returns the name of the specified item, with the given parent id. If the
      * given item is not shareable, this is identical to {@link #getName(ItemId)}.
-     * 
+     *
      * @param id node id
      * @param parentId parent node id
      * @return name
      * @throws ItemNotFoundException
      * @throws RepositoryException
      */
-    Name getName(NodeId id, NodeId parentId) 
+    Name getName(NodeId id, NodeId parentId)
             throws ItemNotFoundException, RepositoryException;
 
     /**
@@ -154,5 +154,52 @@ public interface HierarchyManager {
      * @throws RepositoryException   if another error occurs
      */
     boolean isAncestor(NodeId nodeId, ItemId itemId)
+            throws ItemNotFoundException, RepositoryException;
+
+    //------------------------------------------- operation with shareable nodes
+
+    /**
+     * Determines whether the node with the specified <code>ancestor</code>
+     * is a share ancestor of the item denoted by the given <code>descendant</code>.
+     * This is <code>true</code> for two nodes <code>A</code>, <code>B</code>
+     * if either:
+     * <ul>
+     * <li><code>A</code> is a (proper) ancestor of <code>B</code></li>
+     * <li>there is a non-empty sequence of nodes <code>N<sub>1</sub></code>,...
+     * ,<code>N<sub>k</sub></code> such that <code>A</code>=
+     * <code>N<sub>1</sub></code> and <code>B</code>=<code>N<sub>k</sub></code>
+     * and <code>N<sub>i</sub></code> is the parent or a share-parent of
+     * <code>N<sub>i+1</sub></code> (for every <code>i</code> in <code>1</code>
+     * ...<code>k-1</code>.</li>
+     * </ul>
+     *
+     * @param nodeId node id
+     * @param itemId item id
+     * @return <code>true</code> if the node denoted by <code>ancestor</code>
+     *         is a share ancestor of the item denoted by <code>descendant</code>,
+     *         <code>false</code> otherwise
+     * @throws ItemNotFoundException if any of the specified id's does not
+     *                               denote an existing item.
+     * @throws RepositoryException   if another error occurs
+     */
+    boolean isShareAncestor(NodeId ancestor, NodeId descendant)
+            throws ItemNotFoundException, RepositoryException;
+
+    /**
+     * Returns the depth of the specified share-descendant relative to the given
+     * share-ancestor. If <code>ancestor</code> and <code>descendant</code>
+     * denote the same item, <code>0</code> is returned. If <code>ancestor</code>
+     * does not denote an share-ancestor <code>-1</code> is returned.
+     *
+     * @param ancestor ancestor id
+     * @param descendant descendant id
+     * @return the relative depth; <code>-1</code> if <code>ancestor</code> does
+     *         not denote a share-ancestor of the item denoted by <code>descendant</code>
+     *         (or itself).
+     * @throws ItemNotFoundException if either of the specified id's does not
+     *                               denote an existing item.
+     * @throws RepositoryException   if another error occurs
+     */
+    int getShareRelativeDepth(NodeId ancestorId, ItemId descendantId)
             throws ItemNotFoundException, RepositoryException;
 }

jackrabbit-core/src/main/java/org/apache/jackrabbit/core/ItemManager.java
@@ -434,7 +434,7 @@ public class ItemManager implements ItemLifeCycleListener, Dumpable, ItemStateLi
         node = (NodeImpl) getItem(id);
         if (!node.getParentId().equals(parentId)) {
             // verify that parent actually appears in the shared set
-            if (!node.hasSharedParent(parentId)) {
+            if (!node.hasShareParent(parentId)) {
                 String msg = "Node with id '" + id
                         + "' does not have shared parent with id: " + parentId;
                 throw new ItemNotFoundException(msg);

jackrabbit-core/src/main/java/org/apache/jackrabbit/core/NodeImpl.java
@@ -2043,7 +2043,7 @@ public class NodeImpl extends ItemImpl implements Node {
 
         // (5) do clone operation
         NodeId parentId = getNodeId();
-        src.addShare(parentId);
+        src.addShareParent(parentId);
 
         // (6) modify the state of 'this', i.e. the parent node
         NodeId srcId = src.getNodeId();
@@ -3181,18 +3181,19 @@ public class NodeImpl extends ItemImpl implements Node {
      *         <code>false</code> otherwise.
      * @see NodeState#isShareable()
      */
-    protected boolean isShareable() {
+    boolean isShareable() {
        return ((NodeState) state).isShareable();
     }
 
     /**
      * Helper method, returning the parent id this node is attached to. If this
      * node is shareable, it returns the primary parent id (which remains
-     * fixed). Otherwise returns the underlying state's parent id.
+     * fixed since shareable nodes are not moveable). Otherwise returns the
+     * underlying state's parent id.
      *
      * @return parent id
      */
-    protected NodeId getParentId() {
+    NodeId getParentId() {
         if (primaryParentId != null) {
             return primaryParentId;
         }
@@ -3201,27 +3202,27 @@ public class NodeImpl extends ItemImpl implements Node {
 
     /**
      * Helper method, returning a flag indicating whether this node has
-     * the given shared parent.
+     * the given share-parent.
      *
      * @param parentId parent id
      * @return <code>true</code> if the node has the given shared parent;
      *         <code>false</code> otherwise.
      */
-    protected boolean hasSharedParent(NodeId parentId) {
+    boolean hasShareParent(NodeId parentId) {
         return ((NodeState) state).containsShare(parentId);
     }
 
     /**
-     * Add a parent to the shared set. This method checks first, whether:
+     * Add a share-parent to this node. This method checks, whether:
      * <ul>
      * <li>this node is shareable</li>
-     * <li>adding this parent would create a share cycle</li>
-     * <li>whether this parent is already contained in the shared set</li>
+     * <li>adding the given would create a share cycle</li>
+     * <li>the given parent is already a share-parent</li>
      * </ul>
      * @param parentId parent to add to the shared set
      * @throws RepositoryException if an error occurs
      */
-    protected void addShare(NodeId parentId) throws RepositoryException {
+    void addShareParent(NodeId parentId) throws RepositoryException {
         // verify that we're shareable
         if (!isShareable()) {
             String msg = "Node at " + safeGetJCRPath() + " is not shareable.";
@@ -3293,8 +3294,10 @@ public class NodeImpl extends ItemImpl implements Node {
     /**
      * Invoked when another node in the same shared set has replaced the
      * node state.
+     *
+     * @param state state that is now stored as <code>NodeImpl</code>'s state
      */
-    protected void stateReplaced(NodeState state) {
+    void stateReplaced(NodeState state) {
         this.state = state;
     }
 

jackrabbit-core/src/main/java/org/apache/jackrabbit/core/SessionImpl.java
@@ -1556,6 +1556,13 @@ public class SessionImpl extends AbstractSession
             log.debug(msg);
             throw new RepositoryException(msg, e);
         }
+
+        if (hierMgr.isShareAncestor(targetNode.getNodeId(), destParentNode.getNodeId())) {
+            String msg = destAbsPath + ": invalid destination path (share cycle detected)";
+            log.debug(msg);
+            throw new RepositoryException(msg);
+        }
+
         int ind = destName.getIndex();
         if (ind > 0) {
             // subscript in name element
@@ -1645,7 +1652,7 @@ public class SessionImpl extends AbstractSession
                 log.debug(msg);
                 throw new UnsupportedRepositoryOperationException(msg);
             }
-            
+
             // do move:
             // 1. remove child node entry from old parent
             NodeState srcParentState =

jackrabbit-core/src/main/java/org/apache/jackrabbit/core/state/SessionItemStateManager.java
@@ -19,6 +19,7 @@ package org.apache.jackrabbit.core.state;
 import org.apache.commons.collections.iterators.IteratorChain;
 import org.apache.jackrabbit.core.CachingHierarchyManager;
 import org.apache.jackrabbit.core.HierarchyManager;
+import org.apache.jackrabbit.core.HierarchyManagerImpl;
 import org.apache.jackrabbit.core.ItemId;
 import org.apache.jackrabbit.core.NodeId;
 import org.apache.jackrabbit.core.PropertyId;
@@ -377,7 +378,7 @@ public class SessionItemStateManager
      * Returns an iterator over those transient item state instances that are
      * direct or indirect descendants of the item state with the given
      * <code>parentId</code>. The transient item state instance with the given
-     * <code>parentId</code> itself (if there is such) will not be included.
+     * <code>parentId</code> itself (if there is such)                                                                            not be included.
      * <p/>
      * The instances are returned in depth-first tree traversal order.
      *
@@ -407,7 +408,7 @@ public class SessionItemStateManager
                 // determine relative depth: > 0 means it's a descendant
                 int depth;
                 try {
-                    depth = hierMgr.getRelativeDepth(parentId, state.getId());
+                    depth = hierMgr.getShareRelativeDepth(parentId, state.getId());
                 } catch (ItemNotFoundException infe) {
                     /**
                      * one of the parents of the specified item has been
@@ -520,7 +521,8 @@ public class SessionItemStateManager
             while (iter.hasNext()) {
                 ItemState state = (ItemState) iter.next();
                 // determine relative depth: > 0 means it's a descendant
-                int depth = zombieHierMgr.getRelativeDepth(parentId, state.getId());
+                //int depth = zombieHierMgr.getRelativeDepth(parentId, state.getId());
+                int depth = zombieHierMgr.getShareRelativeDepth(parentId, state.getId());
                 if (depth < 1) {
                     // not a descendant
                     continue;

commit 3d902775a3799d01d40f81daf350e6986ae36692

JCR-1104 - JSR 283 support - shareble nodes (work in progress) - fix test failure introduced in r646336
git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@647072 13f79535-47bb-0310-9956-ffa450edef68

jackrabbit-core/src/main/java/org/apache/jackrabbit/core/HierarchyManagerImpl.java
@@ -225,14 +225,18 @@ public class HierarchyManagerImpl implements HierarchyManager {
      * one parent.
      *
      * @param state item state
+     * @param useOverlayed whether to use overlayed state for shareable nodes
      * @return set of parent <code>NodeId</code>s. If state has no parent,
      *         array has length <code>0</code>.
      */
-    protected Set getParentIds(ItemState state) {
+    protected Set getParentIds(ItemState state, boolean useOverlayed) {
         if (state.isNode()) {
             // if this is a node, quickly check whether it is shareable and
             // whether it contains more than one parent
             NodeState ns = (NodeState) state;
+            if (ns.isShareable() && useOverlayed && ns.hasOverlayedState()) {
+                ns = (NodeState) ns.getOverlayedState();
+            }
             Set s = ns.getSharedSet();
             if (s.size() > 1) {
                 return s;
@@ -618,7 +622,7 @@ public class HierarchyManagerImpl implements HierarchyManager {
         }
         try {
             ItemState state = getItemState(descendant);
-            Set parentIds = getParentIds(state);
+            Set parentIds = getParentIds(state, false);
             while (parentIds.size() > 0) {
                 if (parentIds.contains(ancestor)) {
                     return true;
@@ -627,7 +631,7 @@ public class HierarchyManagerImpl implements HierarchyManager {
                 Iterator iter = parentIds.iterator();
                 while (iter.hasNext()) {
                     NodeId parentId = (NodeId) iter.next();
-                    grandparentIds.addAll(getParentIds(getItemState(parentId)));
+                    grandparentIds.addAll(getParentIds(getItemState(parentId), false));
                 }
                 parentIds = grandparentIds;
             }
@@ -658,10 +662,7 @@ public class HierarchyManagerImpl implements HierarchyManager {
         int depth = 1;
         try {
             ItemState state = getItemState(descendant);
-            if (state.hasOverlayedState()) {
-                state = state.getOverlayedState();
-            }
-            Set parentIds = getParentIds(state);
+            Set parentIds = getParentIds(state, true);
             while (parentIds.size() > 0) {
                 if (parentIds.contains(ancestor)) {
                     return depth;
@@ -672,10 +673,7 @@ public class HierarchyManagerImpl implements HierarchyManager {
                 while (iter.hasNext()) {
                     NodeId parentId = (NodeId) iter.next();
                     state = getItemState(parentId);
-                    if (state.hasOverlayedState()) {
-                        state = state.getOverlayedState();
-                    }
-                    grandparentIds.addAll(getParentIds(state));
+                    grandparentIds.addAll(getParentIds(state, true));
                 }
                 parentIds = grandparentIds;
             }