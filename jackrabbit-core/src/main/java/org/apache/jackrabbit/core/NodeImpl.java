748520151133e6af61750d5c5beb38d5d7aab790
Jukka Zitting <jukka@apache.org>
2008-09-16 16:07:44 +0000

    1.4: Merged revision 668147 (JCR-1651)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/branches/1.4@695953 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-core/src/main/java/org/apache/jackrabbit/core/NodeImpl.java
+++ jackrabbit-core/src/main/java/org/apache/jackrabbit/core/NodeImpl.java
@@ -1984,6 +1984,9 @@ public class NodeImpl extends ItemImpl implements Node {
         sanityCheck();
 
         NodeTypeImpl nt = (NodeTypeImpl) session.getNodeTypeManager().getNodeType(nodeTypeName);
+        if (nt.isMixin()) {
+            throw new RepositoryException(nodeTypeName + ": not a primary node type");
+        }
         return internalAddNode(relPath, nt);
     }
 
