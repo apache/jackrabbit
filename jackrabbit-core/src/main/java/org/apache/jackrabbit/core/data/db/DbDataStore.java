6eac63d9e7c7923fa366d2466c6e0dc11d947ac4
Thomas Mueller <thomasm@apache.org>
2008-10-02 15:27:26 +0000

    JCR-1572 DbDataStore connection does not always reconnect
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@701154 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-core/src/main/java/org/apache/jackrabbit/core/data/db/DbDataStore.java
+++ jackrabbit-core/src/main/java/org/apache/jackrabbit/core/data/db/DbDataStore.java
@@ -285,7 +285,6 @@ public class DbDataStore implements DataStore {
         TempFileInputStream fileInput = null;
         ConnectionRecoveryManager conn = getConnection();
         try {
-            conn.setAutoReconnect(false);
             String id = null, tempId = null;
             long now;
             for (int i = 0; i < ConnectionRecoveryManager.TRIALS; i++) {
@@ -326,6 +325,7 @@ public class DbDataStore implements DataStore {
             } else {
                 throw new DataStoreException("Unsupported stream store algorithm: " + storeStream);
             }
+            // UPDATE DATASTORE SET DATA=? WHERE ID=?
             conn.executeStmt(updateDataSQL, new Object[]{wrapper, tempId});
             now = System.currentTimeMillis();
             long length = in.getPosition();
@@ -362,7 +362,6 @@ public class DbDataStore implements DataStore {
             }
             usesIdentifier(identifier);
             DbDataRecord record = new DbDataRecord(this, identifier, length, now);
-            conn.setAutoReconnect(true);
             return record;
         } catch (Exception e) {
             throw convert("Can not insert new record", e);
