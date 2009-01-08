c30afd25a0820586da6704181958e3e5fd79bb3f
Angela Schreiber <angela@apache.org>
2009-01-08 16:04:21 +0000

    unused imports
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@732736 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-webapp/src/main/java/org/apache/jackrabbit/j2ee/JCRWebdavServerServlet.java
+++ jackrabbit-webapp/src/main/java/org/apache/jackrabbit/j2ee/JCRWebdavServerServlet.java
@@ -16,29 +16,7 @@
  */
 package org.apache.jackrabbit.j2ee;
 
-import org.apache.jackrabbit.server.BasicCredentialsProvider;
-import org.apache.jackrabbit.server.SessionProviderImpl;
-import org.apache.jackrabbit.server.jcr.JCRWebdavServer;
-import org.apache.jackrabbit.webdav.DavException;
-import org.apache.jackrabbit.webdav.DavLocatorFactory;
-import org.apache.jackrabbit.webdav.DavResource;
-import org.apache.jackrabbit.webdav.DavResourceFactory;
-import org.apache.jackrabbit.webdav.DavSessionProvider;
-import org.apache.jackrabbit.webdav.WebdavRequest;
-import org.apache.jackrabbit.webdav.jcr.DavLocatorFactoryImpl;
-import org.apache.jackrabbit.webdav.jcr.DavResourceFactoryImpl;
-import org.apache.jackrabbit.webdav.jcr.JcrDavSession;
-import org.apache.jackrabbit.webdav.jcr.observation.SubscriptionManagerImpl;
-import org.apache.jackrabbit.webdav.jcr.transaction.TxLockManagerImpl;
-import org.apache.jackrabbit.webdav.observation.SubscriptionManager;
-import org.apache.jackrabbit.webdav.server.AbstractWebdavServlet;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import javax.jcr.Repository;
-import javax.jcr.Session;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
 
 /**
  * JCRWebdavServerServlet provides request/response handling for the
