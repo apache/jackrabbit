929101971a28f8a34b17ca41bfb9f505603f42f8
Angela Schreiber <angela@apache.org>
2009-01-22 14:16:00 +0000

    javadoc
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@736653 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-core/src/main/java/org/apache/jackrabbit/core/RepositoryImpl.java
+++ jackrabbit-core/src/main/java/org/apache/jackrabbit/core/RepositoryImpl.java
@@ -881,7 +881,7 @@ public class RepositoryImpl extends AbstractRepository
      * workspace, and thus this feature is only available to customized
      * implementations that subclass RepositoryImpl.
      *
-     * @see https://issues.apache.org/jira/browse/JCR-954
+     * @see <a href="https://issues.apache.org/jira/browse/JCR-954">Issue JCR-954</a>
      * @param workspace name of the workspace
      * @param enabled <code>true</code> to enable integrity checking (default),
      *                <code>false</code> to disable it
