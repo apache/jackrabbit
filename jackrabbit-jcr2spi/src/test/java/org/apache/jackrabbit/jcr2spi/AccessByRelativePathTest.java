049d9910cec33b93cd580f3a9ce44a7c3eb69be1
Angela Schreiber <angela@apache.org>
2009-03-11 10:10:32 +0000

    remove spi dependency, minor improvements
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@752415 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-jcr2spi/src/test/java/org/apache/jackrabbit/jcr2spi/AccessByRelativePathTest.java
+++ jackrabbit-jcr2spi/src/test/java/org/apache/jackrabbit/jcr2spi/AccessByRelativePathTest.java
@@ -16,12 +16,8 @@
  */
 package org.apache.jackrabbit.jcr2spi;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.apache.jackrabbit.test.AbstractJCRTest;
 import org.apache.jackrabbit.test.NotExecutableException;
-import org.apache.jackrabbit.spi.PathFactory;
-import org.apache.jackrabbit.spi.commons.name.PathFactoryImpl;
 
 import javax.jcr.RepositoryException;
 import javax.jcr.Node;
@@ -34,11 +30,8 @@ import javax.jcr.NodeIterator;
  */
 public class AccessByRelativePathTest extends AbstractJCRTest {
 
-    private static Logger log = LoggerFactory.getLogger(AccessByRelativePathTest.class);
-
-    private static final PathFactory P_FACTORY = PathFactoryImpl.getInstance();
-    private static String DOT = P_FACTORY.getCurrentElement().getName().getLocalName();
-    private static String DOTDOT = P_FACTORY.getParentElement().getName().getLocalName();
+    private static String DOT = ".";
+    private static String DOTDOT = "..";
 
     /**
      * <code>Node.hasNode(".") </code> applied to the root node must return
@@ -201,8 +194,10 @@ public class AccessByRelativePathTest extends AbstractJCRTest {
      * @throws RepositoryException
      */
     public void testGetNodeDotDotSlashName() throws RepositoryException, NotExecutableException {
-        // TODO: NotExecutableException if sns exist for testRootNode
         String otherRelPath = DOTDOT + "/" + testRootNode.getName();
+        if (testRootNode.getIndex() > 1) {
+            otherRelPath = otherRelPath + "[" + testRootNode.getIndex() + "]";
+        }
         assertTrue(testRootNode.isSame(testRootNode.getNode(otherRelPath)));
     }
 
