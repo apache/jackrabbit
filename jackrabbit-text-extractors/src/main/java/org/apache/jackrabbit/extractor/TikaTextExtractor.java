0b1e70337bdcf4766e7fc3289b752481590c6ba9
Jukka Zitting <jukka@apache.org>
2009-04-07 15:03:05 +0000

    JCR-1878: Use Apache Tika for text extraction
    Use a static Tika Parser instance to avoid separate instances being created for each search index and TikaTextExtractor subclass.
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@762804 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-text-extractors/src/main/java/org/apache/jackrabbit/extractor/TikaTextExtractor.java
+++ jackrabbit-text-extractors/src/main/java/org/apache/jackrabbit/extractor/TikaTextExtractor.java
@@ -24,15 +24,30 @@ import java.util.Set;
 import org.apache.jackrabbit.extractor.TextExtractor;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.parser.Parser;
 import org.apache.tika.parser.ParsingReader;
 
 public class TikaTextExtractor implements TextExtractor {
 
-    private final AutoDetectParser parser = new AutoDetectParser();
+    /**
+     * Auto-detecting parser.
+     */
+    private static final Parser PARSER;
+
+    /**
+     * Supported content types.
+     */
+    private static final String[] TYPES;
+
+    static {
+        AutoDetectParser parser = new AutoDetectParser();
+        PARSER = parser;
+        Set types = parser.getParsers().keySet();
+        TYPES = (String[]) types.toArray(new String[types.size()]);
+    }
 
     public String[] getContentTypes() {
-        Set types = parser.getParsers().keySet();
-        return (String[]) types.toArray(new String[types.size()]);
+        return TYPES;
     }
 
     public Reader extractText(InputStream stream, String type, String encoding)
@@ -41,7 +56,8 @@ public class TikaTextExtractor implements TextExtractor {
         if (type != null && type.trim().length() > 0) {
             metadata.set(Metadata.CONTENT_TYPE, type.trim());
         }
-        return new ParsingReader(parser, stream, metadata);
+        // TODO: This creates a background thread. Is that a problem?
+        return new ParsingReader(PARSER, stream, metadata);
     }
 
 }
