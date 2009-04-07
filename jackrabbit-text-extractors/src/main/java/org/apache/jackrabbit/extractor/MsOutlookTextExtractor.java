b8408aded532a8a716bd2c8eb697bc3f8ab31dbe
Jukka Zitting <jukka@apache.org>
2009-04-07 15:54:53 +0000

    JCR-1878: Use Apache Tika for text extraction
    Use POI through Tika to avoid the Java 5 compilation errors.
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@762823 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-text-extractors/src/main/java/org/apache/jackrabbit/extractor/MsOutlookTextExtractor.java
+++ jackrabbit-text-extractors/src/main/java/org/apache/jackrabbit/extractor/MsOutlookTextExtractor.java
@@ -16,64 +16,17 @@
  */
 package org.apache.jackrabbit.extractor;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.Reader;
-import java.io.StringReader;
-
-import org.apache.poi.hsmf.MAPIMessage;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
  * Text extractor for Microsoft Outlook messages.
  */
-public class MsOutlookTextExtractor extends AbstractTextExtractor {
+public class MsOutlookTextExtractor extends DefaultTextExtractor {
 
-    /**
-     * Logger instance.
-     */
-    private static final Logger logger =
-        LoggerFactory.getLogger(MsOutlookTextExtractor.class);
+    private static String[] TYPES = new String[] {
+        "application/vnd.ms-outlook"
+    };
 
-    /**
-     * Force loading of dependent class.
-     */
-    static {
-        MAPIMessage.class.getName();
-    }
-
-    /**
-     * Creates a new <code>MsOutlookTextExtractor</code> instance.
-     */
-    public MsOutlookTextExtractor() {
-        super(new String[]{"application/vnd.ms-outlook"});
-    }
-
-    //-------------------------------------------------------< TextExtractor >
-
-    /**
-     * {@inheritDoc}
-     * Returns an empty reader if an error occured extracting text from
-     * the outlook message.
-     */
-    public Reader extractText(InputStream stream,
-                              String type,
-                              String encoding) throws IOException {
-        try {
-        	MAPIMessage message = new MAPIMessage(stream);
-        	StringBuffer buffer = new StringBuffer();
-        	buffer.append(message.getDisplayFrom()).append('\n');
-        	buffer.append(message.getDisplayTo()).append('\n');
-        	buffer.append(message.getSubject()).append('\n');
-        	buffer.append(message.getTextBody());
-            return new StringReader(buffer.toString());
-        } catch (Exception e) {
-            logger.warn("Failed to extract Message content", e);
-            return new StringReader("");
-        } finally {
-            stream.close();
-        }
+    public String[] getContentTypes() {
+        return TYPES;
     }
 
 }
