c6141fec50ae1fc830f9c2bc53c9dbf228090b6c
Tobias Bocanegra <tripod@apache.org>
2009-04-08 13:58:37 +0000

    JCR-2065 use the internal CND file for builtin nodetypes
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@763248 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-core/src/main/java/org/apache/jackrabbit/core/nodetype/NodeTypeDefStore.java
+++ jackrabbit-core/src/main/java/org/apache/jackrabbit/core/nodetype/NodeTypeDefStore.java
@@ -18,16 +18,21 @@ package org.apache.jackrabbit.core.nodetype;
 
 import org.apache.jackrabbit.core.nodetype.xml.NodeTypeReader;
 import org.apache.jackrabbit.core.nodetype.xml.NodeTypeWriter;
+import org.apache.jackrabbit.core.nodetype.compact.CompactNodeTypeDefReader;
+import org.apache.jackrabbit.core.nodetype.compact.ParseException;
 import org.apache.jackrabbit.spi.Name;
+import org.apache.jackrabbit.spi.commons.name.NameConstants;
 
 import javax.jcr.NamespaceRegistry;
 import javax.jcr.RepositoryException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.Reader;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
 
 /**
  * <code>NodeTypeDefStore</code> ...
@@ -55,6 +60,28 @@ public class NodeTypeDefStore {
         NodeTypeDef[] types = NodeTypeReader.read(in);
         for (int i = 0; i < types.length; i++) {
             add(types[i]);
+        }
+    }
+
+    /**
+     * Loads node types from a CND stream.
+     * 
+     * @param in reader containing the nodetype definitions
+     * @param systemId optional name of the stream
+     *
+     * @throws IOException if an I/O error during reading occurrs
+     * @throws InvalidNodeTypeDefException if the CND cannot be parsed
+     */
+    public void loadCND(Reader in, String systemId)
+            throws IOException, InvalidNodeTypeDefException {
+        try {
+            CompactNodeTypeDefReader r = new CompactNodeTypeDefReader(in, systemId);
+            Iterator iter = r.getNodeTypeDefs().iterator();
+            while (iter.hasNext()) {
+                add((NodeTypeDef) iter.next());
+            }
+        } catch (ParseException e) {
+            throw new InvalidNodeTypeDefException("Unable to parse CND stream.", e);
         }
     }
 
