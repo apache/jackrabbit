b6d3853aebeaf76e7b284a77969ab8048f8e57fa
Jukka Zitting <jukka@apache.org>
2009-04-30 12:42:55 +0000

    JCR-2062: JSR 283: Repository Compliance
    Implement AbstractRepository.isStandardDescriptor(String)
    
    Add missing methods to EmptyRepository.
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@770206 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-jcr-commons/src/main/java/org/apache/jackrabbit/commons/repository/EmptyRepository.java
+++ jackrabbit-jcr-commons/src/main/java/org/apache/jackrabbit/commons/repository/EmptyRepository.java
@@ -19,6 +19,7 @@ package org.apache.jackrabbit.commons.repository;
 import javax.jcr.Credentials;
 import javax.jcr.NoSuchWorkspaceException;
 import javax.jcr.Session;
+import javax.jcr.Value;
 
 import org.apache.jackrabbit.commons.AbstractRepository;
 
@@ -34,10 +35,41 @@ public class EmptyRepository extends AbstractRepository {
     /**
      * Returns <code>null</code> since this repository contains no descriptors.
      *
+     * @param key descriptor key
      * @return <code>null</code>
      */
     public String getDescriptor(String key) {
         return null;
+    }
+
+    /**
+     * Returns <code>null</code> since this repository contains no descriptors.
+     *
+     * @param key descriptor key
+     * @return <code>null</code>
+     */
+    public Value getDescriptorValue(String key) {
+        return null;
+    }
+
+    /**
+     * Returns <code>null</code> since this repository contains no descriptors.
+     *
+     * @param key descriptor key
+     * @return <code>null</code>
+     */
+    public Value[] getDescriptorValues(String key) {
+        return null;
+    }
+
+    /**
+     * Returns <code>false</code> since this repository contains no descriptors.
+     *
+     * @param key descriptor key
+     * @return <code>false</code>
+     */
+    public boolean isSingleValueDescriptor(String key) {
+        return false;
     }
 
     /**
