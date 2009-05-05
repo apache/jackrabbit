f2606bdf567edb615ae54d7d69a2322ff9c27aec
Julian Reschke <reschke@apache.org>
2009-05-05 11:51:54 +0000

    JCR-2085: move GetDateTest to jackrabbit-jcr-tests
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@771676 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-jcr-tests/src/main/java/org/apache/jackrabbit/test/api/observation/GetDateTest.java
+++ jackrabbit-jcr-tests/src/main/java/org/apache/jackrabbit/test/api/observation/GetDateTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.jackrabbit.api.jsr283.observation;
+package org.apache.jackrabbit.test.api.observation;
 
 import java.util.List;
 import java.util.Arrays;
