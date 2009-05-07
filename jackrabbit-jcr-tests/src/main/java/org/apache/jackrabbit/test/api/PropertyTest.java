f3068f19de8a48273c368a2cff60c66afb180147
Julian Reschke <reschke@apache.org>
2009-05-07 11:58:43 +0000

    JCR-2059: modify test so that Session.getProperty() gets used
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@772624 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-jcr-tests/src/main/java/org/apache/jackrabbit/test/api/PropertyTest.java
+++ jackrabbit-jcr-tests/src/main/java/org/apache/jackrabbit/test/api/PropertyTest.java
@@ -46,6 +46,8 @@ public class PropertyTest extends AbstractJCRTest {
      * two <code>Property</code> objects representing the same actual repository
      * item have been retrieved through two different sessions and one has been
      * modified.
+     * 
+     * @since JCR 2.0
      */
     public void testIsSameMustNotCompareStates()
             throws RepositoryException {
@@ -58,7 +60,7 @@ public class PropertyTest extends AbstractJCRTest {
         // accuire the same property through a different session
         Session session = helper.getSuperuserSession();
         try {
-            Property prop2 = (Property) session.getItem(prop1.getPath());
+            Property prop2 = session.getProperty(prop1.getPath());
 
             // change the value of prop2
             prop2.setValue("value2");
