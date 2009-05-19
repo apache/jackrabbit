f9f208828e6fc71482458577277f36157f876f17
Jukka Zitting <jukka@apache.org>
2009-05-19 15:47:01 +0000

    JCR-422: Implement a backup tool
    Handle node references
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@776362 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-core/src/main/java/org/apache/jackrabbit/core/persistence/PersistenceCopier.java
+++ jackrabbit-core/src/main/java/org/apache/jackrabbit/core/persistence/PersistenceCopier.java
@@ -24,6 +24,8 @@ import org.apache.jackrabbit.core.PropertyId;
 import org.apache.jackrabbit.core.state.ChangeLog;
 import org.apache.jackrabbit.core.state.ChildNodeEntry;
 import org.apache.jackrabbit.core.state.ItemStateException;
+import org.apache.jackrabbit.core.state.NodeReferences;
+import org.apache.jackrabbit.core.state.NodeReferencesId;
 import org.apache.jackrabbit.core.state.NodeState;
 import org.apache.jackrabbit.core.state.PropertyState;
 import org.apache.jackrabbit.spi.Name;
@@ -139,7 +141,15 @@ public class PersistenceCopier {
             }
         }
 
-        // TODO: Copy node references?
+        // Copy all node references
+        NodeReferencesId refsId = new NodeReferencesId(sourceNode.getNodeId());
+        if (source.exists(refsId)) {
+            changes.modified(source.load(refsId));
+        } else if (target.exists(refsId)) {
+            NodeReferences references = target.load(refsId);
+            references.clearAllReferences();
+            changes.modified(references);
+        }
 
         // Persist the copied states
         target.store(changes);
