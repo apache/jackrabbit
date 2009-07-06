ad91eb2e1a98ab3cf08468c90857dbb537a5960a
Julian Reschke <reschke@apache.org>
2009-07-06 15:01:17 +0000

    JCR-2085: add additional Activities tests
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@791505 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-jcr-tests/src/main/java/org/apache/jackrabbit/test/api/version/ActivitiesTest.java
+++ jackrabbit-jcr-tests/src/main/java/org/apache/jackrabbit/test/api/version/ActivitiesTest.java
@@ -17,6 +17,8 @@
 package org.apache.jackrabbit.test.api.version;
 
 import javax.jcr.Node;
+import javax.jcr.Property;
+import javax.jcr.PropertyType;
 import javax.jcr.Repository;
 import javax.jcr.nodetype.NodeType;
 import javax.jcr.version.VersionManager;
@@ -33,9 +35,14 @@ import javax.jcr.version.VersionManager;
  */
 public class ActivitiesTest extends AbstractVersionTest {
 
+    private VersionManager vm;
+
+    private static String PREFIX = "/jcr:system/jcr:activities/";
+    
     protected void setUp() throws Exception {
         super.setUp();
         checkSupportedOption(Repository.OPTION_ACTIVITIES_SUPPORTED);
+        vm = superuser.getWorkspace().getVersionManager();
     }
 
     protected void tearDown() throws Exception {
@@ -44,7 +51,49 @@ public class ActivitiesTest extends AbstractVersionTest {
 
     public void testCreateRemoveActivity() throws Exception {
         
-        VersionManager vm = superuser.getWorkspace().getVersionManager();
+        Node an = null;
+        
+        try {
+            an = vm.createActivity("foobar");
+            assertNotNull(an);
+            
+            NodeType annt = an.getPrimaryNodeType();
+            assertTrue("create node must be subtype of nt:activity", annt.isNodeType("nt:activity"));
+        }
+        finally {
+            if (an != null) {
+                vm.removeActivity(an);
+            }
+        }
+    }
+
+    public void testSetGetActivity() throws Exception {
+        
+        Node an = null;
+        
+        try {
+            an = vm.createActivity("foobar");
+            assertNotNull(an);
+            
+            assertNull(vm.getActivity());
+            
+            Node old = vm.setActivity(an);
+            assertNull(old);
+            assertEquals(an.getPath(), vm.getActivity().getPath());
+            
+            old = vm.setActivity(null);
+            assertEquals(old.getPath(), an.getPath());
+            assertNull(vm.getActivity());
+        }
+        finally {
+            if (an != null) {
+                vm.removeActivity(an);
+            }
+        }
+    }
+
+    public void testActivitiesPath() throws Exception {
+        
         Node an = null;
         
         try {
@@ -54,8 +103,11 @@ public class ActivitiesTest extends AbstractVersionTest {
             NodeType annt = an.getPrimaryNodeType();
             assertTrue("create node must be subtype of nt:activity", annt.isNodeType("nt:activity"));
 
-            String pathprefix = "/jcr:system/jcr:activities/";
-            assertTrue("path for activity must be below " + pathprefix + ", but was " + an.getPath(), an.getPath().startsWith(pathprefix));
+            assertTrue("path for activity must be below " + PREFIX + ", but was " + an.getPath(), an.getPath().startsWith(PREFIX));
+
+            Node activities = superuser.getNode(PREFIX);
+            
+            assertFalse(activities.getPrimaryNodeType().canAddChildNode("foobar"));
         }
         finally {
             if (an != null) {
@@ -63,4 +115,38 @@ public class ActivitiesTest extends AbstractVersionTest {
             }
         }
     }
+    
+    public void testActivitiesRelation() throws Exception {
+        
+        Node an = null;
+        
+        try {
+            an = vm.createActivity("foobar");
+            vm.setActivity(an);
+            
+            String path = versionableNode.getPath();
+            
+            if (versionableNode.isCheckedOut()) {
+                vm.checkin(path);
+            }
+            
+            vm.checkout(path);
+            
+            versionableNode = superuser.getNode(path);
+            Property act = versionableNode.getProperty(Property.JCR_ACTIVITY);
+            assertNotNull(act);
+            assertEquals(PropertyType.REFERENCE, act.getType());
+            assertTrue(act.getNode().isSame(an));
+            
+            versionableNode.remove();
+            versionableNode.getSession().save();
+        }
+        finally {
+            if (an != null) {
+                vm.removeActivity(an);
+            }
+        }
+    }
+    
 }
+
