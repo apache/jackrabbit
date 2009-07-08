0b0a31f5ba9446eb815b965f25da52559200b435
Marcel Reutegger <mreutegg@apache.org>
2009-07-08 15:13:46 +0000

    JCR-2196: Add cause to ItemStateException in BundleDbPersistenceManager.store()
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@792181 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-core/src/main/java/org/apache/jackrabbit/core/persistence/bundle/BundleDbPersistenceManager.java
+++ jackrabbit-core/src/main/java/org/apache/jackrabbit/core/persistence/bundle/BundleDbPersistenceManager.java
@@ -564,7 +564,7 @@ public class BundleDbPersistenceManager extends AbstractBundlePersistenceManager
                 connectionManager.setAutoReconnect(true);
             }
         } while(blockOnConnectionLoss || trials > 0);
-        throw new ItemStateException(lastException.getMessage());
+        throw new ItemStateException(lastException.getMessage(), lastException);
     }
 
     /**
