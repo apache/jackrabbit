apache/jackrabbit

commit 07be8a82cea0dfe2d586ed025e1ba8a32bad4aa9

JCR-2208: make SQL query tests abort with NotExecutableException if Query.SQL is not supported
git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@794294 13f79535-47bb-0310-9956-ffa450edef68

jackrabbit-jcr-tests/src/main/java/org/apache/jackrabbit/test/api/query/AbstractQueryTest.java
@@ -20,6 +20,7 @@ import org.apache.jackrabbit.test.AbstractJCRTest;
 import org.apache.jackrabbit.test.NotExecutableException;
 import org.apache.jackrabbit.test.api.util.ISO9075;
 
+import javax.jcr.query.InvalidQueryException;
 import javax.jcr.query.QueryResult;
 import javax.jcr.query.RowIterator;
 import javax.jcr.query.Query;
@@ -126,13 +127,14 @@ public abstract class AbstractQueryTest extends AbstractJCRTest {
      * @throws RepositoryException
      * @see #createQuery(String, String)
      */
-    protected Query createQuery(Statement statement) throws RepositoryException {
+    protected Query createQuery(Statement statement)
+        throws RepositoryException, NotExecutableException {
         return createQuery(statement.getStatement(), statement.getLanguage());
     }
 
     /**
      * Creates a {@link Query} for the given statement in the requested
-     * language
+     * language, treating optional languages gracefully
      *
      * @param statement the query should be created for
      * @param language  query language to be used for Query creation
@@ -140,10 +142,24 @@ public abstract class AbstractQueryTest extends AbstractJCRTest {
      *
      * @throws RepositoryException
      */
-    protected Query createQuery(String statement, String language) throws RepositoryException {
+    protected Query createQuery(String statement, String language)
+        throws RepositoryException, NotExecutableException {
         log.println("Creating query: " + statement);
-        return superuser.getWorkspace().getQueryManager().createQuery(statement,
-                language);
+        try {
+            return qm.createQuery(statement, language);
+        }
+        catch (InvalidQueryException ex) {
+            
+            // if language is optional and not reported as "supported" -> 
+            // demote to NotExecutableException
+            
+            if (! isSupportedLanguage(language) && ! Query.JCR_SQL2.equals(language)) {
+                throw new NotExecutableException("Repository does not support " + language + " query syntax");
+            }
+            else {
+                throw ex;
+            }
+        }
     }
 
     /**
@@ -155,7 +171,8 @@ public abstract class AbstractQueryTest extends AbstractJCRTest {
      * @throws RepositoryException
      * @see #execute(String, String)
      */
-    protected QueryResult execute(Statement statement) throws RepositoryException {
+    protected QueryResult execute(Statement statement)
+        throws RepositoryException, NotExecutableException {
         return execute(statement.getStatement(), statement.getLanguage());
     }
 
@@ -170,7 +187,7 @@ public abstract class AbstractQueryTest extends AbstractJCRTest {
      * @throws RepositoryException
      */
     protected QueryResult execute(String statement, String language)
-            throws RepositoryException {
+            throws RepositoryException, NotExecutableException {
         Query query = createQuery(statement, language);
         return query.execute();
     }
@@ -298,10 +315,11 @@ public abstract class AbstractQueryTest extends AbstractJCRTest {
      * @param session the session to use for the query.
      * @param sql the sql query.
      * @param nodes the expected result nodes.
+     * @throws NotExecutableException 
      */
     protected void executeSqlQuery(Session session, String sql, Node[] nodes)
-            throws RepositoryException {
-        QueryResult res = session.getWorkspace().getQueryManager().createQuery(sql, Query.SQL).execute();
+            throws RepositoryException, NotExecutableException {
+        QueryResult res = createQuery(sql, Query.SQL).execute();
         checkResult(res, nodes);
     }
 

jackrabbit-jcr-tests/src/main/java/org/apache/jackrabbit/test/api/query/QueryResultNodeIteratorTest.java
@@ -71,7 +71,7 @@ public class QueryResultNodeIteratorTest extends AbstractQueryTest {
     /**
      * Tests the method <code>NodeIterator.getPosition()</code>.
      */
-    public void testGetPosition() throws RepositoryException {
+    public void testGetPosition() throws RepositoryException, NotExecutableException {
         QueryResult rs = execute(xpathRoot + "//*", Query.XPATH);
 
         // getPosition initially returns 0
@@ -89,8 +89,9 @@ public class QueryResultNodeIteratorTest extends AbstractQueryTest {
     /**
      * Tests the method <code>NodeIterator.getPosition()</code> on an empty
      * <code>NodeIterator</code>.
+     * @throws NotExecutableException 
      */
-    public void testGetPositionEmptyIterator() throws RepositoryException {
+    public void testGetPositionEmptyIterator() throws RepositoryException, NotExecutableException {
         QueryResult rs = execute(xpathRoot + "/" + nodeName4, Query.XPATH);
 
         NodeIterator it = rs.getNodes();
@@ -103,8 +104,9 @@ public class QueryResultNodeIteratorTest extends AbstractQueryTest {
      * Tests if a {@link java.util.NoSuchElementException} is thrown when {@link
      * javax.jcr.NodeIterator#nextNode()} is called and there are no more nodes
      * available.
+     * @throws NotExecutableException 
      */
-    public void testNoSuchElementException() throws RepositoryException {
+    public void testNoSuchElementException() throws RepositoryException, NotExecutableException {
         NodeIterator it = execute(xpathRoot + "//*", Query.XPATH).getNodes();
         while (it.hasNext()) {
             it.nextNode();
@@ -119,8 +121,9 @@ public class QueryResultNodeIteratorTest extends AbstractQueryTest {
 
     /**
      * Tests if {@link javax.jcr.NodeIterator#skip(long)} works correctly.
+     * @throws NotExecutableException 
      */
-    public void testSkip() throws RepositoryException {
+    public void testSkip() throws RepositoryException, NotExecutableException {
         String query = xpathRoot + "//*";
         QueryResult rs = execute(query, Query.XPATH);
         NodeIterator it = rs.getNodes();

jackrabbit-jcr-tests/src/main/java/org/apache/jackrabbit/test/api/query/SQLJcrPathTest.java
@@ -66,7 +66,7 @@ public class SQLJcrPathTest extends AbstractQueryTest {
         String queryStatement = "select * from " + nodeTypeName;
 
         // execute the search query
-        Query query = session.getWorkspace().getQueryManager().createQuery(queryStatement, Query.SQL);
+        Query query = super.createQuery(queryStatement, Query.SQL);
         QueryResult result = query.execute();
 
         assertTrue("jcr:path must be present in query result row",

jackrabbit-jcr-tests/src/main/java/org/apache/jackrabbit/test/api/query/SQLPathTest.java
@@ -21,6 +21,9 @@ import javax.jcr.RepositoryException;
 import javax.jcr.NodeIterator;
 import javax.jcr.Session;
 import javax.jcr.util.TraversingItemVisitor;
+
+import org.apache.jackrabbit.test.NotExecutableException;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -66,8 +69,9 @@ public class SQLPathTest extends AbstractQueryTest {
 
     /**
      * Tests if &lt;somepath>/% returns the descendants of &lt;somepath>.
+     * @throws NotExecutableException 
      */
-    public void testDescendantTestRoot() throws RepositoryException {
+    public void testDescendantTestRoot() throws RepositoryException, NotExecutableException {
         String sql = getStatement(testRoot + "/%");
         executeSqlQuery(session, sql, getDescendants(testRootNode));
     }
@@ -75,8 +79,9 @@ public class SQLPathTest extends AbstractQueryTest {
     /**
      * Tests if &lt;somepath>/% returns no nodes if node at &lt;somepath>
      * is a leaf.
+     * @throws NotExecutableException 
      */
-    public void testDescendantLeaf() throws RepositoryException {
+    public void testDescendantLeaf() throws RepositoryException, NotExecutableException {
         // find leaf
         Node leaf = testRootNode;
         while (leaf.hasNodes()) {
@@ -90,8 +95,9 @@ public class SQLPathTest extends AbstractQueryTest {
      * Tests if &lt;somepath>/%/&lt;nodename> OR &lt;somepath>/&lt;nodename>
      * returns nodes with name &lt;nodename> which are descendants of
      * node at <code>testroot</code>.
+     * @throws NotExecutableException 
      */
-    public void testDescendantSelfTestRoot() throws RepositoryException {
+    public void testDescendantSelfTestRoot() throws RepositoryException, NotExecutableException {
         // get first node which is two levels deeper than node at testroot
         Node n = null;
         for (NodeIterator it = testRootNode.getNodes(); it.hasNext();) {
@@ -118,8 +124,9 @@ public class SQLPathTest extends AbstractQueryTest {
 
     /**
      * Tests if /% AND NOT /%/% returns the child nodes of the root node.
+     * @throws NotExecutableException 
      */
-    public void testChildAxisRoot() throws RepositoryException {
+    public void testChildAxisRoot() throws RepositoryException, NotExecutableException {
         String sql = getStatement("/%");
         sql += " AND NOT " + jcrPath + " LIKE '/%/%'";
         Node[] nodes = toArray(session.getRootNode().getNodes());
@@ -129,8 +136,9 @@ public class SQLPathTest extends AbstractQueryTest {
     /**
      * Tests if &lt;somepath>/% AND NOT &lt;somepath>/%/% returns the child
      * nodes of node at &lt;somepath>.
+     * @throws NotExecutableException 
      */
-    public void testChildAxisTestRoot() throws RepositoryException {
+    public void testChildAxisTestRoot() throws RepositoryException, NotExecutableException {
         String sql = getStatement(testRoot + "/%");
         sql += " AND NOT " + jcrPath + " LIKE '" + testRoot + "/%/%'";
         Node[] nodes = toArray(testRootNode.getNodes());
@@ -140,8 +148,9 @@ public class SQLPathTest extends AbstractQueryTest {
     /**
      * Tests if &lt;somepath>/% AND NOT &lt;somepath>/%/% returns no nodes
      * if the node at &lt;somepath> is a leaf.
+     * @throws NotExecutableException 
      */
-    public void testChildAxisLeaf() throws RepositoryException {
+    public void testChildAxisLeaf() throws RepositoryException, NotExecutableException {
         // find leaf
         Node leaf = testRootNode;
         while (leaf.hasNodes()) {

commit 70bcae2d8efd1fa6a8b1977f518bc89dc38821e2

JCR-2208: fix regression introduced by r794294 (query was executed using an incorrect session, leading to unexpected results), also make more XPath tests conditional
git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@794323 13f79535-47bb-0310-9956-ffa450edef68

jackrabbit-jcr-tests/src/main/java/org/apache/jackrabbit/test/api/query/AbstractQueryTest.java
@@ -135,28 +135,29 @@ public abstract class AbstractQueryTest extends AbstractJCRTest {
     /**
      * Creates a {@link Query} for the given statement in the requested
      * language, treating optional languages gracefully
-     *
-     * @param statement the query should be created for
-     * @param language  query language to be used for Query creation
-     * @return
-     *
      * @throws RepositoryException
      */
-    protected Query createQuery(String statement, String language)
-        throws RepositoryException, NotExecutableException {
+    protected Query createQuery(String statement, String language) throws RepositoryException, NotExecutableException {
+        return createQuery(superuser, statement, language);
+    }
+
+    /**
+     * Creates a {@link Query} for the given statement in the requested
+     * language, treating optional languages gracefully
+     * @throws RepositoryException
+     */
+    protected Query createQuery(Session session, String statement, String language) throws RepositoryException, NotExecutableException {
         log.println("Creating query: " + statement);
         try {
-            return qm.createQuery(statement, language);
-        }
-        catch (InvalidQueryException ex) {
-            
-            // if language is optional and not reported as "supported" -> 
+            return session.getWorkspace().getQueryManager().createQuery(statement, language);
+        } catch (InvalidQueryException ex) {
+
+            // if language is optional and not reported as "supported" ->
             // demote to NotExecutableException
-            
-            if (! isSupportedLanguage(language) && ! Query.JCR_SQL2.equals(language)) {
+
+            if (!isSupportedLanguage(language) && !Query.JCR_SQL2.equals(language)) {
                 throw new NotExecutableException("Repository does not support " + language + " query syntax");
-            }
-            else {
+            } else {
                 throw ex;
             }
         }
@@ -302,10 +303,11 @@ public abstract class AbstractQueryTest extends AbstractJCRTest {
      * @param session the session to use for the query.
      * @param xpath the xpath query.
      * @param nodes the expected result nodes.
+     * @throws NotExecutableException 
      */
     protected void executeXPathQuery(Session session, String xpath, Node[] nodes)
-            throws RepositoryException {
-        QueryResult res = session.getWorkspace().getQueryManager().createQuery(xpath, Query.XPATH).execute();
+            throws RepositoryException, NotExecutableException {
+        QueryResult res = createQuery(session, xpath, Query.XPATH).execute();
         checkResult(res, nodes);
     }
 
@@ -319,7 +321,7 @@ public abstract class AbstractQueryTest extends AbstractJCRTest {
      */
     protected void executeSqlQuery(Session session, String sql, Node[] nodes)
             throws RepositoryException, NotExecutableException {
-        QueryResult res = createQuery(sql, Query.SQL).execute();
+        QueryResult res = createQuery(session, sql, Query.SQL).execute();
         checkResult(res, nodes);
     }
 

jackrabbit-jcr-tests/src/main/java/org/apache/jackrabbit/test/api/query/ElementTest.java
@@ -43,8 +43,9 @@ public class ElementTest extends AbstractQueryTest {
 
     /**
      * Tests the element test without arguments.
+     * @throws NotExecutableException 
      */
-    public void testElementTest() throws RepositoryException {
+    public void testElementTest() throws RepositoryException, NotExecutableException {
         Node n1 = testRootNode.addNode(nodeName1, testNodeType);
         Node n2 = testRootNode.addNode(nodeName2, ntBase);
         Node n3 = testRootNode.addNode(nodeName3, testNodeType);
@@ -56,8 +57,9 @@ public class ElementTest extends AbstractQueryTest {
 
     /**
      * Tests the element test with one any node argument.
+     * @throws NotExecutableException 
      */
-    public void testElementTestAnyNode() throws RepositoryException {
+    public void testElementTestAnyNode() throws RepositoryException, NotExecutableException {
         Node n1 = testRootNode.addNode(nodeName1, testNodeType);
         Node n2 = testRootNode.addNode(nodeName2, ntBase);
         Node n3 = testRootNode.addNode(nodeName3, testNodeType);
@@ -70,8 +72,9 @@ public class ElementTest extends AbstractQueryTest {
     /**
      * Tests the element test with an any node argument and a type argument
      * that matches all nodes (nt:base).
+     * @throws NotExecutableException 
      */
-    public void testElementTestAnyNodeNtBase() throws RepositoryException {
+    public void testElementTestAnyNodeNtBase() throws RepositoryException, NotExecutableException {
         Node n1 = testRootNode.addNode(nodeName1, testNodeType);
         Node n2 = testRootNode.addNode(nodeName2, ntBase);
         Node n3 = testRootNode.addNode(nodeName3, testNodeType);
@@ -84,8 +87,9 @@ public class ElementTest extends AbstractQueryTest {
     /**
      * Tests the element test with an any node argument and a type argument
      * that matches only certain child nodes.
+     * @throws NotExecutableException 
      */
-    public void testElementTestAnyNodeSomeNT() throws RepositoryException {
+    public void testElementTestAnyNodeSomeNT() throws RepositoryException, NotExecutableException {
         Node n1 = testRootNode.addNode(nodeName1, testNodeType);
         testRootNode.addNode(nodeName2, ntBase);
         Node n3 = testRootNode.addNode(nodeName3, testNodeType);
@@ -97,8 +101,9 @@ public class ElementTest extends AbstractQueryTest {
 
     /**
      * Tests the element test with one single name test argument.
+     * @throws NotExecutableException 
      */
-    public void testElementTestNameTest() throws RepositoryException {
+    public void testElementTestNameTest() throws RepositoryException, NotExecutableException {
         Node n1 = testRootNode.addNode(nodeName1, testNodeType);
         testRootNode.addNode(nodeName2, ntBase);
         testRootNode.addNode(nodeName3, testNodeType);
@@ -111,8 +116,9 @@ public class ElementTest extends AbstractQueryTest {
     /**
      * Tests the element test with a name test argument and a type argument that
      * matches all nodes (nt:base).
+     * @throws NotExecutableException 
      */
-    public void testElementTestNameTestNtBase() throws RepositoryException {
+    public void testElementTestNameTestNtBase() throws RepositoryException, NotExecutableException {
         Node n1 = testRootNode.addNode(nodeName1, testNodeType);
         testRootNode.addNode(nodeName2, ntBase);
         testRootNode.addNode(nodeName3, testNodeType);
@@ -125,8 +131,9 @@ public class ElementTest extends AbstractQueryTest {
     /**
      * Tests the element test with a name test argument and a type argument that
      * matches only certain child nodes.
+     * @throws NotExecutableException 
      */
-    public void testElementTestNameTestSomeNT() throws RepositoryException {
+    public void testElementTestNameTestSomeNT() throws RepositoryException, NotExecutableException {
         Node n1 = testRootNode.addNode(nodeName1, testNodeType);
         testRootNode.addNode(nodeName2, ntBase);
         testRootNode.addNode(nodeName3, testNodeType);

jackrabbit-jcr-tests/src/main/java/org/apache/jackrabbit/test/api/query/TextNodeTest.java
@@ -59,8 +59,9 @@ public class TextNodeTest extends AbstractQueryTest {
 
     /**
      * Tests if text() node test is equivalent with jcr:xmltext.
+     * @throws NotExecutableException 
      */
-    public void testTextNodeTest() throws RepositoryException {
+    public void testTextNodeTest() throws RepositoryException, NotExecutableException {
         Node text1 = testRootNode.addNode(jcrXMLText);
         text1.setProperty(jcrXMLCharacters, "foo");
         testRootNode.save();
@@ -71,8 +72,9 @@ public class TextNodeTest extends AbstractQueryTest {
     /**
      * Tests if text() node test is equivalent with jcr:xmltext and will select
      * multiple nodes with name jcr:xmltext.
+     * @throws NotExecutableException 
      */
-    public void testTextNodeTestMultiNodes() throws RepositoryException {
+    public void testTextNodeTestMultiNodes() throws RepositoryException, NotExecutableException {
         Node text1 = testRootNode.addNode(jcrXMLText);
         text1.setProperty(jcrXMLCharacters, "foo");
         Node text2 = testRootNode.addNode(nodeName1, testNodeType).addNode(jcrXMLText);
@@ -85,8 +87,9 @@ public class TextNodeTest extends AbstractQueryTest {
     /**
      * Tests if text() node test is equivalent with jcr:xmltext and jcr:contains
      * matches content in jcr:xmlcharacters property.
+     * @throws NotExecutableException 
      */
-    public void testTextNodeTestContains() throws RepositoryException {
+    public void testTextNodeTestContains() throws RepositoryException, NotExecutableException {
         Node text1 = testRootNode.addNode(jcrXMLText);
         text1.setProperty(jcrXMLCharacters, "the quick brown fox jumps over the lazy dog.");
         Node text2 = testRootNode.addNode(nodeName1, testNodeType).addNode(jcrXMLText);

jackrabbit-jcr2spi/src/test/java/org/apache/jackrabbit/jcr2spi/query/QueryTest.java
@@ -15,6 +15,7 @@
  */
 package org.apache.jackrabbit.jcr2spi.query;
 
+import org.apache.jackrabbit.test.NotExecutableException;
 import org.apache.jackrabbit.test.api.query.AbstractQueryTest;
 
 import javax.jcr.RepositoryException;
@@ -31,8 +32,9 @@ public class QueryTest extends AbstractQueryTest {
 
     /**
      * Queries the child nodes of the root node.
+     * @throws NotExecutableException 
      */
-    public void testQueryChildNodesOfRoot() throws RepositoryException {
+    public void testQueryChildNodesOfRoot() throws RepositoryException, NotExecutableException {
         List nodes = new ArrayList();
         for (NodeIterator it = superuser.getRootNode().getNodes(); it.hasNext(); ) {
             nodes.add(it.nextNode());
@@ -41,7 +43,7 @@ public class QueryTest extends AbstractQueryTest {
         executeXPathQuery(superuser, "/jcr:root/*", children);
     }
 
-    public void testRemappedNamespace() throws RepositoryException {
+    public void testRemappedNamespace() throws RepositoryException, NotExecutableException {
         String namespaceURI = "http://jackrabbit.apache.org/spi/test";
         String defaultPrefix = "spiTest";
 