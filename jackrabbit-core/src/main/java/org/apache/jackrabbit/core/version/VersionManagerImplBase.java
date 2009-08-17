e33a28af8ea16a12c0bda29b63ddd692c970101f
Stefan Guggisberg <stefan@apache.org>
2009-08-17 12:21:19 +0000

    partially reverting last commit (UnsuppRepOpExc->VersionExc) as it caused a test case failure
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@804949 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-core/src/main/java/org/apache/jackrabbit/core/version/VersionManagerImplBase.java
+++ jackrabbit-core/src/main/java/org/apache/jackrabbit/core/version/VersionManagerImplBase.java
@@ -27,7 +27,6 @@ import javax.jcr.Node;
 import javax.jcr.PropertyType;
 import javax.jcr.ItemNotFoundException;
 import javax.jcr.version.Version;
-import javax.jcr.version.VersionException;
 
 import org.apache.jackrabbit.core.HierarchyManager;
 import org.apache.jackrabbit.core.SessionImpl;
@@ -257,7 +256,7 @@ abstract public class VersionManagerImplBase {
         if (baseVersion.isRootVersion()) {
             String msg = "Unable to checkin configuration as it has unversioned child node: " + safeGetJCRPath(root);
             log.debug(msg);
-            throw new VersionException(msg);
+            throw new UnsupportedRepositoryOperationException(msg);
         }
         baseVersions.add(baseVersion.getId());
 
