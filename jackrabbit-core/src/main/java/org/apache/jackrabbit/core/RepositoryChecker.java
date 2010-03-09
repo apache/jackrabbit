3582c2d117c0db3768751b5cdfc6a81d4b90d220
Jukka Zitting <jukka@apache.org>
2010-03-09 11:19:01 +0000

    JCR-2551: Recovery from a lost version history
    Add a very simple recovery mechanims that's triggered by setting the org.apache.jackrabbit.version.recovery system property to true. Work in progress...
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@920806 13f79535-47bb-0310-9956-ffa450edef68

---
+++ jackrabbit-core/src/main/java/org/apache/jackrabbit/core/RepositoryChecker.java
@@ -0,0 +1,132 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.core;
+
+import static org.apache.jackrabbit.spi.commons.name.NameConstants.JCR_BASEVERSION;
+import static org.apache.jackrabbit.spi.commons.name.NameConstants.JCR_PREDECESSORS;
+import static org.apache.jackrabbit.spi.commons.name.NameConstants.JCR_VERSIONHISTORY;
+import static org.apache.jackrabbit.spi.commons.name.NameConstants.MIX_VERSIONABLE;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.jcr.RepositoryException;
+
+import org.apache.jackrabbit.core.id.NodeId;
+import org.apache.jackrabbit.core.id.PropertyId;
+import org.apache.jackrabbit.core.persistence.PersistenceManager;
+import org.apache.jackrabbit.core.state.ChangeLog;
+import org.apache.jackrabbit.core.state.ChildNodeEntry;
+import org.apache.jackrabbit.core.state.ItemStateException;
+import org.apache.jackrabbit.core.state.NodeState;
+import org.apache.jackrabbit.core.version.InternalVersionManager;
+import org.apache.jackrabbit.spi.Name;
+
+/**
+ * Tool for checking for and optionally fixing consistency issues in a
+ * repository. Currently this class only contains a simple versioning
+ * recovery feature for
+ * <a href="https://issues.apache.org/jira/browse/JCR-2551">JCR-2551</a>.
+ */
+class RepositoryChecker {
+
+    private final PersistenceManager workspace;
+
+    private final ChangeLog workspaceChanges;
+
+    private final InternalVersionManager versionManager;
+
+    public RepositoryChecker(
+            PersistenceManager workspace,
+            InternalVersionManager versionManager) {
+        this.workspace = workspace;
+        this.workspaceChanges = new ChangeLog();
+        this.versionManager = versionManager;
+    }
+
+    public void check(NodeId id, boolean recurse)
+            throws RepositoryException {
+        try {
+            NodeState state = workspace.load(id);
+            checkVersionHistory(state);
+
+            if (recurse) {
+                for (ChildNodeEntry child : state.getChildNodeEntries()) {
+                    if (child.getId() != RepositoryImpl.SYSTEM_ROOT_NODE_ID) {
+                        check(child.getId(), recurse);
+                    }
+                }
+            }
+        } catch (ItemStateException e) {
+            throw new RepositoryException("Unable to access node " + id, e);
+        }
+    }
+
+    public void fix() throws RepositoryException {
+        System.out.println("Fixing workspace issues");
+        if (workspaceChanges.hasUpdates()) {
+            System.out.println("... workspace has issues");
+            try {
+                workspace.store(workspaceChanges);
+            } catch (ItemStateException e) {
+                e.printStackTrace();
+                throw new RepositoryException(
+                        "Failed to fix workspace inconsistencies", e);
+            }
+        }
+    }
+
+    private void checkVersionHistory(NodeState node) {
+        if (node.hasPropertyName(JCR_VERSIONHISTORY)) {
+            System.out.println("Checking version history of node " + node.getNodeId());
+            try {
+                versionManager.getVersionHistoryOfNode(node.getNodeId());
+            } catch (Exception e) {
+                e.printStackTrace();
+                removeVersionHistoryReferences(node);
+            }
+        }
+    }
+
+    private void removeVersionHistoryReferences(NodeState node) {
+        NodeState modified =
+            new NodeState(node, NodeState.STATUS_EXISTING_MODIFIED, true);
+
+        Set<Name> mixins = new HashSet<Name>(node.getMixinTypeNames());
+        if (mixins.remove(MIX_VERSIONABLE)) {
+            modified.setMixinTypeNames(mixins);
+        }
+
+        removeProperty(modified, JCR_VERSIONHISTORY);
+        removeProperty(modified, JCR_BASEVERSION);
+        removeProperty(modified, JCR_PREDECESSORS);
+
+        workspaceChanges.modified(modified);
+    }
+
+    private void removeProperty(NodeState node, Name name) {
+        if (node.hasPropertyName(name)) {
+            node.removePropertyName(name);
+            try {
+                workspaceChanges.deleted(workspace.load(
+                        new PropertyId(node.getNodeId(), name)));
+            } catch (ItemStateException ignoe) {
+            }
+        }
+    }
+
+}
