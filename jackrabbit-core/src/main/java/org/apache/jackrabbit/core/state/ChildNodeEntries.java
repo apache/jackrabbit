9c8d5ca7403b83363f4e3d46bcbcced8164675df
Jukka Zitting <jukka@apache.org>
2010-06-16 13:52:31 +0000

    JCR-2598: Saving concurrent sessions executing random operations causes a corrupt JCR
    Patch by Stephan Huttenhuis
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@955229 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-core/src/main/java/org/apache/jackrabbit/core/state/ChildNodeEntries.java
+++ jackrabbit-core/src/main/java/org/apache/jackrabbit/core/state/ChildNodeEntries.java
@@ -153,6 +153,9 @@ class ChildNodeEntries implements List<ChildNodeEntry>, Cloneable {
         }
     }
 
+    // The index may have changed because of changes by another session. Use remove(NodeId id)
+    // instead    
+    @Deprecated
     @SuppressWarnings("unchecked")
     public ChildNodeEntry remove(Name nodeName, int index) {
         if (index < 1) {
@@ -233,7 +236,7 @@ class ChildNodeEntries implements List<ChildNodeEntry>, Cloneable {
      * @return the removed entry or <code>null</code> if there is no such entry.
      */
     public ChildNodeEntry remove(ChildNodeEntry entry) {
-        return remove(entry.getName(), entry.getIndex());
+        return remove(entry.getId());
     }
 
     /**
