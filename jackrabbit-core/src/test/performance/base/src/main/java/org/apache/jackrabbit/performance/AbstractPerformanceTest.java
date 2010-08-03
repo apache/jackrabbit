65029a423da91b1fd063645d1f1f2bf934f8d40b
Jukka Zitting <jukka@apache.org>
2010-08-03 13:26:03 +0000

    JCR-2695: Jackrabbit performance test suite
    Basic test suite structure. Work in progress...
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@981864 13f79535-47bb-0310-9956-ffa450edef68

---
+++ jackrabbit-core/src/test/performance/base/src/main/java/org/apache/jackrabbit/performance/AbstractPerformanceTest.java
@@ -0,0 +1,112 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.performance;
+
+import static org.testng.AssertJUnit.fail;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Random;
+
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.SimpleCredentials;
+import javax.jcr.version.Version;
+import javax.jcr.version.VersionHistory;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.jackrabbit.api.JackrabbitNodeTypeManager;
+import org.apache.jackrabbit.core.RepositoryImpl;
+import org.apache.jackrabbit.core.config.RepositoryConfig;
+
+public abstract class AbstractPerformanceTest {
+
+    protected void createRepositories(String name) throws Exception {
+        // Create a repository using the Jackrabbit default configuration
+        createRepository(
+                name,
+                RepositoryImpl.class.getResourceAsStream("repository.xml"));
+
+        // Create repositories for any special configurations included
+        File directory = new File(new File("src", "test"), "resources");
+        File[] files = directory.listFiles();
+        if (files != null) {
+            Arrays.sort(files);
+            for (File file : files) {
+                String xml = file.getName();
+                if (file.isFile() && xml.endsWith(".xml")) {
+                    createRepository(
+                            name + "-" + xml.substring(0, xml.length() - 4),
+                            FileUtils.openInputStream(file));
+                }
+            }
+        }
+    }
+
+    /**
+     * Creates a named test repository with the given configuration file.
+     *
+     * @param name name of the repository
+     * @param xml input stream for reading the repository configuration
+     * @throws Exception if the repository could not be created
+     */
+    protected void createRepository(String name, InputStream xml)
+            throws Exception {
+        File directory = new File(new File("target", "repository"), name);
+        File configuration = new File(directory, "repository.xml");
+
+        // Copy the configuration file into the repository directory
+        try {
+            OutputStream output = FileUtils.openOutputStream(configuration);
+            try {
+                IOUtils.copy(xml, output);
+            } finally {
+                output.close();
+            }
+        } finally {
+            xml.close();
+        }
+
+        // Create the repository
+        try {
+            RepositoryConfig config = RepositoryConfig.create(
+                    configuration.getPath(), directory.getPath());
+            RepositoryImpl repository = RepositoryImpl.create(config);
+            try {
+                Session session = repository.login(
+                        new SimpleCredentials("admin", "admin".toCharArray()));
+                try {
+                    createTestData(session);
+                } finally {
+                    session.logout();
+                }
+            } finally {
+                repository.shutdown();
+            }
+        } catch (RepositoryException e) {
+            e.printStackTrace();
+            fail("Create repository " + name);
+        }
+    }
+
+}
