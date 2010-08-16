6589f8a7f6096e7f50e3b1db7efabecd7ea8292d
Jukka Zitting <jukka@apache.org>
2010-08-16 12:50:12 +0000

    JCR-2699: Improve read/write concurrency
    Improve the test infrastructure to better handle background jobs
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@985904 13f79535-47bb-0310-9956-ffa450edef68

--- test/performance/base/src/main/java/org/apache/jackrabbit/performance/AbstractTest.java
+++ test/performance/base/src/main/java/org/apache/jackrabbit/performance/AbstractTest.java
@@ -16,8 +16,13 @@
  */
 package org.apache.jackrabbit.performance;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import javax.jcr.Credentials;
 import javax.jcr.Repository;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
 
 /**
  * Abstract base class for individual performance benchmarks.
@@ -28,34 +33,155 @@ public abstract class AbstractTest {
 
     private Credentials credentials;
 
-    public void beforeSuite() throws Exception {
+    private List<Session> sessions;
+
+    private List<Thread> threads;
+
+    private volatile boolean running;
+
+    /**
+     * Prepares this performance benchmark.
+     *
+     * @param repository the repository to use
+     * @param credentials credentials of a user with write access
+     * @throws Exception if the benchmark can not be prepared
+     */
+    public void setUp(Repository repository, Credentials credentials)
+            throws Exception {
+        this.repository = repository;
+        this.credentials = credentials;
+        this.sessions = new LinkedList<Session>();
+        this.threads = new LinkedList<Thread>();
+
+        this.running = true;
+
+        beforeSuite();
     }
 
-    public void beforeTest() throws Exception {
+    /**
+     * Executes a single iteration of this test.
+     *
+     * @return number of milliseconds spent in this iteration
+     * @throws Exception if an error occurs
+     */
+    public long execute() throws Exception {
+        beforeTest();
+        try {
+            long start = System.currentTimeMillis();
+            runTest();
+            return System.currentTimeMillis() - start;
+        } finally {
+            afterTest();
+        }
+    }
+    /**
+     * Cleans up after this performance benchmark.
+     *
+     * @throws Exception if the benchmark can not be cleaned up
+     */
+    public void tearDown() throws Exception {
+        afterSuite();
+
+        this.running = false;
+        for (Thread thread : threads) {
+            thread.join();
+        }
+
+        for (Session session : sessions) {
+            if (session.isLive()) {
+                session.logout();
+            }
+        }
+
+        this.threads = null;
+        this.sessions = null;
+        this.credentials = null;
+        this.repository = null;
     }
 
-    public abstract void runTest() throws Exception;
-
-    public void afterTest() throws Exception {
+    /**
+     * Run before any iterations of this test get executed. Subclasses can
+     * override this method to set up static test content.
+     *
+     * @throws Exception if an error occurs
+     */
+    protected void beforeSuite() throws Exception {
     }
 
-    public void afterSuite() throws Exception {
+    protected void beforeTest() throws Exception {
     }
 
-    public Repository getRepository() {
+    protected abstract void runTest() throws Exception;
+
+    protected void afterTest() throws Exception {
+    }
+
+    /**
+     * Run after all iterations of this test have been executed. Subclasses can
+     * override this method to clean up static test content.
+     *
+     * @throws Exception if an error occurs
+     */
+    protected void afterSuite() throws Exception {
+    }
+
+    protected Repository getRepository() {
         return repository;
     }
 
-    void setRepository(Repository repository) {
-        this.repository = repository;
-    }
-
-    public Credentials getCredentials() {
+    protected Credentials getCredentials() {
         return credentials;
     }
 
-    void setCredentials(Credentials credentials) {
-        this.credentials = credentials;
+    /**
+     * Returns a new reader session that will be automatically closed once
+     * all the iterations of this test have been executed.
+     *
+     * @return reader session
+     */
+    protected Session loginReader() {
+        try {
+            Session session = repository.login();
+            sessions.add(session);
+            return session;
+        } catch (RepositoryException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Returns a new writer session that will be automatically closed once
+     * all the iterations of this test have been executed.
+     *
+     * @return writer session
+     */
+    protected Session loginWriter() {
+        try {
+            Session session = repository.login(credentials);
+            sessions.add(session);
+            return session;
+        } catch (RepositoryException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Adds a background thread that repeatedly executes the given job
+     * until all the iterations of this test have been executed.
+     *
+     * @param job background job
+     */
+    protected void addBackgroundJob(final Runnable job) {
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                while (running) {
+                    job.run();
+                }
+            }
+        };
+        thread.start();
+        threads.add(thread);
     }
 
     public String toString() {
