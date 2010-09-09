1aa1d7f6f3ed79b756089b4e2d6819a9328ff1f0
Jukka Zitting <jukka@apache.org>
2010-09-09 13:04:31 +0000

    JCR-2089: Use java.util.concurrent
    After the refactoring of DefaultISMLocking it's no longer possible (even in theory) for WriteLock.downgrade() to throw InterruptedExceptions, so we can safely remove the throws declaration.
    
    There should never be a reason for downgrade() to block, so the method should not be declared to throw InterruptedExceptions.
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@995412 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-core/src/main/java/org/apache/jackrabbit/core/state/ISMLocking.java
+++ jackrabbit-core/src/main/java/org/apache/jackrabbit/core/state/ISMLocking.java
@@ -81,10 +81,8 @@ public interface ISMLocking {
          * used to further release the read lock.
          *
          * @return the read lock downgraded from this write lock.
-         * @throws InterruptedException if the current thread is interrupted
-         *                              while downgrading the write lock.
          */
-        ReadLock downgrade() throws InterruptedException;
+        ReadLock downgrade();
 
     }
 
