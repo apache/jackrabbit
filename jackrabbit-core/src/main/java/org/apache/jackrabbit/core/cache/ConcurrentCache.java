1583af4f4b0e9ce0a4d6f8556acc8e7e926aa945
Jukka Zitting <jukka@apache.org>
2010-10-06 14:30:13 +0000

    JCR-2699: Improve read/write concurrency
    Use a constant for the default number of cache segments to use, as computing the number of available processors can be quite expensive.
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@1005057 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-core/src/main/java/org/apache/jackrabbit/core/cache/ConcurrentCache.java
+++ jackrabbit-core/src/main/java/org/apache/jackrabbit/core/cache/ConcurrentCache.java
@@ -31,6 +31,19 @@ import java.util.Map;
  */
 public class ConcurrentCache<K, V> extends AbstractCache {
 
+    /**
+     * Default number of cache segments to use. Use the number of available
+     * processors (even if that might change during runtime!) as a reasonable
+     * approximation of the amount of parallelism we should expect in the
+     * worst case.
+     * <p>
+     * One reason for this value being a constant is that the
+     * {@link Runtime#availableProcessors()} call is somewhat expensive at
+     * least in some environments.
+     */
+    private static int DEFAULT_NUMBER_OF_SEGMENTS =
+        Runtime.getRuntime().availableProcessors();
+
     private static class E<V> {
 
         private final V value;
@@ -65,7 +78,7 @@ public class ConcurrentCache<K, V> extends AbstractCache {
     }
 
     public ConcurrentCache() {
-        this(Runtime.getRuntime().availableProcessors());
+        this(DEFAULT_NUMBER_OF_SEGMENTS);
     }
 
     /**
