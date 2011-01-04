d32a4f9ad0470927c0145383053af5a2d4b3bc27
Jukka Zitting <jukka@apache.org>
2011-01-04 19:44:14 +0000

    JCR-1301: Trouble undeploying jackrabbit-webapp from Tomcat
    Avoid problems in the DerbyShutdown class when Derby is not used.
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@1055164 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-webapp/src/main/java/org/apache/jackrabbit/j2ee/DerbyShutdown.java
+++ jackrabbit-webapp/src/main/java/org/apache/jackrabbit/j2ee/DerbyShutdown.java
@@ -16,10 +16,11 @@
  */
 package org.apache.jackrabbit.j2ee;
 
+import java.lang.reflect.Method;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
+import java.util.Enumeration;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -37,19 +38,38 @@ public class DerbyShutdown implements ServletContextListener {
     }
 
     public void contextDestroyed(ServletContextEvent event) {
-        try {
-            String shutdown = "jdbc:derby:;shutdown=true";
-            // Try to find the Derby JDBC driver
-            Driver driver = DriverManager.getDriver(shutdown);
-            // Check if the Derby driver comes from this webapp
-            if (driver.getClass().getClassLoader()
-                    == DerbyShutdown.class.getClassLoader()) {
-                // Unregister the automatically loaded driver
-                DriverManager.deregisterDriver(driver);
-                // Tell Derby to release all remaining resources
-                driver.connect(shutdown, new Properties());
+        ClassLoader loader = DerbyShutdown.class.getClassLoader();
+
+        // Deregister all JDBC drivers loaded from this webapp
+        Enumeration<Driver> drivers = DriverManager.getDrivers();
+        while (drivers.hasMoreElements()) {
+            Driver driver = drivers.nextElement();
+            // Check if this driver comes from this webapp
+            if (driver.getClass().getClassLoader() == loader) {
+                try {
+                    DriverManager.deregisterDriver(driver);
+                } catch (SQLException ignore) {
+                }
             }
-        } catch (SQLException ignore) {
+        }
+
+        // Explicitly tell Derby to release all remaining resources.
+        // Use reflection to avoid problems when the Derby is not used.
+        try {
+            Class<?> monitorClass =
+                loader.loadClass("org.apache.derby.iapi.services.monitor.Monitor");
+            if (monitorClass.getClassLoader() == loader) {
+                Method getMonitorMethod =
+                    monitorClass.getMethod("getMonitor", new Class<?>[0]);
+                Object monitor =
+                    getMonitorMethod.invoke(null, new Object[0]);
+                if (monitor != null) {
+                    Method shutdownMethod =
+                        monitor.getClass().getMethod("shutdown", new Class<?>[0]);
+                    shutdownMethod.invoke(monitor, new Object[0]);
+                }
+            }
+        } catch (Exception ignore) {
         }
     }
 
