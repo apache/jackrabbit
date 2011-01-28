aab5959aebd6394c31971b09774070ef470ff506
Angela Schreiber <angela@apache.org>
2011-01-28 13:27:56 +0000

    javadoc typo
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@1064664 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-core/src/main/java/org/apache/jackrabbit/core/cluster/UpdateEventListener.java
+++ jackrabbit-core/src/main/java/org/apache/jackrabbit/core/cluster/UpdateEventListener.java
@@ -32,7 +32,7 @@ public interface UpdateEventListener {
      *
      * @param changes external changes containing only node and property ids.
      * @param events events to deliver
-     * @param timestamp when the change occured.
+     * @param timestamp when the change occurred.
      * @param userData the user data associated with this update.
      * @throws RepositoryException if the update cannot be processed
      */
