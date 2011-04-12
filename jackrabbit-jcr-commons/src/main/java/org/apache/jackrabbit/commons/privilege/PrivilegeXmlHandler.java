6cc456a8ef2310783fc7aaca218e148a68cf76dd
Angela Schreiber <angela@apache.org>
2011-04-12 15:53:13 +0000

    JCR-2940 - fixing license header
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@1091484 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-jcr-commons/src/main/java/org/apache/jackrabbit/commons/privilege/PrivilegeXmlHandler.java
+++ jackrabbit-jcr-commons/src/main/java/org/apache/jackrabbit/commons/privilege/PrivilegeXmlHandler.java
@@ -70,6 +70,26 @@ class PrivilegeXmlHandler implements PrivilegeHandler {
 
     private static final String ATTR_XMLNS = "xmlns:";
 
+    private static final String LICENSE_HEADER = createLicenseHeader();
+
+    private static String createLicenseHeader() {
+        return "\n" +                
+                    "   Licensed to the Apache Software Foundation (ASF) under one or more\n" +
+                    "   contributor license agreements.  See the NOTICE file distributed with\n" +
+                    "   this work for additional information regarding copyright ownership.\n" +
+                    "   The ASF licenses this file to You under the Apache License, Version 2.0\n" +
+                    "   (the \"License\"); you may not use this file except in compliance with\n" +
+                    "   the License.  You may obtain a copy of the License at\n" +
+                    "\n" +
+                    "       http://www.apache.org/licenses/LICENSE-2.0\n" +
+                    "\n" +
+                    "   Unless required by applicable law or agreed to in writing, software\n" +
+                    "   distributed under the License is distributed on an \"AS IS\" BASIS,\n" +
+                    "   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n" +
+                    "   See the License for the specific language governing permissions and\n" +
+                    "   limitations under the License.\n";
+    }
+
     /**
      * Constant for <code>DocumentBuilderFactory</code>.
      */
@@ -78,7 +98,7 @@ class PrivilegeXmlHandler implements PrivilegeHandler {
     private static DocumentBuilderFactory createFactory() {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         factory.setNamespaceAware(true);
-        factory.setIgnoringComments(true);
+        factory.setIgnoringComments(false);
         factory.setIgnoringElementContentWhitespace(true);
         return factory;
     }
@@ -150,6 +170,7 @@ class PrivilegeXmlHandler implements PrivilegeHandler {
         try {
             DocumentBuilder builder = createDocumentBuilder();
             Document doc = builder.newDocument();
+            doc.appendChild(doc.createComment(LICENSE_HEADER));
             Element privileges = (Element) doc.appendChild(doc.createElement(XML_PRIVILEGES));
 
             for (String prefix : namespaces.keySet()) {
