d307ac0c39568482886cb29a1619e4c7298f00df
Claus Köll <ckoell@apache.org>
2011-10-27 06:17:40 +0000

    added default serialVersionUID
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@1189625 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-jca/src/main/java/org/apache/jackrabbit/jca/JCARepositoryHandle.java
+++ jackrabbit-jca/src/main/java/org/apache/jackrabbit/jca/JCARepositoryHandle.java
@@ -110,7 +110,9 @@ public final class JCARepositoryHandle extends ProxyRepository
     private static class JCARepositoryFactory
             implements RepositoryFactory, Serializable {
 
-        private final JCAManagedConnectionFactory mcf;
+		private static final long serialVersionUID = 5364039431121341634L;
+		
+		private final JCAManagedConnectionFactory mcf;
 
         public JCARepositoryFactory(JCAManagedConnectionFactory mcf) {
             this.mcf = mcf;
