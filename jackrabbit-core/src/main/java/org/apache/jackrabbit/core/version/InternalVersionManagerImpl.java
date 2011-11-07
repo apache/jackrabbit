574ae09f78f68c4239b65409aab76841387611ba
Julian Reschke <reschke@apache.org>
2011-11-07 17:31:06 +0000

    JCR-3139: missing sync in InternalVersionManagerImpl.externalUpdate can cause ConcurrentModificationException
    added synchronization
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@1198827 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-core/src/main/java/org/apache/jackrabbit/core/version/InternalVersionManagerImpl.java
+++ jackrabbit-core/src/main/java/org/apache/jackrabbit/core/version/InternalVersionManagerImpl.java
@@ -645,9 +645,12 @@ public class InternalVersionManagerImpl extends InternalVersionManagerBase
 
         Collection<InternalVersionItem> items =
             new ArrayList<InternalVersionItem>();
-        for (Map.Entry<ItemId, InternalVersionItem> entry : versionItems.entrySet()) {
-            if (changes.has(entry.getKey())) {
-                items.add(entry.getValue());
+        synchronized (versionItems) {
+            for (Map.Entry<ItemId, InternalVersionItem> entry : versionItems
+                    .entrySet()) {
+                if (changes.has(entry.getKey())) {
+                    items.add(entry.getValue());
+                }
             }
         }
         itemsUpdated(items);
