c56c29a1f010befc3596166a05cd85732ee808bf
Julian Reschke <reschke@apache.org>
2011-12-14 14:41:27 +0000

    JCR-3177: Remove jdk 1.4 restriction for jcr-tests
    Reduce warning noise on retention tests
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@1214265 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-jcr-tests/src/main/java/org/apache/jackrabbit/test/api/retention/HoldTest.java
+++ jackrabbit-jcr-tests/src/main/java/org/apache/jackrabbit/test/api/retention/HoldTest.java
@@ -253,7 +253,7 @@ public class HoldTest extends AbstractRetentionTest {
     public void testAddHoldOnLockedNode() throws NotExecutableException, RepositoryException {
         Node child = getLockedChildNode();
         // remember current holds for clean up.
-        List holdsBefore = Arrays.asList(retentionMgr.getHolds(child.getPath()));
+        List<Hold> holdsBefore = Arrays.asList(retentionMgr.getHolds(child.getPath()));
 
         // get another session.
         javax.jcr.Session otherS = getHelper().getSuperuserSession();
@@ -269,9 +269,9 @@ public class HoldTest extends AbstractRetentionTest {
             otherS.logout();
 
             // clear holds (in case of test failure)
-            List holds = new ArrayList(Arrays.asList(retentionMgr.getHolds(child.getPath())));
+            List<Hold> holds = new ArrayList<Hold>(Arrays.asList(retentionMgr.getHolds(child.getPath())));
             if (holds.removeAll(holdsBefore)) {
-                for (Iterator it = holds.iterator(); it.hasNext();) {
+                for (Iterator<Hold> it = holds.iterator(); it.hasNext();) {
                     retentionMgr.removeHold(child.getPath(), (Hold) it.next());
                 }
             }
