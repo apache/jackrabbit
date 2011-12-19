c9ffacdc202c078813d2f5b435eb2b7ba6e2e5d1
Julian Reschke <reschke@apache.org>
2011-12-19 14:01:37 +0000

    remove unused import
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@1220752 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-core/src/main/java/org/apache/jackrabbit/core/persistence/pool/BundleDbPersistenceManager.java
+++ jackrabbit-core/src/main/java/org/apache/jackrabbit/core/persistence/pool/BundleDbPersistenceManager.java
@@ -28,7 +28,6 @@ import java.sql.Types;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
