e06fdb9742f3f878313e7876a9c94c9a116bea1e
Julian Reschke <reschke@apache.org>
2012-05-14 12:03:23 +0000

    JCR-3307: check supported option OPTION_ACTIVITIES_SUPPORTED
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@1338172 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-jcr-tests/src/main/java/org/apache/jackrabbit/test/api/version/AbstractMergeTest.java
+++ jackrabbit-jcr-tests/src/main/java/org/apache/jackrabbit/test/api/version/AbstractMergeTest.java
@@ -25,6 +25,7 @@ import javax.jcr.nodetype.NodeType;
 import javax.jcr.nodetype.NodeTypeManager;
 
 import org.apache.jackrabbit.test.AbstractJCRTest;
+import org.apache.jackrabbit.test.NotExecutableException;
 
 /**
  * <code>AbstractMergeTest</code> is the abstract base class for all merge
@@ -136,5 +137,5 @@ public abstract class AbstractMergeTest extends AbstractJCRTest {
     }
 
     // initialize nodes
-    abstract void initNodes() throws RepositoryException;
+    abstract void initNodes() throws RepositoryException, NotExecutableException;
 }
