1684cf9c35e5ee03606ce47169985c31b6ead1ca
Julian Reschke <reschke@apache.org>
2012-07-02 14:08:39 +0000

    JCR-3367 [backport] implement InMemBundlePM#getAllNodeIds correctly (fix missing import in revision 1355403)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/branches/2.4@1356234 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-core/src/main/java/org/apache/jackrabbit/core/persistence/mem/InMemBundlePersistenceManager.java
+++ jackrabbit-core/src/main/java/org/apache/jackrabbit/core/persistence/mem/InMemBundlePersistenceManager.java
@@ -27,6 +27,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
