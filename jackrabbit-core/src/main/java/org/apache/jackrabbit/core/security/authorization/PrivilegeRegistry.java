cd07139b04bb416ea7cc319832ae7618da3e410d
Julian Reschke <reschke@apache.org>
2012-07-24 17:34:41 +0000

    JCR-3390: Reordering policy node fails with AccessDeniedException (ported to 2.2)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/branches/2.2@1365186 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-core/src/main/java/org/apache/jackrabbit/core/security/authorization/PrivilegeRegistry.java
+++ jackrabbit-core/src/main/java/org/apache/jackrabbit/core/security/authorization/PrivilegeRegistry.java
@@ -258,13 +258,6 @@ public final class PrivilegeRegistry {
                 perm |= Permission.ADD_NODE;
             }
 
-            // modify_child_node_collection permission is granted through
-            // privileges on the parent
-            if ((parentPrivs & ADD_CHILD_NODES) == ADD_CHILD_NODES &&
-                    (parentPrivs & REMOVE_CHILD_NODES) == REMOVE_CHILD_NODES) {
-                perm |= Permission.MODIFY_CHILD_NODE_COLLECTION;
-            }
-
             /*
              remove_node is
              allowed: only if remove_child_nodes privilege is present on
@@ -283,6 +276,13 @@ public final class PrivilegeRegistry {
                     perm |= Permission.REMOVE_NODE;
                 }
             }
+        }
+
+        // modify_child_node_collection permission is granted through
+        // privileges on the parent
+        if ((parentPrivs & ADD_CHILD_NODES) == ADD_CHILD_NODES &&
+                (parentPrivs & REMOVE_CHILD_NODES) == REMOVE_CHILD_NODES) {
+            perm |= Permission.MODIFY_CHILD_NODE_COLLECTION;
         }
 
         // the remaining (special) permissions are simply defined on the node
