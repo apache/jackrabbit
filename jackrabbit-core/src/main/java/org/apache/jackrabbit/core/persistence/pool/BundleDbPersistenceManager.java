4698aca886a214fb4957be5563dbc51a1f5306c9
Unico Hommes <unico@apache.org>
2012-09-27 09:42:08 +0000

    JCR-3383 Backport: Unclosed Resources in ConnectionHelper if ResultSet is null
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/branches/2.2@1390911 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-core/src/main/java/org/apache/jackrabbit/core/persistence/pool/BundleDbPersistenceManager.java
+++ jackrabbit-core/src/main/java/org/apache/jackrabbit/core/persistence/pool/BundleDbPersistenceManager.java
@@ -498,7 +498,7 @@ public class BundleDbPersistenceManager
             }
             failures++;
             log.error("Failed to persist ChangeLog (stacktrace on DEBUG log level), blockOnConnectionLoss = "
-                    + blockOnConnectionLoss, lastException);
+                    + blockOnConnectionLoss + ": " + lastException);
             log.debug("Failed to persist ChangeLog", lastException);
             if (blockOnConnectionLoss || failures <= 1) { // if we're going to try again
                 try {
@@ -1033,13 +1033,15 @@ public class BundleDbPersistenceManager
             ResultSet rs =
                 conHelper.exec(bundleSelectSQL, getKey(id), false, 0);
             try {
-                if (rs.next()) {
+                if (rs != null && rs.next()) {
                     return readBundle(id, rs, 1);
                 } else {
                     return null;
                 }
             } finally {
-                rs.close();
+            	if (rs != null) {
+            		rs.close();
+            	}
             }
         } catch (SQLException e) {
             String msg = "failed to read bundle: " + id + ": " + e;
