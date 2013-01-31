8476213eb3620f4ee3f747f447e0a672b63df8e1
Unico Hommes <unico@apache.org>
2013-01-31 11:14:29 +0000

    JCR-3506 add ability of reverse lucene index check: whether all items in the repository that should be indexed are present in the index. because this check will take more time and resources it must be explicitly enabled using a system property.
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@1440908 13f79535-47bb-0310-9956-ffa450edef68

---
+++ jackrabbit-core/src/test/java/org/apache/jackrabbit/core/query/lucene/SearchIndexConsistencyCheckTest.java
@@ -0,0 +1,124 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.core.query.lucene;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.jcr.Node;
+import javax.jcr.Session;
+
+import org.apache.jackrabbit.core.SearchManager;
+import org.apache.jackrabbit.core.TestHelper;
+import org.apache.jackrabbit.core.id.NodeId;
+import org.apache.jackrabbit.core.query.lucene.hits.AbstractHitCollector;
+import org.apache.jackrabbit.core.state.NodeState;
+import org.apache.jackrabbit.test.AbstractJCRTest;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.TermQuery;
+
+public class SearchIndexConsistencyCheckTest extends AbstractJCRTest {
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        System.setProperty("org.apache.jackrabbit.checker.index.reverse", "true");
+    }
+
+    public void testIndexMissesNode() throws Exception {
+
+        Session s = getHelper().getSuperuserSession();
+        SearchManager searchManager = TestHelper.getSearchManager(s);
+        SearchIndex searchIndex = (SearchIndex) searchManager.getQueryHandler();
+
+        Node foo = testRootNode.addNode("foo");
+        testRootNode.getSession().save();
+        NodeId fooId = new NodeId(foo.getIdentifier());
+
+        Iterator<NodeId> remove = Collections.singletonList(fooId).iterator();
+        Iterator<NodeState> add = Collections.<NodeState>emptyList().iterator();
+
+        searchIndex.updateNodes(remove, add);
+
+        ConsistencyCheck consistencyCheck = searchIndex.runConsistencyCheck();
+        List<ConsistencyCheckError> errors = consistencyCheck.getErrors();
+        assertEquals("Expected 1 index consistencey error", 1, errors.size());
+
+        ConsistencyCheckError error = errors.iterator().next();
+        assertEquals("Different node was reported to be missing", error.id, fooId);
+
+        consistencyCheck.repair(false);
+
+        assertTrue("Index was not repaired properly", searchIndexContainsNode(searchIndex, fooId));
+    }
+
+    public void testIndexContainsUnknownNode() throws Exception {
+
+        Session s = getHelper().getSuperuserSession();
+        SearchManager searchManager = TestHelper.getSearchManager(s);
+        SearchIndex searchIndex = (SearchIndex) searchManager.getQueryHandler();
+
+        NodeId nodeId = new NodeId(0, 0);
+        NodeState nodeState = new NodeState(nodeId, null, null, 1, false);
+
+        Iterator<NodeId> remove = Collections.<NodeId>emptyList().iterator();
+        Iterator<NodeState> add = Collections.singletonList(nodeState).iterator();
+
+        searchIndex.updateNodes(remove, add);
+
+        ConsistencyCheck consistencyCheck = searchIndex.runConsistencyCheck();
+        List<ConsistencyCheckError> errors = consistencyCheck.getErrors();
+        assertEquals("Expected 1 index consistency error", 1, errors.size());
+
+        ConsistencyCheckError error = errors.iterator().next();
+        assertEquals("Different node was reported to be unknown", error.id, nodeId);
+
+        consistencyCheck.repair(false);
+
+        assertFalse("Index was not repaired properly", searchIndexContainsNode(searchIndex, nodeId));
+    }
+
+    private boolean searchIndexContainsNode(SearchIndex searchIndex, NodeId nodeId) throws IOException {
+        final List<Integer> docs = new ArrayList<Integer>(1);
+        final IndexReader reader = searchIndex.getIndexReader();
+        try {
+            IndexSearcher searcher = new IndexSearcher(reader);
+            try {
+                Query q = new TermQuery(new Term(FieldNames.UUID, nodeId.toString()));
+                searcher.search(q, new AbstractHitCollector() {
+                    @Override
+                    protected void collect(final int doc, final float score) {
+                        docs.add(doc);
+                    }
+                });
+            } finally {
+                searcher.close();
+            }
+        } finally {
+            Util.closeOrRelease(reader);
+        }
+        return !docs.isEmpty();
+
+    }
+
+}
