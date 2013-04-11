9c0832808c6ac8e46ff7f429bed9d4b4c29cc23b
Julian Reschke <reschke@apache.org>
2013-04-11 16:08:52 +0000

    JCR-3562: fix NameParser to accept "{a" as a valid local (non-expanded) name
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@1466938 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-core/src/test/java/org/apache/jackrabbit/core/NodeImplTest.java
+++ jackrabbit-core/src/test/java/org/apache/jackrabbit/core/NodeImplTest.java
@@ -397,4 +397,16 @@ public class NodeImplTest extends AbstractJCRTest {
         root.remove();
         session.save();
     }
+
+    public void testBracketsInNodeName() throws Exception {
+        final Node root = testRootNode.addNode("testBracketsInNodeName");
+
+        final String[] childNames = { "{A}", "B}", "{C", "(D)", "E)", "(F", };
+
+        for (String name : childNames) {
+            root.addNode(name);
+            root.getSession().save();
+            assertTrue("Expecting child " + name + " to have been created", root.hasNode(name));
+        }
+    }
 }
