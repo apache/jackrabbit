be835a0140d67a62b6e0f21dc41330be974f94a4
Marcel Reutegger <mreutegg@apache.org>
2013-05-16 09:32:56 +0000

    JCR-3596: Reduce level for 'overwriting cached item' log messages in ItemManager
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/branches/2.6@1483277 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-core/src/main/java/org/apache/jackrabbit/core/ItemManager.java
+++ jackrabbit-core/src/main/java/org/apache/jackrabbit/core/ItemManager.java
@@ -933,7 +933,7 @@ public class ItemManager implements ItemStateListener {
             }
             ItemId id = data.getId();
             if (itemCache.containsKey(id)) {
-                log.warn("overwriting cached item " + id);
+                log.debug("overwriting cached item " + id);
             }
             if (log.isDebugEnabled()) {
                 log.debug("caching item " + id);
@@ -1261,7 +1261,7 @@ public class ItemManager implements ItemStateListener {
             }
             Object old = map.put(data.getPrimaryParentId(), data);
             if (old != null) {
-                log.warn("overwriting cached item: " + old);
+                log.debug("overwriting cached item: " + old);
             }
         }
 
