320d05cd871c3669f6c4514d7f9c733c47c4d735
Sylvester Abreu <sylvester.abreu@digitaslbi.com>
2015-09-15 10:15:19 +0100

    Deleted redundant null references

--- jackrabbit-aws-ext/src/main/java/org/apache/jackrabbit/aws/ext/Utils.java
+++ jackrabbit-aws-ext/src/main/java/org/apache/jackrabbit/aws/ext/Utils.java
@@ -82,7 +82,7 @@ public final class Utils {
     public static AmazonS3Client openService(final Properties prop) {
         String accessKey = prop.getProperty(S3Constants.ACCESS_KEY);
         String secretKey = prop.getProperty(S3Constants.SECRET_KEY);
-        AmazonS3Client s3service = null;
+        AmazonS3Client s3service;
         if (StringUtils.isNullOrEmpty(accessKey)
             || StringUtils.isNullOrEmpty(secretKey)) {
             LOG.info("Configuring Amazon Client from environment");
@@ -95,7 +95,7 @@ public final class Utils {
                 getClientConfiguration(prop));
         }
         String region = prop.getProperty(S3Constants.S3_REGION);
-        String endpoint = null;
+        String endpoint;
         String propEndPoint = prop.getProperty(S3Constants.S3_END_POINT);
         if ((propEndPoint != null) & !"".equals(propEndPoint)) {
             endpoint = propEndPoint;
