b0364eabafb9ecf9a25d0ffea61c9d002e609d1e
Manfred Baedke <baedke@apache.org>
2015-12-21 16:15:48 +0000

    JCR-2633: Modified externally exception when modifying mixinTypes with single session
    Persist jcr:mixinTypes to avoid inconsistencies when adding and removing mixin types (patch created by berry@halderen.net).
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/branches/2.10@1721205 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-core/src/main/java/org/apache/jackrabbit/core/persistence/bundle/AbstractBundlePersistenceManager.java
+++ jackrabbit-core/src/main/java/org/apache/jackrabbit/core/persistence/bundle/AbstractBundlePersistenceManager.java
@@ -642,9 +642,8 @@ public abstract class AbstractBundlePersistenceManager implements
                 bundle.update((NodeState) state);
             } else {
                 PropertyId id = (PropertyId) state.getId();
-                // skip redundant primaryType, mixinTypes and uuid properties
+                // skip redundant primaryType and uuid properties
                 if (id.getName().equals(JCR_PRIMARYTYPE)
-                    || id.getName().equals(JCR_MIXINTYPES)
                     || id.getName().equals(JCR_UUID)) {
                     continue;
                 }
@@ -690,9 +689,8 @@ public abstract class AbstractBundlePersistenceManager implements
         for (ItemState state : changeLog.addedStates()) {
             if (!state.isNode()) {
                 PropertyId id = (PropertyId) state.getId();
-                // skip primaryType pr mixinTypes properties
+                // skip primaryType and uuid properties
                 if (id.getName().equals(JCR_PRIMARYTYPE)
-                    || id.getName().equals(JCR_MIXINTYPES)
                     || id.getName().equals(JCR_UUID)) {
                     continue;
                 }
