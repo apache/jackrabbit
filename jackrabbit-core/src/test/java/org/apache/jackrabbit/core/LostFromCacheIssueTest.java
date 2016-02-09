b5b61821e93889125acfda66b2b05ced77dc1d9e
Manfred Baedke <baedke@apache.org>
2016-02-09 16:45:48 +0000

    JCR-3948: LostFromCacheIssueTest failure
    Fixed messed up test case setup.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/branches/2.6@1729409 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-core/src/test/java/org/apache/jackrabbit/core/LostFromCacheIssueTest.java
+++ jackrabbit-core/src/test/java/org/apache/jackrabbit/core/LostFromCacheIssueTest.java
@@ -1,13 +1,31 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.jackrabbit.core;
 
 import javax.jcr.NamespaceException;
 import javax.jcr.NamespaceRegistry;
 import javax.jcr.Node;
+import javax.jcr.PathNotFoundException;
 import javax.jcr.Property;
-import javax.jcr.Session;
+import javax.jcr.RepositoryException;
 import javax.jcr.Workspace;
 import javax.jcr.nodetype.NodeTypeManager;
 
+import org.apache.jackrabbit.core.nodetype.InvalidNodeTypeDefException;
 import org.apache.jackrabbit.core.nodetype.NodeTypeManagerImpl;
 import org.apache.jackrabbit.core.nodetype.NodeTypeRegistry;
 import org.apache.jackrabbit.spi.Name;
@@ -19,24 +37,31 @@ import org.apache.jackrabbit.test.AbstractJCRTest;
 
 public class LostFromCacheIssueTest extends AbstractJCRTest {
 
-    public Property mixinTypes;
+    private static final String NAMESPACE_PREFIX = "LostFromCacheIssueTestNamespacePrefix";
+    private static final String NAMESPACE_URI = "http://www.onehippo.org/test/1.0";
 
+    private static final String TESTNODE_PATH = "/LostFromCacheIssueTest/node";
+
+    private static final String NODETYPE_1 = NAMESPACE_PREFIX + ":mixin";
+    private static final String NODETYPE_2 = NAMESPACE_PREFIX + ":mxn";
+
+    public Property mixinTypes;
 
     public void setUp() throws Exception {
 
         super.setUp();
-        Session session = superuser;
 
-        System.err.println("Registering namespace and node types...");
-        Workspace workspace = session.getWorkspace();
+        Workspace workspace = superuser.getWorkspace();
         NamespaceRegistry namespaceRegistry = workspace.getNamespaceRegistry();
         NodeTypeManager ntmgr = workspace.getNodeTypeManager();
         NodeTypeRegistry nodetypeRegistry = ((NodeTypeManagerImpl)ntmgr).getNodeTypeRegistry();
         try {
-            namespaceRegistry.registerNamespace("lfcit", "data:lfcit");
-        } catch (NamespaceException ignore) { /* mapping may already be present */  }
+            namespaceRegistry.registerNamespace(NAMESPACE_PREFIX, NAMESPACE_URI);
+        } catch (NamespaceException ignore) {
+            //already exists
+        }
         QNodeTypeDefinition nodeTypeDefinition = new QNodeTypeDefinitionImpl(
-                ((SessionImpl)session).getQName("lfcit:mixin"),
+                ((SessionImpl)superuser).getQName(NODETYPE_1),
                 Name.EMPTY_ARRAY,
                 Name.EMPTY_ARRAY,
                 true,
@@ -47,9 +72,13 @@ public class LostFromCacheIssueTest extends AbstractJCRTest {
                 QPropertyDefinition.EMPTY_ARRAY,
                 QNodeDefinition.EMPTY_ARRAY
                 );
-        nodetypeRegistry.registerNodeType(nodeTypeDefinition);
+        try {
+            nodetypeRegistry.registerNodeType(nodeTypeDefinition);
+        } catch (InvalidNodeTypeDefException ignore) {
+            //already exists
+        }
         nodeTypeDefinition = new QNodeTypeDefinitionImpl(
-                ((SessionImpl)session).getQName("lfcit:mxn"),
+                ((SessionImpl)superuser).getQName(NODETYPE_2),
                 Name.EMPTY_ARRAY,
                 Name.EMPTY_ARRAY,
                 true,
@@ -60,25 +89,58 @@ public class LostFromCacheIssueTest extends AbstractJCRTest {
                 QPropertyDefinition.EMPTY_ARRAY,
                 QNodeDefinition.EMPTY_ARRAY
                 );
-        nodetypeRegistry.registerNodeType(nodeTypeDefinition);
+        try {
+            nodetypeRegistry.registerNodeType(nodeTypeDefinition);
+        } catch (InvalidNodeTypeDefException ignore) {
+            //already exists
+        }
 
-        session.getRootNode().addNode("test").addNode("node");
-        session.save();
+        getOrCreate(superuser.getRootNode(), TESTNODE_PATH);
+        superuser.save();
+    }
+
+    private static Node getOrCreate(Node parent, String path) throws RepositoryException {
+        if (parent == null || path == null || path.trim().isEmpty()) {
+            throw new IllegalArgumentException("Missing `parent` or `path`");
+        }
+
+        String p = path;
+        if (path.startsWith("/")) {
+            p = path.substring(1);
+        }
+
+        Node node = null;
+        try {
+            node = parent.getNode(p);
+        } catch (PathNotFoundException e) {
+            // swallowing exception
+        }
+
+        if (node == null) {
+            // if null is not there and therefore creating it
+            for (String n : p.split("/")) {
+                if (node == null) {
+                    node = parent.addNode(n);
+                } else {
+                    node = node.addNode(n);
+                }
+            }
+        }
+
+        return node;
     }
 
     public void testIssue() throws Exception {
-        String path = "/test/node";
-        Session session = superuser;
-        Node node = session.getRootNode().getNode(path.substring(1));
-        node.addMixin("lfcit:mxn");
+        Node node = superuser.getRootNode().getNode(TESTNODE_PATH.substring(1));
+        node.addMixin(NODETYPE_2);
         mixinTypes = node.getProperty("jcr:mixinTypes");
-        session.save();
-        node.addMixin("lfcit:mixin");
-        session.save();
-        node.removeMixin("lfcit:mxn");
-        node.removeMixin("lfcit:mixin");
-        session.save();
-        node.addMixin("lfcit:mixin");
-        session.save();
+        superuser.save();
+        node.addMixin(NODETYPE_1);
+        superuser.save();
+        node.removeMixin(NODETYPE_2);
+        node.removeMixin(NODETYPE_1);
+        superuser.save();
+        node.addMixin(NODETYPE_1);
+        superuser.save();
     }
 }
