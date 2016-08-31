283df6f101676579086400e30e8dd42eacd5ef33
Julian Reschke <reschke@apache.org>
2016-08-31 12:39:31 +0000

    JCR-4009: back out changes for JCR-4002
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/trunk@1758597 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-webdav/src/main/java/org/apache/jackrabbit/webdav/server/AbstractWebdavServlet.java
+++ jackrabbit-webdav/src/main/java/org/apache/jackrabbit/webdav/server/AbstractWebdavServlet.java
@@ -596,7 +596,7 @@ abstract public class AbstractWebdavServlet extends HttpServlet implements DavCo
      */
     protected void doPost(WebdavRequest request, WebdavResponse response,
                           DavResource resource) throws IOException, DavException {
-        response.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+        doPut(request, response, resource);
     }
 
     /**
@@ -1384,6 +1384,7 @@ abstract public class AbstractWebdavServlet extends HttpServlet implements DavCo
      * @param out
      * @return
      * @see #doPut(WebdavRequest, WebdavResponse, DavResource)
+     * @see #doPost(WebdavRequest, WebdavResponse, DavResource)
      * @see #doMkCol(WebdavRequest, WebdavResponse, DavResource)
      */
     protected OutputContext getOutputContext(DavServletResponse response, OutputStream out) {
