d8114439c405e1ba3e0679ac54cabee13205f027
Julian Reschke <reschke@apache.org>
2016-12-15 16:30:27 +0000

    JCR-4064: Refactor WebDAV related code out of HttpClient3-specific classes for better future re-use (ported to 2.10)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/branches/2.10@1774494 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-webdav/src/main/java/org/apache/jackrabbit/webdav/client/methods/MkColMethod.java
+++ jackrabbit-webdav/src/main/java/org/apache/jackrabbit/webdav/client/methods/MkColMethod.java
@@ -18,15 +18,11 @@ package org.apache.jackrabbit.webdav.client.methods;
 
 import org.apache.jackrabbit.webdav.DavMethods;
 import org.apache.jackrabbit.webdav.DavServletResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * <code>MkColMethod</code>...
  */
 public class MkColMethod extends DavMethodBase {
-
-    private static Logger log = LoggerFactory.getLogger(MkColMethod.class);
 
     public MkColMethod(String uri) {
         super(uri);
