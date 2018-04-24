87a99badd02a1e2269a083d96d13a985e69f4d6f
Julian Reschke <reschke@apache.org>
2018-04-24 07:25:15 +0000

    JCR-4185: spi2dav URIResolverImpl should check status code before attempting to parse multistatus (ported to 2.12)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/branches/2.12@1829963 13f79535-47bb-0310-9956-ffa450edef68

--- jackrabbit-spi2dav/src/main/java/org/apache/jackrabbit/spi2dav/URIResolverImpl.java
+++ jackrabbit-spi2dav/src/main/java/org/apache/jackrabbit/spi2dav/URIResolverImpl.java
@@ -280,13 +280,15 @@ class URIResolverImpl implements URIResolver {
         try {
             method = new PropFindMethod(uri, nameSet, DavConstants.DEPTH_0);
 
-            service.getClient(sessionInfo).executeMethod(method);
+            int status = service.getClient(sessionInfo).executeMethod(method);
+            if (status != DavServletResponse.SC_MULTI_STATUS) {
+                throw new ItemNotFoundException("Unable to retrieve the node with id " + uri + ", response status was: " + status);
+            }
             MultiStatusResponse[] responses = method.getResponseBodyAsMultiStatus().getResponses();
             if (responses.length != 1) {
                 throw new ItemNotFoundException("Unable to retrieve the node with id " + uri);
             }
             return buildNodeId(parentId, uri, responses[0], sessionInfo.getWorkspaceName(), service.getNamePathResolver(sessionInfo));
-
         } catch (IOException e) {
             throw new RepositoryException(e);
         } catch (DavException e) {
