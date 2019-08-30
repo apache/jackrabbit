a35cfb4845d5286477fb07cebd7a3d12f27d2229
Woonsan Ko <woonsan@apache.org>
2019-08-30 11:16:46 -0400

    JCR-4458: exclude RepositoryStubImpl fix/improvements

--- jackrabbit-jcr2dav/src/test/java/org/apache/jackrabbit/jcr2dav/RepositoryStubImpl.java
+++ jackrabbit-jcr2dav/src/test/java/org/apache/jackrabbit/jcr2dav/RepositoryStubImpl.java
@@ -54,11 +54,7 @@ public class RepositoryStubImpl extends JackrabbitRepositoryStub {
 
     private final String protectedRemoveImplClass;
 
-    private static final String WEBDAV_SERVLET_CONTEXT_PATH = System.getProperty("WebDAVServletContext", "");
-
-    private static final String WEBDAV_SERVLET_PATH_PREFIX = System.getProperty("WebDAVServletPrefix", "/server");
-
-    private static final String WEBDAV_SERVLET_PATH_MAPPING = WEBDAV_SERVLET_PATH_PREFIX + "/*";
+    private static final String WEBDAV_SERVLET_CONTEXT = System.getProperty("WebDAVServletContext", "/");
 
     public RepositoryStubImpl(Properties env) {
         super(env);
@@ -80,7 +76,7 @@ public class RepositoryStubImpl extends JackrabbitRepositoryStub {
                     return repository;
                 }
             });
-            String pathPrefix = WEBDAV_SERVLET_PATH_PREFIX;
+            String pathPrefix = WEBDAV_SERVLET_CONTEXT;
             if (pathPrefix.endsWith("/")) {
                 pathPrefix = pathPrefix.substring(0,  pathPrefix.length() - 1);
             }
@@ -88,8 +84,8 @@ public class RepositoryStubImpl extends JackrabbitRepositoryStub {
             holder.setInitParameter(JCRWebdavServerServlet.INIT_PARAM_MISSING_AUTH_MAPPING, "");
             holder.setInitParameter(JcrRemotingServlet.INIT_PARAM_PROTECTED_HANDLERS_CONFIG, protectedRemoveImplClass);
 
-            ServletContextHandler schandler = new ServletContextHandler(server, WEBDAV_SERVLET_CONTEXT_PATH);
-            schandler.addServlet(holder, WEBDAV_SERVLET_PATH_MAPPING);
+            ServletContextHandler schandler = new ServletContextHandler(server, WEBDAV_SERVLET_CONTEXT);
+            schandler.addServlet(holder, "/*");
         }
 
         if (connector == null) {
@@ -111,7 +107,7 @@ public class RepositoryStubImpl extends JackrabbitRepositoryStub {
             try {
                 Map<String, String> parameters = new HashMap<String, String>();
 
-                String uri = "http://localhost:" + connector.getLocalPort() + WEBDAV_SERVLET_CONTEXT_PATH + WEBDAV_SERVLET_PATH_PREFIX;
+                String uri = "http://localhost:" + connector.getLocalPort() + WEBDAV_SERVLET_CONTEXT;
 
                 String parmName = System.getProperty(this.getClass().getName() + ".REPURIPARM", JcrUtils.REPOSITORY_URI);
                 parameters.put(parmName, uri);
