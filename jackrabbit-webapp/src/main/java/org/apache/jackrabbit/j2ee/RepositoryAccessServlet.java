612102e22b436b7a92fc2ff2568277f511439751
Julian Reschke <reschke@apache.org>
2024-02-09 16:15:22 +0100

    JCR-5024: webapp: un-deprecate RepositoryAccessServlet.getRepositoryByJNDI (#160)

--- jackrabbit-webapp/src/main/java/org/apache/jackrabbit/j2ee/RepositoryAccessServlet.java
+++ jackrabbit-webapp/src/main/java/org/apache/jackrabbit/j2ee/RepositoryAccessServlet.java
@@ -195,8 +195,6 @@ public class RepositoryAccessServlet extends HttpServlet {
     }
 
     /**
-     * @deprecated RMI support is deprecated and will be removed in a future version of Jackrabbit; see <a href=https://issues.apache.org/jira/browse/JCR-4972 target=_blank>Jira ticket JCR-4972</a> for more information.
-     * <p>
      * Checks if the repository is available via JNDI and returns it.
      * @return the repository or <code>null</code>
      * @throws ServletException if this servlet is not properly configured.
