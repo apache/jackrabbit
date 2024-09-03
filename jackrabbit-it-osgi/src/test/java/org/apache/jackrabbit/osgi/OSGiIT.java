e48ad8137563dfb2ab86783155eaf5fe25be8557
Julian Reschke <julian.reschke@gmx.de>
2024-09-03 04:30:04 +0100

    JCR-5099: Update Logback version to 1.2.13

--- jackrabbit-it-osgi/src/test/java/org/apache/jackrabbit/osgi/OSGiIT.java
+++ jackrabbit-it-osgi/src/test/java/org/apache/jackrabbit/osgi/OSGiIT.java
@@ -66,8 +66,8 @@ public class OSGiIT {
                 mavenBundle("org.apache.felix", "org.apache.felix.fileinstall", "3.2.6"),
                 mavenBundle("org.slf4j", "slf4j-api", "1.7.36"),
                 mavenBundle("commons-logging", "commons-logging", "1.2"),
-                mavenBundle("ch.qos.logback", "logback-core", "1.2.11"),
-                mavenBundle("ch.qos.logback", "logback-classic", "1.2.11"),
+                mavenBundle("ch.qos.logback", "logback-core", "1.2.13"),
+                mavenBundle("ch.qos.logback", "logback-classic", "1.2.13"),
                 frameworkProperty("repository.home").value("target"),
                 systemProperties(
                         systemProperty("logback.configurationFile")
